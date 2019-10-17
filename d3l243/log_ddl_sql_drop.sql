f00859935b8873b5cdcb9ad194b89ef9f270b23d
alchemistmatt <matthew.monroe@pnnl.gov>
2019-10-16 19:00:20 -0700

    Add d3l243 database objects

---
+++ d3l243/log_ddl_sql_drop.sql
@@ -0,0 +1,36 @@
+--
+-- Name: log_ddl_sql_drop(); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+
+CREATE FUNCTION public.log_ddl_sql_drop() RETURNS event_trigger
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Adds log entries to t_schema_change_log regarding the dropped object
+**
+**  Auth:   mem
+**  Date:   10/08/2019 mem - Initial version
+**
+*****************************************************/
+DECLARE
+    -- _msg text;
+BEGIN
+
+    INSERT INTO t_schema_change_log (create_date, login_name, client_addr, command_tag, object_type, schema_name, object_name)
+    SELECT now(),
+           current_user,
+           inet_client_addr(),
+           'DROP',
+           e.object_type,
+           e.schema_name,
+           e.object_name
+    FROM pg_event_trigger_dropped_objects() e
+    WHERE e.schema_name <> 'pg_toast' and not e.object_name is null;
+
+END
+$$;
+
+
+ALTER FUNCTION public.log_ddl_sql_drop() OWNER TO d3l243;
---
+++ d3l243/t_comments.sql
@@ -0,0 +1,11 @@
+--
+-- Name: t_comments; Type: TABLE; Schema: public; Owner: d3l243
+--
+
+CREATE TABLE public.t_comments (
+    id integer,
+    comment public.citext
+);
+
+
+ALTER TABLE public.t_comments OWNER TO d3l243;
