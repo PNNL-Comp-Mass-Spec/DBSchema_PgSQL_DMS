833f00c75a1ba44d707ba724102b661868af7982
alchemistmatt <matthew.monroe@pnnl.gov>
2020-01-23 18:43:02 -0800

    Remove extra files that are now part of the parent object's file

--- pgwatch2/pgwatch2.monitored_db md_id.sql
+++
@@ -1,5 +0,0 @@
---
--- Name: monitored_db md_id; Type: DEFAULT; Schema: pgwatch2; Owner: pgwatch2
---
-
-ALTER TABLE ONLY pgwatch2.monitored_db ALTER COLUMN md_id SET DEFAULT nextval('pgwatch2.monitored_db_md_id_seq'::regclass);
--- pgwatch2/pgwatch2.monitored_db_md_id_seq.sql
+++
@@ -1,5 +0,0 @@
---
--- Name: monitored_db_md_id_seq; Type: SEQUENCE OWNED BY; Schema: pgwatch2; Owner: pgwatch2
---
-
-ALTER SEQUENCE pgwatch2.monitored_db_md_id_seq OWNED BY pgwatch2.monitored_db.md_id;
