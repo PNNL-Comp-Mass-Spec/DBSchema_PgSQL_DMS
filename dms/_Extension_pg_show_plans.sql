c9844e9b3add7dc8796da1a19c3ecea906234fcb
alchemistmatt <matthew.monroe@pnnl.gov>
2020-05-15 17:25:57 -0700

    Update extensions

---
+++ dms/_Extension_pg_show_plans.sql
@@ -0,0 +1,12 @@
+--
+-- Name: pg_show_plans; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_show_plans WITH SCHEMA public;
+
+--
+-- Name: EXTENSION pg_show_plans; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION pg_show_plans IS 'show query plans of all currently running SQL statements';
+
