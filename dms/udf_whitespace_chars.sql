b488e6b7aea13dd94f2ca47bfc7f72466c654ed7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-05 22:12:41 -0700

    Add function udf_whitespace_chars

---
+++ dms/udf_whitespace_chars.sql
@@ -0,0 +1,49 @@
+--
+-- Name: udf_whitespace_chars(text, integer); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE FUNCTION public.udf_whitespace_chars(_entityname text, _allowspace integer DEFAULT 0) RETURNS boolean
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**  Checks for whitespace characters: CRLF, tab, and space
+**  Allows symbols and letters, including periods, dashes,
+**  and underscores
+**
+**  Returns false if no problems
+**  Returns true if whitespace characters are present
+**
+**  This function is called by numerous Check Constraints,
+**  including on tables T_Dataset and T_Experiments
+**
+**  Auth:   mem
+**  Date:   02/15/2011
+**          04/05/2022 mem - Ported to PostgreSQL
+**
+****************************************************/
+DECLARE
+    _invalidChars int := 0;
+BEGIN
+
+    If position(Chr(10) in _entityName) > 0 OR                 -- CR
+       position(Chr(13) in _entityName) > 0 OR                 -- LF
+       position(Chr(9) in _entityName) > 0 OR                  -- Tab
+       _allowSpace = 0 AND position(' ' in _entityName) > 0 -- Space
+    Then
+        Return True;
+    Else
+        Return False;
+    End If;
+
+END
+$$;
+
+
+ALTER FUNCTION public.udf_whitespace_chars(_entityname text, _allowspace integer) OWNER TO d3l243;
+
+--
+-- Name: FUNCTION udf_whitespace_chars(_entityname text, _allowspace integer); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON FUNCTION public.udf_whitespace_chars(_entityname text, _allowspace integer) IS 'udfWhitespaceChars';
+
