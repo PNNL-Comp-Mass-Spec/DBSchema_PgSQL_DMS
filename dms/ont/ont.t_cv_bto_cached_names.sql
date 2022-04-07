f42f6708824a9262141342768cca306210d70aa7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-07 13:19:38 -0700

    Add identity column to t_cv_bto_cached_names
    Greatly speeds up deleting extra rows

--- dms/ont/ont.t_cv_bto_cached_names.sql
+++ dms/ont/ont.t_cv_bto_cached_names.sql
@@ -3,12 +3,26 @@
 --
 
 CREATE TABLE ont.t_cv_bto_cached_names (
+    entry_id integer NOT NULL,
     identifier public.citext NOT NULL,
     term_name public.citext NOT NULL
 );
 
 
 ALTER TABLE ont.t_cv_bto_cached_names OWNER TO d3l243;
+
+--
+-- Name: t_cv_bto_cached_names_entry_id_seq; Type: SEQUENCE; Schema: ont; Owner: d3l243
+--
+
+ALTER TABLE ont.t_cv_bto_cached_names ALTER COLUMN entry_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME ont.t_cv_bto_cached_names_entry_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 --
 -- Name: t_cv_bto_entry_id_seq; Type: SEQUENCE; Schema: ont; Owner: d3l243
