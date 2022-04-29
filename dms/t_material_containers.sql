1ad40cb1eff9446efcbd1612ffe0b1b523fa1ee6
alchemistmatt <matthew.monroe@pnnl.gov>
2022-04-29 16:43:00 -0700

    Add computed sort_key column to t_material_containers

--- dms/t_material_containers.sql
+++ dms/t_material_containers.sql
@@ -11,7 +11,17 @@ CREATE TABLE public.t_material_containers (
     location_id integer NOT NULL,
     created timestamp without time zone NOT NULL,
     status public.citext NOT NULL,
-    researcher public.citext
+    researcher public.citext,
+    sort_key integer GENERATED ALWAYS AS (
+CASE
+    WHEN (container OPERATOR(public.=) 'Staging'::public.citext) THEN 2147483645
+    WHEN (container OPERATOR(public.=) 'Met_Staging'::public.citext) THEN 2147483644
+    WHEN (container OPERATOR(public.~~) '%Staging%'::public.citext) THEN (2147483500 + char_length((container)::text))
+    WHEN (container OPERATOR(public.=) 'na'::public.citext) THEN 2147483500
+    WHEN (container OPERATOR(public.~) similar_to_escape('MC-[0-9]%'::text)) THEN ("substring"((container)::text, 4, 1000))::integer
+    WHEN (container OPERATOR(public.~~) 'Bin%'::public.citext) THEN char_length((container)::text)
+    ELSE (ascii("substring"((container)::text, 1, 1)) * 10000000)
+END) STORED
 );
 
 
