6da72ef6fbc1ba5063638ecdb1d5151cc23fa67b
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-10 21:08:01 -0700

    Add new fields and foreign keys to the operations tasks table

--- dms/t_operations_tasks.sql
+++ dms/t_operations_tasks.sql
@@ -4,12 +4,14 @@
 
 CREATE TABLE public.t_operations_tasks (
     task_id integer NOT NULL,
+    task_type_id integer DEFAULT 1 NOT NULL,
     tab public.citext,
     requester public.citext,
     requested_personnel public.citext,
     assigned_personnel public.citext,
     description public.citext NOT NULL,
     comments public.citext,
+    lab_id integer DEFAULT 100 NOT NULL,
     status public.citext DEFAULT 'Normal'::public.citext NOT NULL,
     priority public.citext,
     created timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
@@ -40,6 +42,20 @@ ALTER TABLE public.t_operations_tasks ALTER COLUMN task_id ADD GENERATED ALWAYS 
 
 ALTER TABLE ONLY public.t_operations_tasks
     ADD CONSTRAINT pk_t_operations_tasks PRIMARY KEY (task_id);
+
+--
+-- Name: t_operations_tasks fk_t_operations_tasks_t_lab_locations; Type: FK CONSTRAINT; Schema: public; Owner: d3l243
+--
+
+ALTER TABLE ONLY public.t_operations_tasks
+    ADD CONSTRAINT fk_t_operations_tasks_t_lab_locations FOREIGN KEY (lab_id) REFERENCES public.t_lab_locations(lab_id);
+
+--
+-- Name: t_operations_tasks fk_t_operations_tasks_t_operations_task_type; Type: FK CONSTRAINT; Schema: public; Owner: d3l243
+--
+
+ALTER TABLE ONLY public.t_operations_tasks
+    ADD CONSTRAINT fk_t_operations_tasks_t_operations_task_type FOREIGN KEY (task_type_id) REFERENCES public.t_operations_task_type(task_type_id);
 
 --
 -- Name: TABLE t_operations_tasks; Type: ACL; Schema: public; Owner: d3l243
