e10d9cf35b2f71db96f2feb0e59480964b716edd
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-29 20:49:31 -0700

    Add table to track capture task stats

---
+++ dms/cap/cap.t_capture_task_stats.sql
@@ -0,0 +1,21 @@
+--
+-- Name: t_capture_task_stats; Type: TABLE; Schema: cap; Owner: d3l243
+--
+
+CREATE TABLE cap.t_capture_task_stats (
+    script public.citext NOT NULL,
+    instrument public.citext NOT NULL,
+    year integer NOT NULL,
+    jobs integer NOT NULL
+);
+
+
+ALTER TABLE cap.t_capture_task_stats OWNER TO d3l243;
+
+--
+-- Name: t_capture_task_stats pk_t_capture_task_stats; Type: CONSTRAINT; Schema: cap; Owner: d3l243
+--
+
+ALTER TABLE ONLY cap.t_capture_task_stats
+    ADD CONSTRAINT pk_t_capture_task_stats PRIMARY KEY (script, instrument, year);
+
