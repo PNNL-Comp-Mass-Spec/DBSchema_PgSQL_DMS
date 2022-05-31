6a01d1b06a01089fbfe97f841e12850a0c0f45ca
alchemistmatt <matthew.monroe@pnnl.gov>
2022-05-30 18:43:54 -0700

    Rename table

--- dms/cap/cap.t_myemsl_test_uploads.sql
+++ dms/cap/cap.t_myemsl_test_uploads.sql
@@ -1,8 +1,8 @@
 --
--- Name: t_myemsl_testuploads; Type: TABLE; Schema: cap; Owner: d3l243
+-- Name: t_myemsl_test_uploads; Type: TABLE; Schema: cap; Owner: d3l243
 --
 
-CREATE TABLE cap.t_myemsl_testuploads (
+CREATE TABLE cap.t_myemsl_test_uploads (
     entry_id integer NOT NULL,
     job integer NOT NULL,
     dataset_id integer NOT NULL,
@@ -24,14 +24,14 @@ CREATE TABLE cap.t_myemsl_testuploads (
 );
 
 
-ALTER TABLE cap.t_myemsl_testuploads OWNER TO d3l243;
+ALTER TABLE cap.t_myemsl_test_uploads OWNER TO d3l243;
 
 --
--- Name: t_myemsl_testuploads_entry_id_seq; Type: SEQUENCE; Schema: cap; Owner: d3l243
+-- Name: t_myemsl_test_uploads_entry_id_seq; Type: SEQUENCE; Schema: cap; Owner: d3l243
 --
 
-ALTER TABLE cap.t_myemsl_testuploads ALTER COLUMN entry_id ADD GENERATED ALWAYS AS IDENTITY (
-    SEQUENCE NAME cap.t_myemsl_testuploads_entry_id_seq
+ALTER TABLE cap.t_myemsl_test_uploads ALTER COLUMN entry_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME cap.t_myemsl_test_uploads_entry_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -40,47 +40,47 @@ ALTER TABLE cap.t_myemsl_testuploads ALTER COLUMN entry_id ADD GENERATED ALWAYS 
 );
 
 --
--- Name: t_myemsl_testuploads pk_t_myemsl_testuploads; Type: CONSTRAINT; Schema: cap; Owner: d3l243
+-- Name: t_myemsl_test_uploads pk_t_myemsl_test_uploads; Type: CONSTRAINT; Schema: cap; Owner: d3l243
 --
 
-ALTER TABLE ONLY cap.t_myemsl_testuploads
-    ADD CONSTRAINT pk_t_myemsl_testuploads PRIMARY KEY (entry_id);
+ALTER TABLE ONLY cap.t_myemsl_test_uploads
+    ADD CONSTRAINT pk_t_myemsl_test_uploads PRIMARY KEY (entry_id);
 
 --
--- Name: ix_t_myemsl_testuploads_dataset_id_include_status_num; Type: INDEX; Schema: cap; Owner: d3l243
+-- Name: ix_t_myemsl_test_uploads_dataset_id_include_status_num; Type: INDEX; Schema: cap; Owner: d3l243
 --
 
-CREATE INDEX ix_t_myemsl_testuploads_dataset_id_include_status_num ON cap.t_myemsl_testuploads USING btree (dataset_id) INCLUDE (status_num);
+CREATE INDEX ix_t_myemsl_test_uploads_dataset_id_include_status_num ON cap.t_myemsl_test_uploads USING btree (dataset_id) INCLUDE (status_num);
 
 --
--- Name: ix_t_myemsl_testuploads_entered; Type: INDEX; Schema: cap; Owner: d3l243
+-- Name: ix_t_myemsl_test_uploads_entered; Type: INDEX; Schema: cap; Owner: d3l243
 --
 
-CREATE INDEX ix_t_myemsl_testuploads_entered ON cap.t_myemsl_testuploads USING btree (entered);
+CREATE INDEX ix_t_myemsl_test_uploads_entered ON cap.t_myemsl_test_uploads USING btree (entered);
 
 --
--- Name: ix_t_myemsl_testuploads_job; Type: INDEX; Schema: cap; Owner: d3l243
+-- Name: ix_t_myemsl_test_uploads_job; Type: INDEX; Schema: cap; Owner: d3l243
 --
 
-CREATE INDEX ix_t_myemsl_testuploads_job ON cap.t_myemsl_testuploads USING btree (job);
+CREATE INDEX ix_t_myemsl_test_uploads_job ON cap.t_myemsl_test_uploads USING btree (job);
 
 --
--- Name: ix_t_myemsl_testuploads_status_num_include_entry_id; Type: INDEX; Schema: cap; Owner: d3l243
+-- Name: ix_t_myemsl_test_uploads_status_num_include_entry_id; Type: INDEX; Schema: cap; Owner: d3l243
 --
 
-CREATE INDEX ix_t_myemsl_testuploads_status_num_include_entry_id ON cap.t_myemsl_testuploads USING btree (status_num) INCLUDE (entry_id);
+CREATE INDEX ix_t_myemsl_test_uploads_status_num_include_entry_id ON cap.t_myemsl_test_uploads USING btree (status_num) INCLUDE (entry_id);
 
 --
--- Name: t_myemsl_testuploads fk_t_myemsl_testuploads_t_uri_paths_content_uri; Type: FK CONSTRAINT; Schema: cap; Owner: d3l243
+-- Name: t_myemsl_test_uploads fk_t_myemsl_test_uploads_t_uri_paths_content_uri; Type: FK CONSTRAINT; Schema: cap; Owner: d3l243
 --
 
-ALTER TABLE ONLY cap.t_myemsl_testuploads
-    ADD CONSTRAINT fk_t_myemsl_testuploads_t_uri_paths_content_uri FOREIGN KEY (content_uri_path_id) REFERENCES cap.t_uri_paths(uri_path_id);
+ALTER TABLE ONLY cap.t_myemsl_test_uploads
+    ADD CONSTRAINT fk_t_myemsl_test_uploads_t_uri_paths_content_uri FOREIGN KEY (content_uri_path_id) REFERENCES cap.t_uri_paths(uri_path_id);
 
 --
--- Name: t_myemsl_testuploads fk_t_myemsl_testuploads_t_uri_paths_status_uri; Type: FK CONSTRAINT; Schema: cap; Owner: d3l243
+-- Name: t_myemsl_test_uploads fk_t_myemsl_test_uploads_t_uri_paths_status_uri; Type: FK CONSTRAINT; Schema: cap; Owner: d3l243
 --
 
-ALTER TABLE ONLY cap.t_myemsl_testuploads
-    ADD CONSTRAINT fk_t_myemsl_testuploads_t_uri_paths_status_uri FOREIGN KEY (status_uri_path_id) REFERENCES cap.t_uri_paths(uri_path_id);
+ALTER TABLE ONLY cap.t_myemsl_test_uploads
+    ADD CONSTRAINT fk_t_myemsl_test_uploads_t_uri_paths_status_uri FOREIGN KEY (status_uri_path_id) REFERENCES cap.t_uri_paths(uri_path_id);
 
