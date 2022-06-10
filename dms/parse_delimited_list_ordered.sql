66aa05b6d676d1ff2d9290775ab12db3419bb135
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-10 14:04:05 -0700

    Add parameter _maxRows to delimited list parsing function

--- dms/parse_delimited_list_ordered.sql
+++ dms/parse_delimited_list_ordered.sql
@@ -1,8 +1,8 @@
 --
--- Name: parse_delimited_list_ordered(text, text); Type: FUNCTION; Schema: public; Owner: d3l243
+-- Name: parse_delimited_list_ordered(text, text, integer); Type: FUNCTION; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION public.parse_delimited_list_ordered(_delimitedlist text, _delimiter text DEFAULT ','::text) RETURNS TABLE(entry_id integer, value text)
+CREATE OR REPLACE FUNCTION public.parse_delimited_list_ordered(_delimitedlist text, _delimiter text DEFAULT ','::text, _maxrows integer DEFAULT 0) RETURNS TABLE(entry_id integer, value text)
     LANGUAGE plpgsql
     AS $$
 /****************************************************
@@ -28,6 +28,7 @@ CREATE OR REPLACE FUNCTION public.parse_delimited_list_ordered(_delimitedlist te
 **  Date:   10/16/2007
 **          03/27/2013 mem - Now replacing Tab characters, carriage returns and line feeds with _delimiter
 **          01/14/2020 mem - Ported to PostgreSQL
+**          06/10/2022 mem - Added parameter _maxRows
 **
 ****************************************************/
 BEGIN
@@ -48,15 +49,26 @@ BEGIN
         End If;
     End If;
 
+    If Coalesce(_maxRows, 0) <= 0 Then
+        _maxRows = 2147483647;
+    End If;
+
     RETURN QUERY
     SELECT (Row_Number() OVER ())::int As entry_id, Trim(SplitQ.Value) AS ValueText
     FROM ( SELECT regexp_split_to_table(_delimitedList, _delimiter) AS Value
          ) SplitQ
-    WHERE COALESCE(Trim(SplitQ.Value), '') <> '';
+    WHERE COALESCE(Trim(SplitQ.Value), '') <> ''
+    LIMIT _maxRows;
 
 END
 $$;
 
 
-ALTER FUNCTION public.parse_delimited_list_ordered(_delimitedlist text, _delimiter text) OWNER TO d3l243;
+ALTER FUNCTION public.parse_delimited_list_ordered(_delimitedlist text, _delimiter text, _maxrows integer) OWNER TO d3l243;
+
+--
+-- Name: FUNCTION parse_delimited_list_ordered(_delimitedlist text, _delimiter text, _maxrows integer); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON FUNCTION public.parse_delimited_list_ordered(_delimitedlist text, _delimiter text, _maxrows integer) IS 'ParseDelimitedListOrdered';
 
