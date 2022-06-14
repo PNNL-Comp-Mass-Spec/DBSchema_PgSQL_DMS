e5064dacb9131a9b1ae66d3b756d30916fd7761a
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-13 18:29:42 -0700

    Dramatically improve query performance by moving function call to outside the filter query
    This query runs quickly on SQL Server, even when the call to get_research_team_user_role_list is inside the filter query
    On Postgres, moving the function call decreases query times from 2.5 minutes to under 2 seconds

--- dms/v_notification_sample_prep_request_by_research_team.sql
+++ dms/v_notification_sample_prep_request_by_research_team.sql
@@ -5,11 +5,11 @@
 CREATE VIEW public.v_notification_sample_prep_request_by_research_team AS
  SELECT DISTINCT tne.entry_id AS seq,
     tet.event_type AS event,
-    t_sample_prep_request.prep_request_id AS entity,
-    t_sample_prep_request.request_name AS name,
+    spr.prep_request_id AS entity,
+    spr.request_name AS name,
     t.campaign,
-    t.name AS "User",
-    t.role AS "Role",
+    t.name AS person,
+    public.get_research_team_user_role_list(t.team_id, t.user_id) AS person_role,
     tne.entered,
     tet.target_entity_type AS "#EntityType",
     t."#PRN",
@@ -18,18 +18,19 @@ CREATE VIEW public.v_notification_sample_prep_request_by_research_team AS
     tet.link_template
    FROM ((((public.t_notification_event tne
      JOIN public.t_notification_event_type tet ON ((tet.event_type_id = tne.event_type_id)))
-     JOIN public.t_sample_prep_request ON ((tne.target_id = t_sample_prep_request.prep_request_id)))
-     JOIN public.t_sample_prep_request_state_name ON ((t_sample_prep_request.state_id = t_sample_prep_request_state_name.state_id)))
+     JOIN public.t_sample_prep_request spr ON ((tne.target_id = spr.prep_request_id)))
+     JOIN public.t_sample_prep_request_state_name sprsn ON ((spr.state_id = sprsn.state_id)))
      JOIN ( SELECT t_campaign.campaign,
             t_users.name,
-            public.get_research_team_user_role_list(srtm.team_id, srtm.user_id) AS role,
-            t_users.username AS "#PRN"
+            t_users.username AS "#PRN",
+            srtm.team_id,
+            srtm.user_id
            FROM ((((public.t_campaign
              JOIN public.t_research_team ON ((t_campaign.research_team = t_research_team.team_id)))
              JOIN public.t_research_team_membership srtm ON ((t_research_team.team_id = srtm.team_id)))
              JOIN public.t_users ON ((srtm.user_id = t_users.user_id)))
              JOIN public.t_research_team_roles srtr ON ((srtm.role_id = srtr.role_id)))
-          WHERE ((t_campaign.state OPERATOR(public.=) 'Active'::public.citext) AND (t_users.active OPERATOR(public.=) 'Y'::public.citext))) t ON ((t.campaign OPERATOR(public.=) t_sample_prep_request.campaign)))
+          WHERE ((t_campaign.state OPERATOR(public.=) 'Active'::public.citext) AND (t_users.active OPERATOR(public.=) 'Y'::public.citext))) t ON ((t.campaign OPERATOR(public.=) spr.campaign)))
   WHERE ((tet.target_entity_type = 3) AND (tet.visible = 'Y'::bpchar));
 
 
