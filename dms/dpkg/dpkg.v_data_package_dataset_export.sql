c1cf8badadb8c0a7009828d7a93cfee9c4f969cf
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-18 20:34:43 -0700

    Add new views

---
+++ dms/dpkg/dpkg.v_data_package_dataset_export.sql
@@ -0,0 +1,18 @@
+--
+-- Name: v_data_package_dataset_export; Type: VIEW; Schema: dpkg; Owner: d3l243
+--
+
+CREATE VIEW dpkg.v_data_package_dataset_export AS
+ SELECT t_data_package_datasets.data_pkg_id,
+    t_data_package_datasets.dataset_id,
+    t_data_package_datasets.dataset,
+    t_data_package_datasets.experiment,
+    t_data_package_datasets.instrument,
+    t_data_package_datasets.created,
+    t_data_package_datasets.item_added,
+    t_data_package_datasets.package_comment
+   FROM dpkg.t_data_package_datasets;
+
+
+ALTER TABLE dpkg.v_data_package_dataset_export OWNER TO d3l243;
+
---
+++ dms/dpkg/dpkg.v_data_package_experiments_export.sql
@@ -0,0 +1,16 @@
+--
+-- Name: v_data_package_experiments_export; Type: VIEW; Schema: dpkg; Owner: d3l243
+--
+
+CREATE VIEW dpkg.v_data_package_experiments_export AS
+ SELECT t_data_package_experiments.data_pkg_id AS data_package_id,
+    t_data_package_experiments.experiment_id,
+    t_data_package_experiments.experiment,
+    t_data_package_experiments.created,
+    t_data_package_experiments.item_added,
+    t_data_package_experiments.package_comment
+   FROM dpkg.t_data_package_experiments;
+
+
+ALTER TABLE dpkg.v_data_package_experiments_export OWNER TO d3l243;
+
