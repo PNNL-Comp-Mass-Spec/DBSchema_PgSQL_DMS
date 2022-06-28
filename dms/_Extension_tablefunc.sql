2ad9c146c0f0b14ee465e4d4419cdc4cf542562a
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-27 18:22:19 -0700

    Add tablefunc extension and crosstab-based view

---
+++ dms/_Extension_tablefunc.sql
@@ -0,0 +1,12 @@
+--
+-- Name: tablefunc; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS tablefunc WITH SCHEMA public;
+
+--
+-- Name: EXTENSION tablefunc; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION tablefunc IS 'functions that manipulate whole tables, including crosstab';
+
---
+++ dms/cap/cap.v_job_step_backlog_crosstab.sql
@@ -0,0 +1,28 @@
+--
+-- Name: v_job_step_backlog_crosstab; Type: VIEW; Schema: cap; Owner: d3l243
+--
+
+CREATE VIEW cap.v_job_step_backlog_crosstab AS
+ SELECT ct.posting_time,
+    ct."ArchiveStatusCheck",
+    ct."ArchiveUpdate",
+    ct."ArchiveVerify",
+    ct."DatasetArchive",
+    ct."DatasetCapture",
+    ct."DatasetInfo",
+    ct."DatasetIntegrity",
+    ct."DatasetQuality",
+    ct."SourceFileRename",
+    ct."ImsDeMultiplex"
+   FROM public.crosstab('SELECT date_trunc(''minute'', posting_time) AS posting_time,
+           step_tool,
+           backlog_count
+    FROM cap.V_Job_Step_Backlog_History
+    ORDER  BY 1,2'::text, 'SELECT unnest(''{ArchiveStatusCheck, ArchiveUpdate, ArchiveVerify,
+                     DatasetArchive, DatasetCapture, DatasetInfo,
+                     DatasetIntegrity, DatasetQuality,
+                     SourceFileRename, ImsDeMultiplex}''::text[])'::text) ct(posting_time timestamp without time zone, "ArchiveStatusCheck" integer, "ArchiveUpdate" integer, "ArchiveVerify" integer, "DatasetArchive" integer, "DatasetCapture" integer, "DatasetInfo" integer, "DatasetIntegrity" integer, "DatasetQuality" integer, "SourceFileRename" integer, "ImsDeMultiplex" integer);
+
+
+ALTER TABLE cap.v_job_step_backlog_crosstab OWNER TO d3l243;
+
