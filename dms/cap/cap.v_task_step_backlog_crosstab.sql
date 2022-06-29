503120f1e4b0777544505aae74e92ea0c7184573
alchemistmatt <matthew.monroe@pnnl.gov>
2022-06-28 18:15:55 -0700

    Rename views

--- dms/cap/cap.v_task_step_backlog_crosstab.sql
+++ dms/cap/cap.v_task_step_backlog_crosstab.sql
@@ -1,8 +1,8 @@
 --
--- Name: v_job_step_backlog_crosstab; Type: VIEW; Schema: cap; Owner: d3l243
+-- Name: v_task_step_backlog_crosstab; Type: VIEW; Schema: cap; Owner: d3l243
 --
 
-CREATE VIEW cap.v_job_step_backlog_crosstab AS
+CREATE VIEW cap.v_task_step_backlog_crosstab AS
  SELECT ct.posting_time,
     ct."ArchiveStatusCheck",
     ct."ArchiveUpdate",
@@ -17,12 +17,12 @@ CREATE VIEW cap.v_job_step_backlog_crosstab AS
    FROM public.crosstab('SELECT date_trunc(''minute'', posting_time) AS posting_time,
            step_tool,
            backlog_count
-    FROM cap.V_Job_Step_Backlog_History
+    FROM cap.v_task_step_backlog_history
     ORDER  BY 1,2'::text, 'SELECT unnest(''{ArchiveStatusCheck, ArchiveUpdate, ArchiveVerify,
                      DatasetArchive, DatasetCapture, DatasetInfo,
                      DatasetIntegrity, DatasetQuality,
                      SourceFileRename, ImsDeMultiplex}''::text[])'::text) ct(posting_time timestamp without time zone, "ArchiveStatusCheck" integer, "ArchiveUpdate" integer, "ArchiveVerify" integer, "DatasetArchive" integer, "DatasetCapture" integer, "DatasetInfo" integer, "DatasetIntegrity" integer, "DatasetQuality" integer, "SourceFileRename" integer, "ImsDeMultiplex" integer);
 
 
-ALTER TABLE cap.v_job_step_backlog_crosstab OWNER TO d3l243;
+ALTER TABLE cap.v_task_step_backlog_crosstab OWNER TO d3l243;
 
--- dms/cap/cap.v_task_step_backlog_history.sql
+++ dms/cap/cap.v_task_step_backlog_history.sql
@@ -1,8 +1,8 @@
 --
--- Name: v_job_step_backlog_history; Type: VIEW; Schema: cap; Owner: d3l243
+-- Name: v_task_step_backlog_history; Type: VIEW; Schema: cap; Owner: d3l243
 --
 
-CREATE VIEW cap.v_job_step_backlog_history AS
+CREATE VIEW cap.v_task_step_backlog_history AS
  SELECT t_task_step_status_history.step_tool,
     t_task_step_status_history.posting_time,
     sum(t_task_step_status_history.step_count) AS backlog_count
@@ -11,5 +11,5 @@ CREATE VIEW cap.v_job_step_backlog_history AS
   GROUP BY t_task_step_status_history.step_tool, t_task_step_status_history.posting_time;
 
 
-ALTER TABLE cap.v_job_step_backlog_history OWNER TO d3l243;
+ALTER TABLE cap.v_task_step_backlog_history OWNER TO d3l243;
 
