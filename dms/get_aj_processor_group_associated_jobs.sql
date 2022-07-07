9e9acafa20e22e2c60cb20aebac5c9439aed55a7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-06 21:39:42 -0700

    Move Group By queries into subqueries

--- dms/get_aj_processor_group_associated_jobs.sql
+++ dms/get_aj_processor_group_associated_jobs.sql
@@ -19,6 +19,7 @@ CREATE OR REPLACE FUNCTION public.get_aj_processor_group_associated_jobs(_groupi
 **  Date:   02/16/2007
 **          02/23/2007 mem - Added parameter _jobStateFilter
 **          06/17/2022 mem - Ported to PostgreSQL
+**          07/06/2022 mem - Move Group By queries into subqueries
 **
 *****************************************************/
 DECLARE
@@ -29,50 +30,56 @@ BEGIN
     _result := '';
 
     If _jobStateFilter = 0 Then
-        SELECT string_agg(JS.job_state || ': ' || COUNT(AJPGA.job)::text, ', ' ORDER BY AJ.job_state_id)
+        SELECT string_agg(CountQ.job_state || ': ' || CountQ.Jobs::text, ', ' ORDER BY CountQ.job_state_id)
         INTO _result
-        FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
-             t_analysis_job AJ ON AJPGA.job = AJ.job INNER JOIN
-             t_analysis_job_state JS ON AJ.job_state_id = JS.job_state_id
-        WHERE AJPGA.group_id = _groupID AND AJ.job_state_id IN (1, 8, 10)
-        GROUP BY JS.job_state, AJ.job_state_id;
+        FROM (
+            SELECT JS.job_state, J.job_state_id, COUNT(AJPGA.job) As Jobs
+            FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
+                 t_analysis_job J ON AJPGA.job = J.job INNER JOIN
+                 t_analysis_job_state JS ON J.job_state_id = JS.job_state_id
+            WHERE AJPGA.group_id = _groupID AND J.job_state_id IN (1, 8, 10)
+            GROUP BY JS.job_state, J.job_state_id) CountQ;
     End If;
 
     If _jobStateFilter = 1 Then
-        SELECT string_agg(JS.job_state + ': ' + COUNT(AJPGA.job)::text, ', ' ORDER BY AJ.job_state_id)
+        SELECT string_agg(CountQ.job_state || ': ' || CountQ.Jobs::text, ', ' ORDER BY CountQ.job_state_id)
         INTO _resultAppend
-        FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
-            t_analysis_job AJ ON AJPGA.job = AJ.job INNER JOIN
-            t_analysis_job_state JS ON AJ.job_state_id = JS.job_state_id
-        WHERE AJPGA.group_id = _groupID AND AJ.job_state_id IN (1, 2, 3, 8, 9, 10, 11, 16, 17)
-        GROUP BY JS.job_state, AJ.job_state_id;
+        FROM (
+            SELECT JS.job_state, J.job_state_id, COUNT(AJPGA.job) As Jobs
+            FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
+                t_analysis_job J ON AJPGA.job = J.job INNER JOIN
+                t_analysis_job_state JS ON J.job_state_id = JS.job_state_id
+            WHERE AJPGA.group_id = _groupID AND J.job_state_id IN (1, 2, 3, 8, 9, 10, 11, 16, 17)
+            GROUP BY JS.job_state, J.job_state_id) CountQ;
 
         If Coalesce(_resultAppend, '') <> '' Then
             If _result <> '' THEN
                 _result := _result || ', ';
             End If;
 
-            _result := _result || ', ' || _resultAppend;
+            _result := _result || _resultAppend;
         End If;
     End If;
 
     If _jobStateFilter Not In (0, 1) Then
         _resultAppend := '';
 
-        SELECT string_agg(JS.job_state + ': ' + COUNT(AJPGA.job)::text, ', ' ORDER BY AJ.job_state_id)
+        SELECT string_agg(CountQ.job_state || ': ' || CountQ.Jobs::text, ', ' ORDER BY CountQ.job_state_id)
         INTO _resultAppend
-        FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
-             t_analysis_job AJ ON AJPGA.job = AJ.job INNER JOIN
-             t_analysis_job_state JS ON AJ.job_state_id = JS.job_state_id
-        WHERE AJPGA.group_id = _groupID
-        GROUP BY JS.job_state, AJ.job_state_id;
+        FROM (
+            SELECT JS.job_state, J.job_state_id, COUNT(AJPGA.job) AS Jobs
+            FROM t_analysis_job_processor_group_associations AJPGA INNER JOIN
+                 t_analysis_job J ON AJPGA.job = J.job INNER JOIN
+                 t_analysis_job_state JS ON J.job_state_id = JS.job_state_id
+            WHERE AJPGA.group_id = _groupID
+            GROUP BY JS.job_state, J.job_state_id) CountQ;
 
         If Coalesce(_resultAppend, '') <> '' Then
             If _result <> '' THEN
                 _result := _result || ', ';
             End If;
 
-            _result := _result || ', ' || _resultAppend;
+            _result := _result || _resultAppend;
         End If;
     End If;
 
