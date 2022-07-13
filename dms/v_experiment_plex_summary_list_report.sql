e1e1a031085c121e01c3a7cd76423984d9253993
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-12 18:29:57 -0700

    Add new views

---
+++ dms/v_experiment_plex_summary_list_report.sql
@@ -0,0 +1,38 @@
+--
+-- Name: v_experiment_plex_summary_list_report; Type: VIEW; Schema: public; Owner: d3l243
+--
+
+CREATE VIEW public.v_experiment_plex_summary_list_report AS
+ SELECT plexmembers.plex_exp_id,
+    e.experiment AS plex_experiment,
+    c.campaign,
+    org.organism,
+    count(*) AS channels,
+    sum(
+        CASE
+            WHEN (channeltypename.channel_type_name OPERATOR(public.=) 'Reference'::public.citext) THEN 1
+            ELSE 0
+        END) AS ref_channels,
+    e.labelling,
+    min(plexmembers.entered) AS created,
+    bto.tissue,
+    e.sample_prep_request_id AS request,
+    e.created AS plex_exp_created
+   FROM ((((((public.t_experiment_plex_members plexmembers
+     JOIN public.t_experiment_plex_channel_type_name channeltypename ON ((plexmembers.channel_type_id = channeltypename.channel_type_id)))
+     JOIN public.t_experiments e ON ((plexmembers.plex_exp_id = e.exp_id)))
+     JOIN public.t_organisms org ON ((e.organism_id = org.organism_id)))
+     JOIN public.t_campaign c ON ((e.campaign_id = c.campaign_id)))
+     LEFT JOIN ont.v_bto_id_to_name bto ON ((e.tissue_id OPERATOR(public.=) bto.identifier)))
+     LEFT JOIN public.t_sample_labelling_reporter_ions reporterions ON (((plexmembers.channel = reporterions.channel) AND (e.labelling OPERATOR(public.=) reporterions.label))))
+  GROUP BY plexmembers.plex_exp_id, e.experiment, org.organism, e.labelling, e.created, c.campaign, bto.tissue, e.sample_prep_request_id;
+
+
+ALTER TABLE public.v_experiment_plex_summary_list_report OWNER TO d3l243;
+
+--
+-- Name: TABLE v_experiment_plex_summary_list_report; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.v_experiment_plex_summary_list_report TO readaccess;
+
---
+++ dms/v_experiment_stats_list_report.sql
@@ -0,0 +1,34 @@
+--
+-- Name: v_experiment_stats_list_report; Type: VIEW; Schema: public; Owner: d3l243
+--
+
+CREATE VIEW public.v_experiment_stats_list_report AS
+ SELECT countq.year,
+    countq.month,
+    countq.experiments,
+    countq.researcher
+   FROM ( SELECT EXTRACT(month FROM e.created) AS month,
+            EXTRACT(year FROM e.created) AS year,
+            count(*) AS experiments,
+            'Total'::public.citext AS researcher
+           FROM public.t_experiments e
+          GROUP BY (EXTRACT(month FROM e.created)), (EXTRACT(year FROM e.created))
+        UNION
+         SELECT EXTRACT(month FROM e.created) AS month,
+            EXTRACT(year FROM e.created) AS year,
+            count(*) AS experiments,
+            u.name AS researcher
+           FROM (public.t_experiments e
+             JOIN public.t_users u ON ((e.researcher_prn OPERATOR(public.=) u.username)))
+          GROUP BY (EXTRACT(month FROM e.created)), (EXTRACT(year FROM e.created)), u.name) countq
+  ORDER BY countq.year DESC, countq.month DESC, countq.experiments DESC;
+
+
+ALTER TABLE public.v_experiment_stats_list_report OWNER TO d3l243;
+
+--
+-- Name: TABLE v_experiment_stats_list_report; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.v_experiment_stats_list_report TO readaccess;
+
