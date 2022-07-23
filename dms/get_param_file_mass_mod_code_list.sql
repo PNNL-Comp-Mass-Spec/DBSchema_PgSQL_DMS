efe696039a9142dbb45a4580b645e05d9e37c437
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-22 20:21:02 -0700

    Change delimiter to a comma

--- dms/get_param_file_mass_mod_code_list.sql
+++ dms/get_param_file_mass_mod_code_list.sql
@@ -14,18 +14,19 @@ CREATE OR REPLACE FUNCTION public.get_param_file_mass_mod_code_list(_paramfileid
 **  Auth:   mem
 **  Date:   11/04/2021 mem - Initial version
 **          06/22/2022 mem - Ported to PostgreSQL
+**          07/22/2022 mem - Change the delimiter to a comma
 **
 *****************************************************/
 DECLARE
     _result text;
 BEGIN
     If _includeSymbol > 0 Then
-        SELECT string_agg(Mod_Code_With_Symbol, '; ' ORDER BY Mod_Code_With_Symbol)
+        SELECT string_agg(Mod_Code_With_Symbol, ', ' ORDER BY Mod_Code_With_Symbol)
         INTO _result
         FROM V_Param_File_Mass_Mod_Info
         WHERE Param_File_ID = _paramFileId;
     Else
-        SELECT string_agg(Mod_Code, '; ' ORDER BY Mod_Code)
+        SELECT string_agg(Mod_Code, ', ' ORDER BY Mod_Code)
         INTO _result
         FROM V_Param_File_Mass_Mod_Info
         WHERE Param_File_ID = _paramFileId;
