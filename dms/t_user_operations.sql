2bc94ec23e127fd337228ebfb5d7a429c520054b
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-22 20:21:12 -0700

    Add unique constraint index

--- dms/t_user_operations.sql
+++ dms/t_user_operations.sql
@@ -32,6 +32,12 @@ ALTER TABLE ONLY public.t_user_operations
     ADD CONSTRAINT pk_t_user_operations PRIMARY KEY (operation_id);
 
 --
+-- Name: ix_t_user_operations_unique_operation; Type: INDEX; Schema: public; Owner: d3l243
+--
+
+CREATE UNIQUE INDEX ix_t_user_operations_unique_operation ON public.t_user_operations USING btree (operation);
+
+--
 -- Name: TABLE t_user_operations; Type: ACL; Schema: public; Owner: d3l243
 --
 
