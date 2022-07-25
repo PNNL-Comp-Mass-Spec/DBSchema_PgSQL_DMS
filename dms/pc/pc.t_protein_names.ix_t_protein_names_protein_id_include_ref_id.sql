423a84a7cbc7a18f1dc56cbbace8d67fd56e523a
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-25 14:40:23 -0700

    Remove standalone index definition file

--- dms/pc/pc.t_protein_names.ix_t_protein_names_protein_id_include_ref_id.sql
+++
@@ -1,6 +0,0 @@
---
--- Name: t_protein_names.ix_t_protein_names_protein_id_include_ref_id; Type: INDEX; Schema: pc; Owner: d3l243
---
-
-CREATE INDEX "t_protein_names.ix_t_protein_names_protein_id_include_ref_id" ON pc.t_protein_names USING btree (protein_id) INCLUDE (reference_id, name, description, annotation_type_id);
-
