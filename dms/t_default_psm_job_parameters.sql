cb7a115a434bb1a66297bc21d14afd5740086adc
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-28 17:06:19 -0700

    Change primary key to entry_id and add a unique constraint index

--- dms/t_default_psm_job_parameters.sql
+++ dms/t_default_psm_job_parameters.sql
@@ -3,8 +3,8 @@
 --
 
 CREATE TABLE public.t_default_psm_job_parameters (
-    job_type_name public.citext NOT NULL,
     entry_id integer NOT NULL,
+    job_type_name public.citext NOT NULL,
     tool_name public.citext NOT NULL,
     dyn_met_ox smallint NOT NULL,
     stat_cys_alk smallint NOT NULL,
@@ -34,7 +34,13 @@ ALTER TABLE public.t_default_psm_job_parameters ALTER COLUMN entry_id ADD GENERA
 --
 
 ALTER TABLE ONLY public.t_default_psm_job_parameters
-    ADD CONSTRAINT pk_t_default_psm_job_parameters PRIMARY KEY (job_type_name, tool_name, dyn_met_ox, stat_cys_alk, dyn_sty_phos);
+    ADD CONSTRAINT pk_t_default_psm_job_parameters PRIMARY KEY (entry_id);
+
+--
+-- Name: ix_t_default_psm_job_parameters_uniq_type_tool_met_ox_cys_alk; Type: INDEX; Schema: public; Owner: d3l243
+--
+
+CREATE UNIQUE INDEX ix_t_default_psm_job_parameters_uniq_type_tool_met_ox_cys_alk ON public.t_default_psm_job_parameters USING btree (job_type_name, tool_name, dyn_met_ox, stat_cys_alk, dyn_sty_phos);
 
 --
 -- Name: t_default_psm_job_parameters fk_t_default_psm_job_parameters_t_analysis_tool; Type: FK CONSTRAINT; Schema: public; Owner: d3l243
