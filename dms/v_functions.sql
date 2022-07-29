e901be02453a22e79a7d2ede65a093a91457117c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-07-29 12:49:21 -0700

    Add view that lists user-defined PostgreSQL functions and procedures

---
+++ dms/v_functions.sql
@@ -0,0 +1,29 @@
+--
+-- Name: v_functions; Type: VIEW; Schema: public; Owner: d3l243
+--
+
+CREATE VIEW public.v_functions AS
+ SELECT n.nspname AS schema,
+    (p.proname)::public.citext AS name,
+        CASE
+            WHEN (p.prokind = 'f'::"char") THEN 'function'::text
+            WHEN (p.prokind = 'p'::"char") THEN 'procedure'::text
+            WHEN (p.prokind = 'a'::"char") THEN 'aggregate function'::text
+            WHEN (p.prokind = 'w'::"char") THEN 'window function'::text
+            ELSE (p.prokind)::text
+        END AS function_type,
+    p.proargnames AS arguments,
+    p.oid
+   FROM (pg_proc p
+     LEFT JOIN pg_namespace n ON ((p.pronamespace = n.oid)))
+  WHERE (n.nspname <> ALL (ARRAY['pg_catalog'::name, 'information_schema'::name]));
+
+
+ALTER TABLE public.v_functions OWNER TO d3l243;
+
+--
+-- Name: TABLE v_functions; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.v_functions TO readaccess;
+
