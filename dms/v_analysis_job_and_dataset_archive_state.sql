0d89ead36961ac7063793e7aa484813189c0370c
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-05 21:34:55 -0700

    Add dataset_id column to view and swith to left outer join

--- dms/v_analysis_job_and_dataset_archive_state.sql
+++ dms/v_analysis_job_and_dataset_archive_state.sql
@@ -10,12 +10,13 @@ CREATE VIEW public.v_analysis_job_and_dataset_archive_state AS
             WHEN ((j.job_state_id = 1) AND (da.archive_state_id > 3)) THEN ((((js.job_state)::text || ' (Dataset '::text) || (dasn.archive_state)::text) || ')'::text)
             ELSE (js.job_state)::text
         END AS job_state,
-    COALESCE(dasn.archive_state, ''::public.citext) AS dataset_archive_state
-   FROM ((public.t_dataset_archive_state_name dasn
-     JOIN public.t_dataset_archive da ON ((dasn.archive_state_id = da.archive_state_id)))
-     RIGHT JOIN ((public.t_analysis_job j
+    COALESCE(dasn.archive_state, ''::public.citext) AS dataset_archive_state,
+    j.dataset_id
+   FROM ((((public.t_analysis_job j
      JOIN public.t_analysis_job_state js ON ((j.job_state_id = js.job_state_id)))
-     JOIN public.t_dataset d ON ((j.dataset_id = d.dataset_id))) ON ((da.dataset_id = d.dataset_id)));
+     JOIN public.t_dataset d ON ((j.dataset_id = d.dataset_id)))
+     LEFT JOIN public.t_dataset_archive da ON ((da.dataset_id = j.dataset_id)))
+     LEFT JOIN public.t_dataset_archive_state_name dasn ON ((dasn.archive_state_id = da.archive_state_id)));
 
 
 ALTER TABLE public.v_analysis_job_and_dataset_archive_state OWNER TO d3l243;
