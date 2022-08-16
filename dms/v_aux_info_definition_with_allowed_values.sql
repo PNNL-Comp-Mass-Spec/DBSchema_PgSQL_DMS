cb6fa01258fdf251bf6e2cb5337eb631997ff7a3
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-16 15:21:20 -0700

    Add new aux info definition view

---
+++ dms/v_aux_info_definition_with_allowed_values.sql
@@ -0,0 +1,31 @@
+--
+-- Name: v_aux_info_definition_with_allowed_values; Type: VIEW; Schema: public; Owner: d3l243
+--
+
+CREATE VIEW public.v_aux_info_definition_with_allowed_values AS
+ SELECT category_target.aux_target AS target,
+    category.aux_category AS category,
+    subcategory.aux_subcategory AS subcategory,
+    item.aux_description AS item,
+    item.aux_description_id AS item_id,
+    public.get_aux_info_allowed_values(item.aux_description_id) AS allowed_values,
+    category.sequence AS sc,
+    subcategory.sequence AS ss,
+    item.sequence AS si,
+    item.data_size,
+    item.helper_append
+   FROM (((public.t_aux_info_category category
+     JOIN public.t_aux_info_subcategory subcategory ON ((category.aux_category_id = subcategory.aux_category_id)))
+     JOIN public.t_aux_info_description item ON ((subcategory.aux_subcategory_id = item.aux_subcategory_id)))
+     JOIN public.t_aux_info_target category_target ON ((category.target_type_id = category_target.aux_target_id)))
+  WHERE (item.active = 'Y'::bpchar);
+
+
+ALTER TABLE public.v_aux_info_definition_with_allowed_values OWNER TO d3l243;
+
+--
+-- Name: TABLE v_aux_info_definition_with_allowed_values; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.v_aux_info_definition_with_allowed_values TO readaccess;
+
