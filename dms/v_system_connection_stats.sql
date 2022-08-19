bfe49cbc0c7a0e2e452f40c3617f9a9e2b514dc1
alchemistmatt <matthew.monroe@pnnl.gov>
2022-08-18 17:27:18 -0700

    Update connection stats view to properly rollup stats across all databases when used by a user who is not a superuser

--- dms/v_system_connection_stats.sql
+++ dms/v_system_connection_stats.sql
@@ -3,14 +3,21 @@
 --
 
 CREATE VIEW public.v_system_connection_stats AS
- SELECT pg_stat_activity.datname AS database,
-    count(*) AS open,
-    count(*) FILTER (WHERE (pg_stat_activity.state = 'active'::text)) AS active,
-    count(*) FILTER (WHERE (pg_stat_activity.state = 'idle'::text)) AS idle,
-    count(*) FILTER (WHERE (pg_stat_activity.state = 'idle in transaction'::text)) AS idle_in_transaction
-   FROM pg_stat_activity
-  WHERE (pg_stat_activity.backend_type = 'client backend'::text)
-  GROUP BY ROLLUP(pg_stat_activity.datname);
+ SELECT COALESCE(countq.database, '<All DBs>'::name) AS database,
+    countq.open,
+    countq.active,
+    countq.idle,
+    countq.idle_in_transaction
+   FROM ( SELECT filterq.datname AS database,
+            count(*) AS open,
+            count(*) FILTER (WHERE (filterq.state = 'active'::text)) AS active,
+            count(*) FILTER (WHERE (filterq.state = 'idle'::text)) AS idle,
+            count(*) FILTER (WHERE (filterq.state = 'idle in transaction'::text)) AS idle_in_transaction
+           FROM ( SELECT pg_stat_activity.datname,
+                    pg_stat_activity.state
+                   FROM pg_stat_activity
+                  WHERE ((pg_stat_activity.backend_type = 'client backend'::text) OR ((pg_stat_activity.backend_type IS NULL) AND (COALESCE(pg_stat_activity.usename, 'postgres'::name) <> 'postgres'::name)))) filterq
+          GROUP BY ROLLUP(filterq.datname)) countq;
 
 
 ALTER TABLE public.v_system_connection_stats OWNER TO d3l243;
