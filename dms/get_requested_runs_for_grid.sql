4fee3f39f3607e40fa8a4fcbafedb354ab401d67
alchemistmatt <matthew.monroe@pnnl.gov>
2022-10-25 21:07:51 -0700

    Add new functions and procedures

---
+++ dms/get_requested_runs_for_grid.sql
@@ -0,0 +1,55 @@
+--
+-- Name: get_requested_runs_for_grid(text); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE FUNCTION public.get_requested_runs_for_grid(_itemlist text) RETURNS TABLE(request integer, name public.citext, status public.citext, batchid integer, instrument public.citext, separation_type public.citext, experiment public.citext, cart public.citext, "column" smallint, block integer, run_order integer)
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Returns the info for the requested run IDs in itemList
+**
+**  Auth:   grk
+**  Date:   01/13/2013
+**          01/13/2013 grk - initial release
+**          03/14/2013 grk - removed "Active" status filter
+**          10/19/2020 mem - Rename the instrument group column to RDS_instrument_group
+**          10/25/2022 mem - Ported to PostgreSQL
+**
+*****************************************************/
+BEGIN
+    RETURN QUERY
+    SELECT TRR.request_id AS Request,
+           TRR.request_name AS Name,
+           TRR.state_name AS Status,
+           TRR.batch_id AS BatchID,
+           TRR.instrument_group AS Instrument,
+           TRR.separation_group AS Separation_Type,
+           TEX.experiment AS Experiment,
+           t_lc_cart.cart_name AS Cart,
+           TRR.cart_column AS "Column",
+           TRR.Block AS Block,
+           TRR.run_order AS Run_Order
+    FROM t_requested_run TRR
+         INNER JOIN t_lc_cart
+           ON TRR.cart_id = t_lc_cart.cart_id
+         INNER JOIN t_requested_run_batches TRB
+           ON TRR.batch_id = TRB.batch_id
+         INNER JOIN t_experiments TEX
+           ON TRR.exp_id = TEX.exp_id
+         INNER JOIN ( SELECT Value FROM public.parse_delimited_integer_list(_itemList)) RequestQ
+           ON TRR.request_id = RequestQ.Value;
+
+END
+$$;
+
+
+ALTER FUNCTION public.get_requested_runs_for_grid(_itemlist text) OWNER TO d3l243;
+
+--
+-- Name: FUNCTION get_requested_runs_for_grid(_itemlist text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON FUNCTION public.get_requested_runs_for_grid(_itemlist text) IS 'GetRequestedRunsForGrid';
+
---
+++ dms/getrequestedrunsforgrid.sql
@@ -0,0 +1,92 @@
+--
+-- Name: getrequestedrunsforgrid(text, refcursor, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.getrequestedrunsforgrid(IN _itemlist text, INOUT _results refcursor DEFAULT '_results'::refcursor, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Returns the info for the requested run IDs in itemList
+**
+**  Arguments:
+**    _itemList          Comma separated list of requested run IDs
+**    _results           Cursor for retrieving the results
+**    _message           Output: message (if an error)
+**    _returnCode        Output: return code (if an error)
+**                       supports the % wildcard
+**
+**  Use this to view the data returned by the _results cursor
+**
+**      BEGIN;
+**          CALL public.GetRequestedRunsForGrid (
+**              _itemList => '123456,123457,323457,823457,229733,225708'
+**          );
+**          FETCH ALL FROM _results;
+**      END;
+**
+**  Auth:   mem
+**  Date:   10/25/2022 mem - Initial version
+
+*****************************************************/
+DECLARE
+    _sqlstate text;
+    _exceptionMessage text;
+    _exceptionDetail text;
+    _exceptionContext text;
+BEGIN
+
+    -----------------------------------------------
+    -- Validate the inputs
+    -----------------------------------------------
+    --
+    _itemList := Coalesce(_itemList, '');
+
+    _message := '';
+    _returnCode := '';
+
+    RAISE INFO '%', _message;
+
+    Open _results For
+        SELECT request,
+            name,
+            status,
+            batchID,
+            instrument,
+            separation_Type,
+            experiment,
+            cart,
+            "column",
+            block,
+            run_order
+        FROM public.get_requested_runs_for_grid(_itemList);
+
+EXCEPTION
+    WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS
+            _sqlState         = returned_sqlstate,
+            _exceptionMessage = message_text,
+            _exceptionDetail  = pg_exception_detail,
+            _exceptionContext = pg_exception_context;
+
+    _message := local_error_handler (
+                    _sqlState, _exceptionMessage, _exceptionDetail, _exceptionContext,
+                    _callingProcLocation => '', _logError => true);
+
+    If Coalesce(_returnCode, '') = '' Then
+        _returnCode := _sqlState;
+    End If;
+
+END
+$$;
+
+
+ALTER PROCEDURE public.getrequestedrunsforgrid(IN _itemlist text, INOUT _results refcursor, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE getrequestedrunsforgrid(IN _itemlist text, INOUT _results refcursor, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.getrequestedrunsforgrid(IN _itemlist text, INOUT _results refcursor, INOUT _message text, INOUT _returncode text) IS 'GetRequestedRunsForGrid returns results from function get_requested_runs_for_grid';
+
