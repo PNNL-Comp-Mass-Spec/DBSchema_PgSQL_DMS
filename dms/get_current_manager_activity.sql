f8206a7b0dd2e9d6962790c7714d9ec4b2cb49b8
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-17 17:03:48 -0800

    Update manager activity function to use DMS_Capture and DMS_Pipeline

--- dms/get_current_manager_activity.sql
+++ dms/get_current_manager_activity.sql
@@ -1,21 +1,26 @@
 --
--- Name: get_current_manager_activity(integer, timestamp without time zone); Type: FUNCTION; Schema: public; Owner: d3l243
+-- Name: get_current_manager_activity(boolean, timestamp without time zone, integer); Type: FUNCTION; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION public.get_current_manager_activity(_monthrange integer DEFAULT 3, _jobfinishend timestamp without time zone DEFAULT NULL::timestamp without time zone) RETURNS TABLE(source text, "When" timestamp without time zone, who text, what text)
+CREATE OR REPLACE FUNCTION public.get_current_manager_activity(_activeonly boolean DEFAULT true, _startdate timestamp without time zone DEFAULT NULL::timestamp without time zone, _months integer DEFAULT 3) RETURNS TABLE(source public.citext, who public.citext, what public.citext, status_date timestamp without time zone)
     LANGUAGE plpgsql
     AS $$
 /****************************************************
 **
 **  Desc:
-**      Get snapshot of current activity of managers
+**      Get snapshot of current manager activity
 **
 **  Arguments:
-**    _monthRange       Number of months to examine, backward from either the current date, or from _jobFinishEnd
-**    _jobFinishEnd     When null, examine activity from the current date, backward _monthRange months
-**                      When a valid timestamp, examine activity from the given date, backward _monthRange months
+**    _activeOnly       When true, only show processors actively running a capture task job or analysis job
+**                      When false, use _startDate and _months to show all processors that were active within a date range
+**    _startDate        Start date to look for processors that ran at least one capture task job or analysis job; ignored if _activeOnly is true
+**                      If null, will auto-compute using the current time and _months
+**    _months           Number of months to examine, starting with _startDate; ignored if _activeOnly is true
+**
 **  Example usage:
-**    SELECT * FROM get_current_manager_activity(_jobFinishEnd => '2009-01-01');
+**    SELECT * FROM get_current_manager_activity(_activeOnly => true)
+**    SELECT * FROM get_current_manager_activity(_activeOnly => false, _startDate => '2022-07-01');
+**    SELECT * FROM get_current_manager_activity(_activeOnly => false, _startDate => '2022-07-01', _months => 1);
 **
 **  Auth:   grk
 **  Date:   10/06/2003 grk - Initial version
@@ -31,140 +36,178 @@ CREATE OR REPLACE FUNCTION public.get_current_manager_activity(_monthrange integ
 **          10/27/2022 mem - Change # column to lowercase
 **          11/02/2022 mem - Remove # from column name
 **          11/16/2022 mem - Ported to PostgreSQL
+**          11/17/2022 mem - Updated to use DMS_Capture and DMS_Pipeline
 **
 *****************************************************/
 DECLARE
-    _jobFinishStart timestamp;
+    _endDate timestamp;
 BEGIN
+    _activeOnly := Coalesce(_activeOnly, true);
 
-    _monthRange := Coalesce(_monthRange, 3);
-    _jobFinishEnd := Coalesce(_jobFinishEnd, CURRENT_TIMESTAMP);
+    _months := Coalesce(_months, 3);
+    If (_months < 1) Then
+        _months := 1;
+    End If;
 
-    _jobFinishStart = _jobFinishEnd - make_interval(months => _monthRange);
+    _startDate := Coalesce(_startDate, CURRENT_TIMESTAMP - make_interval(months => _months));
 
-    -- Temporary table to hold accumulated results
+    _endDate = _startDate + make_interval(months => _months);
+
+    -- Temporary table to hold manager status
     --
     CREATE TEMP TABLE Tmp_ManagerActivity (
-        Source text,
-        "When" timestamp,
-        Who  text,
-        What text
+        Source citext,
+        Status_Date timestamp,
+        Who  citext,
+        What citext
     );
 
-    RAISE INFO 'Examining jobs started betwen % and %', _jobFinishStart::date, _jobFinishEnd::date;
+    -- RAISE INFO 'Examining capture tasks and analysis jobs started betwen % and %', _startDate::date, _endDate::date;
 
-    -- Populate temporary table with known analysis managers
+    If _activeOnly Then
+        INSERT INTO Tmp_ManagerActivity(Who, What, Status_Date, Source)
+        SELECT RankQ.Who,
+               RankQ.What,
+               RankQ.Start,
+               'Analysis Jobs' as Source
+        FROM ( SELECT 'Analysis: ' || Processor As Who,
+                      format('Processing job %s, step %s, tool %s', job, step, step_tool) AS What,
+                      Start,
+                      Row_Number() Over (Partition By Processor Order By Start Desc) As StartRank
+               FROM sw.t_job_steps
+               WHERE State = 4
+             ) RankQ
+        WHERE RankQ.StartRank = 1;
+
+        INSERT INTO Tmp_ManagerActivity(Who, What, Status_Date, Source)
+        SELECT RankQ.Who,
+               RankQ.What,
+               RankQ.Start,
+               'Capture Tasks' as Source
+        FROM ( SELECT 'Capture: ' || Processor As Who,
+                      format('Processing task %s, step %s, tool %s', job, step, step_tool) AS What,
+                      Start,
+                      Row_Number() Over (Partition By Processor Order By Start Desc) As StartRank
+               FROM sw.t_job_steps
+               WHERE State = 4
+             ) RankQ
+        WHERE RankQ.StartRank = 1;
+
+        RETURN QUERY
+        SELECT
+            A.Source,
+            A.Who,
+            A.What,
+            A.Status_Date
+        FROM Tmp_ManagerActivity A
+        ORDER BY A.Who;
+
+        DROP TABLE Tmp_ManagerActivity;
+        RETURN;
+    End If;
+
+    -- Populate temporary table with analysis managers that started a job within the date range
     --
-    INSERT INTO Tmp_ManagerActivity("When", Who, What, Source)
-    SELECT M."When",
-           'Analysis: ' || M.assigned_processor_name AS Who,
-           format('Nothing in health log or in process, but active between %s and %s', _jobFinishStart::date, _jobFinishEnd::date) AS What,
-           'Historic' as Source
-    FROM (  -- get distinct list of analysis managers
-            -- that have been active in the previous three months
+    INSERT INTO Tmp_ManagerActivity(Who, What, Status_Date, Source)
+    SELECT 'Analysis: ' || M.processor AS Who,
+           'Idle' AS What,
+           M.Most_Recent_Start,
+           'Analysis Jobs' as Source
+    FROM (  -- Get distinct list of analysis managers
+            -- that have been active within the date range
             --
-            SELECT COALESCE(assigned_processor_name, '(unknown)') as assigned_processor_name,
-                   COALESCE(MAX(start), make_date(2003, 1, 1)) as "When"
-            FROM  T_Analysis_Job
-            WHERE Start BETWEEN _jobFinishStart AND _jobFinishEnd
-            GROUP BY assigned_processor_name
+            SELECT UnionQ.processor, Max(Most_Recent_Start) As Most_Recent_Start
+            FROM ( SELECT processor, Max(Coalesce(Start, make_date(1970, 1, 1))) As Most_Recent_Start
+                   FROM sw.t_job_steps
+                   WHERE Start BETWEEN _startDate AND _endDate
+                   GROUP BY processor
+                   UNION
+                   SELECT processor, Max(Coalesce(Start, make_date(1970, 1, 1))) As Most_Recent_Start
+                   FROM sw.t_job_steps_history
+                   WHERE Start BETWEEN _startDate AND _endDate
+                   GROUP BY processor
+                ) UnionQ
+            GROUP BY UnionQ.processor
         ) M;
 
-    -- Update any entries that have active job with later date than existing entry in Tmp_ManagerActivity
+    -- Update actively running processors
     --
     UPDATE Tmp_ManagerActivity M
-    Set "When" = T."When",
-        Who = T.Who,
+    Set Who = T.Who,
         What = T.What,
-        Source = 'Jobs'
-    From ( -- get list of jobs in progress
-           --
-           SELECT start as "When", 'Analysis: ' || assigned_processor_name as Who, 'Job in progress: ' || CAST(job AS text) AS What
-           FROM   t_analysis_job
-           WHERE  job_state_id = 2
+        Status_Date = T.Start
+    From ( SELECT RankQ.Who,
+                  RankQ.What,
+                  RankQ.Start
+           FROM ( SELECT 'Analysis: ' || Processor As Who,
+                         format('Processing job %s, step %s, tool %s', job, step, step_tool) AS What,
+                         Start,
+                         Row_Number() Over (Partition By Processor Order By Start Desc) As StartRank
+                  FROM sw.t_job_steps
+                  WHERE State = 4 And Start BETWEEN _startDate AND _endDate
+                ) RankQ
+           WHERE RankQ.StartRank = 1
          ) T
-    WHERE M.Who = T.Who AND M."When" <= T."When";
+    WHERE M.Who = T.Who;
 
-    -- Update any entries that have active capture with later date (from event log) than health log
+
+    -- Populate temporary table with capture task managers that started a capture or archive task within the date range
     --
-    Update Tmp_ManagerActivity M
-    Set "When" = T."When",
-        Who = T.Who,
+    INSERT INTO Tmp_ManagerActivity(Who, What, Status_Date, Source)
+    SELECT 'Capture: ' || M.processor AS Who,
+           'Idle' AS What,
+           M.Most_Recent_Start,
+           'Capture Tasks' as Source
+    FROM (  -- Get distinct list of analysis managers
+            -- that have been active within the date range
+            --
+            SELECT UnionQ.processor, Max(Most_Recent_Start) As Most_Recent_Start
+            FROM ( SELECT processor, Max(Coalesce(Start, make_date(1970, 1, 1))) As Most_Recent_Start
+                   FROM cap.t_task_steps
+                   WHERE Start BETWEEN _startDate AND _endDate
+                   GROUP BY processor
+                   UNION
+                   SELECT processor, Max(Coalesce(Start, make_date(1970, 1, 1))) As Most_Recent_Start
+                   FROM cap.t_task_steps_history
+                   WHERE Start BETWEEN _startDate AND _endDate
+                   GROUP BY processor
+                ) UnionQ
+            GROUP BY UnionQ.processor
+        ) M;
+
+    -- Update actively running processors
+    --
+    UPDATE Tmp_ManagerActivity M
+    Set Who = T.Who,
         What = T.What,
-        Source = 'Capture'
-    From ( -- get list of captures in progress
-           --
-           SELECT t_event_log.entered AS "When",
-                  'Capture: ' || t_storage_path.machine_name AS Who,
-                  'In Progress: ' || t_dataset.dataset  AS What
-           FROM t_dataset INNER JOIN
-                t_storage_path ON t_dataset.storage_path_ID = t_storage_path.storage_path_id INNER JOIN
-                t_event_log ON t_dataset.dataset_id = t_event_log.target_id
-           WHERE t_dataset.dataset_state_id = 2 AND t_event_log.target_type = 4
+        Status_Date = T.Start
+    From ( SELECT RankQ.Who,
+                  RankQ.What,
+                  RankQ.Start
+           FROM ( SELECT 'Capture: ' || Processor As Who,
+                         format('Processing task %s, step %s, tool %s', job, step, step_tool) AS What,
+                         Start,
+                         Row_Number() Over (Partition By Processor Order By Start Desc) As StartRank
+                  FROM cap.t_task_steps
+                  WHERE State = 4 And Start BETWEEN _startDate AND _endDate
+                ) RankQ
+           WHERE RankQ.StartRank = 1
          ) T
-    WHERE M.Who = T.Who And M."When" < T."When";
-
-    -- Update any entries that have active preparation with later date (from event log) than health log
-    --
-    Update Tmp_ManagerActivity M
-    Set "When" = T."When",
-        Who = T.Who,
-        What = T.What,
-        Source = 'Preparation'
-    From ( -- get list of preparation in progress
-          --
-          SELECT
-              t_event_log.entered AS "When",
-              'In Progress: ' || t_dataset.dataset AS What,
-              'Preparation: ' || t_dataset.ds_prep_server_name AS Who
-          FROM t_dataset INNER JOIN
-               t_storage_path ON t_dataset.storage_path_id = t_storage_path.storage_path_id INNER JOIN
-               t_event_log ON t_dataset.dataset_id = t_event_log.target_id
-          WHERE t_dataset.dataset_state_id = 7 AND
-                t_event_log.target_type = 4 AND
-                t_event_log.target_state = 7
-        ) T
-    WHERE M.Who = T.Who And M."When" < T."When";
-
-    -- Update any entries that have active archive with later date (from event log) than health log
-    --
-    Update Tmp_ManagerActivity M
-    Set "When" = T."When",
-        Who = T.Who,
-        What = T.What,
-        Source = 'Archive'
-    From ( -- get list of archive in progress
-           --
-           SELECT t_event_log.entered AS "When",
-                  'Archive: ' || t_storage_path.machine_name AS Who,
-                  'In Progress: ' || t_dataset.dataset AS What
-           FROM t_dataset INNER JOIN
-                t_storage_path ON t_dataset.storage_path_id = t_storage_path.storage_path_id INNER JOIN
-                t_event_log ON t_dataset.dataset_id = t_event_log.target_id INNER JOIN
-                t_dataset_archive ON t_dataset.dataset_id = t_dataset_archive.dataset_id
-           WHERE t_event_log.target_type = 6 AND t_dataset_archive.archive_state_id IN (2, 7)
-         ) T
-    WHERE M.Who = T.Who And M."When" < T."When";
+    WHERE M.Who = T.Who;
 
     RETURN QUERY
     SELECT
         A.Source,
-        A."When",
         A.Who,
-        A.What
+        A.What,
+        A.Status_Date
     FROM Tmp_ManagerActivity A
-    ORDER by A.Who DESC;
+    ORDER by A.Who;
 
     DROP TABLE Tmp_ManagerActivity;
 END
 $$;
 
 
-ALTER FUNCTION public.get_current_manager_activity(_monthrange integer, _jobfinishend timestamp without time zone) OWNER TO d3l243;
-
---
--- Name: FUNCTION get_current_manager_activity(_monthrange integer, _jobfinishend timestamp without time zone); Type: COMMENT; Schema: public; Owner: d3l243
---
-
-COMMENT ON FUNCTION public.get_current_manager_activity(_monthrange integer, _jobfinishend timestamp without time zone) IS 'GetCurrentManagerActivity';
+ALTER FUNCTION public.get_current_manager_activity(_activeonly boolean, _startdate timestamp without time zone, _months integer) OWNER TO d3l243;
 
