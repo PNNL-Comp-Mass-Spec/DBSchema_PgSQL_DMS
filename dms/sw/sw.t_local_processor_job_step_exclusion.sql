57a8226b7edb207b95caacc58326a2619f227f0b
alchemistmatt <matthew.monroe@pnnl.gov>
2022-12-15 21:01:18 -0800

    Rename id to processor_id

--- dms/sw/sw.t_local_processor_job_step_exclusion.sql
+++ dms/sw/sw.t_local_processor_job_step_exclusion.sql
@@ -3,7 +3,7 @@
 --
 
 CREATE TABLE sw.t_local_processor_job_step_exclusion (
-    id integer NOT NULL,
+    processor_id integer NOT NULL,
     step integer NOT NULL
 );
 
@@ -15,7 +15,7 @@ ALTER TABLE sw.t_local_processor_job_step_exclusion OWNER TO d3l243;
 --
 
 ALTER TABLE ONLY sw.t_local_processor_job_step_exclusion
-    ADD CONSTRAINT pk_t_local_processor_job_step_exclusion PRIMARY KEY (id, step);
+    ADD CONSTRAINT pk_t_local_processor_job_step_exclusion PRIMARY KEY (processor_id, step);
 
 --
 -- Name: TABLE t_local_processor_job_step_exclusion; Type: ACL; Schema: sw; Owner: d3l243
