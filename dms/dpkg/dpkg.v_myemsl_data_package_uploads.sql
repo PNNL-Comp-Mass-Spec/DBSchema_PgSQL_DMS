0aa34f2f076f770c1d2e7d332ba9fc002205f9d5
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-12 21:19:45 -0800

    Add new views

---
+++ dms/dpkg/dpkg.v_myemsl_data_package_uploads.sql
@@ -0,0 +1,42 @@
+--
+-- Name: v_myemsl_data_package_uploads; Type: VIEW; Schema: dpkg; Owner: d3l243
+--
+
+CREATE VIEW dpkg.v_myemsl_data_package_uploads AS
+ SELECT mu.entry_id,
+    mu.data_pkg_id AS data_package_id,
+    mu.subfolder,
+    mu.file_count_new,
+    mu.file_count_updated,
+    round((((mu.bytes)::numeric / 1024.0) / 1024.0), 3) AS mb,
+    round((mu.upload_time_seconds)::numeric, 1) AS upload_time_seconds,
+    mu.status_uri_path_id,
+    mu.status_num,
+    mu.error_code,
+    (((p.uri_path)::text || (mu.status_num)::text) ||
+        CASE
+            WHEN (p.uri_path OPERATOR(public.~~) '%/status/%'::public.citext) THEN '/xml'::text
+            ELSE ''::text
+        END) AS status_uri,
+    mu.available,
+    mu.verified,
+    mu.entered
+   FROM (dpkg.t_myemsl_uploads mu
+     LEFT JOIN dpkg.t_uri_paths p ON ((mu.status_uri_path_id = p.uri_path_id)));
+
+
+ALTER TABLE dpkg.v_myemsl_data_package_uploads OWNER TO d3l243;
+
+--
+-- Name: VIEW v_myemsl_data_package_uploads; Type: COMMENT; Schema: dpkg; Owner: d3l243
+--
+
+COMMENT ON VIEW dpkg.v_myemsl_data_package_uploads IS 'This view is used by clsDataPackageArchiver in the DataPackage Archive Manager';
+
+--
+-- Name: TABLE v_myemsl_data_package_uploads; Type: ACL; Schema: dpkg; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE dpkg.v_myemsl_data_package_uploads TO readaccess;
+GRANT SELECT ON TABLE dpkg.v_myemsl_data_package_uploads TO writeaccess;
+
