9d1d450581b426fa2ad303499886cae05a41960d
alchemistmatt <matthew.monroe@pnnl.gov>
2023-01-31 17:05:11 -0800

    Update test function

--- dms/test/test.test_harness2.sql
+++ dms/test/test.test_harness2.sql
@@ -2,7 +2,7 @@
 -- Name: test_harness2(text); Type: FUNCTION; Schema: test; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION test.test_harness2(_managernamelist text) RETURNS TABLE(mgr_id integer, paramtypeid integer, value text)
+CREATE OR REPLACE FUNCTION test.test_harness2(_managernamelist text) RETURNS TABLE(mgr_id integer, mgr_name public.citext, param_type_id integer, value public.citext)
     LANGUAGE plpgsql
     AS $$
 DECLARE
@@ -21,40 +21,32 @@ BEGIN
 
     INSERT INTO TmpManagerList (manager_name)
     SELECT manager_name
-    FROM mc.parse_manager_name_list(_managerNameList, _remove_unknown_managers => 1);
+    FROM mc.parse_manager_name_list(_managerNameList, _remove_unknown_managers => 0);
 
     UPDATE TmpManagerList
     SET mgr_id = M.mgr_id
     FROM mc.t_mgrs M
     WHERE M.mgr_name = TmpManagerList.manager_name;
 
-    SELECT param_id
+    SELECT pt.param_type_id
     INTO _paramTypeID
-    FROM mc.t_param_type
-    WHERE param_name = 'ManagerErrorCleanupMode';
+    FROM mc.t_param_type pt
+    WHERE pt.param_name = 'ManagerErrorCleanupMode';
 
     RAISE info 'ManagerErrorCleanupMode param type ID: %', _paramTypeID;
 
     RETURN query
-    SELECT M.mgr_id, 0 AS test, M.manager_name::text
+    SELECT M.mgr_id, M.manager_name, 0 AS param_type_id, ''::citext AS value
     FROM TmpManagerList M;
 
     -- Calling RETURN query again will append additional rows to the output table
     --
     RETURN query
-    SELECT A.mgr_id, _paramTypeID, '0'
-    FROM ( SELECT MgrListA.mgr_id
-           FROM TmpManagerList MgrListA
-         ) A
-         LEFT OUTER JOIN
-          ( SELECT MgrListB.mgr_id
-            FROM TmpManagerList MgrListB
-                 INNER JOIN mc.t_param_value PV
-                   ON MgrListB.mgr_id = PV.mgr_id
-            WHERE PV.type_id = _paramTypeID
-         ) B
-           ON A.mgr_id = B.mgr_id
-    WHERE B.mgr_id IS NULL;
+    SELECT M.mgr_id, M.manager_name, PV.param_type_id, PV.value
+    FROM TmpManagerList M
+         INNER JOIN mc.t_param_value PV
+           ON M.mgr_id = PV.mgr_id
+    WHERE PV.param_type_id = _paramTypeID;
 
     DROP TABLE TmpManagerList;
 
@@ -71,6 +63,8 @@ EXCEPTION
     RAISE Info 'Exception context; %', _exceptionContext;
 
     RAISE Exception '%, code %; see the output for context', _message, _sqlState;
+
+    DROP TABLE If Exists TmpManagerList;
 
 End
 $$;
