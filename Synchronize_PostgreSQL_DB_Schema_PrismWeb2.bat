65fdf0dec5860c75d8ca26b6f8378c69805d4643
alchemistmatt <matthew.monroe@pnnl.gov>
2023-02-03 14:21:48 -0800

    Add/update batch files

--- Synchronize_PostgreSQL_DB_Schema_PrismWeb2.bat
+++ Synchronize_PostgreSQL_DB_Schema_PrismWeb2.bat
@@ -8,6 +8,6 @@ F:
 pushd "F:\Documents\Projects\DataMining\Database_Tools\DB_Schema_Export_Tool\Automation"
 
 @echo on
-..\bin\DB_Schema_Export_Tool.exe /conf:PgSQL_ExportOptions_PrismDB1.conf
+..\bin\DB_Schema_Export_Tool.exe /conf:PgSQL_ExportOptions_Prismweb2.conf
 
 popd
