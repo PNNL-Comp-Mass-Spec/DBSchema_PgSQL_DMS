9e52fb32ea3d1d0f066d338e5eaa20a370d46888
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-28 20:22:29 -0700

    Add custom collation that sorts underscores before letters

---
+++ dms/_Collation_general_ci_ai.sql
@@ -0,0 +1,9 @@
+--
+-- Name: general_ci_ai; Type: COLLATION; Schema: public; Owner: d3l243
+--
+
+CREATE COLLATION public.general_ci_ai (provider = icu, deterministic = false, locale = '@ColStrength=primary');
+
+
+ALTER COLLATION public.general_ci_ai OWNER TO d3l243;
+
