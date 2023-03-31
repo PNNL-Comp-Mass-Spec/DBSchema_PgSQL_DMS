cafbd52f28c9814c1566492c26118fc752d6be59
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-30 21:19:01 -0700

    Add tables to track deleted requested run batches and batch groups

---
+++ dms/t_deleted_requested_run_batch_group.sql
@@ -0,0 +1,43 @@
+--
+-- Name: t_deleted_requested_run_batch_group; Type: TABLE; Schema: public; Owner: d3l243
+--
+
+CREATE TABLE public.t_deleted_requested_run_batch_group (
+    entry_id integer NOT NULL,
+    batch_group_id integer,
+    batch_group public.citext NOT NULL,
+    description public.citext,
+    owner_user_id integer,
+    created timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+ALTER TABLE public.t_deleted_requested_run_batch_group OWNER TO d3l243;
+
+--
+-- Name: t_deleted_requested_run_batch_group_entry_id_seq; Type: SEQUENCE; Schema: public; Owner: d3l243
+--
+
+ALTER TABLE public.t_deleted_requested_run_batch_group ALTER COLUMN entry_id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.t_deleted_requested_run_batch_group_entry_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+--
+-- Name: t_deleted_requested_run_batch_group pk_t_deleted_requested_run_batch_group; Type: CONSTRAINT; Schema: public; Owner: d3l243
+--
+
+ALTER TABLE ONLY public.t_deleted_requested_run_batch_group
+    ADD CONSTRAINT pk_t_deleted_requested_run_batch_group PRIMARY KEY (entry_id);
+
+--
+-- Name: TABLE t_deleted_requested_run_batch_group; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.t_deleted_requested_run_batch_group TO readaccess;
+GRANT SELECT ON TABLE public.t_deleted_requested_run_batch_group TO writeaccess;
+
