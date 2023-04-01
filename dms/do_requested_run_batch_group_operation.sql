99b6c5168db4b7cf1f319ba98cfb85ffaf9b8026
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-31 20:35:00 -0700

    Add procedures for updating requested run batch and batch groups

---
+++ dms/do_requested_run_batch_group_operation.sql
@@ -0,0 +1,112 @@
+--
+-- Name: do_requested_run_batch_group_operation(integer, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.do_requested_run_batch_group_operation(IN _batchgroupid integer, IN _mode text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Delete a requested run batch group
+**
+**  Arguments:
+**    _mode   'Delete'
+**
+**  Auth:   mem
+**  Date:   03/31/2023 mem - Initial version
+**
+*****************************************************/
+DECLARE
+    _schemaName text;
+    _nameWithSchema text;
+    _authorized boolean;
+
+BEGIN
+
+    _message := '';
+    _returnCode := '';
+
+    ---------------------------------------------------
+    -- Verify that the user can execute this procedure from the given client host
+    ---------------------------------------------------
+
+    SELECT schema_name, name_with_schema
+    INTO _schemaName, _nameWithSchema
+    FROM get_current_function_info('<auto>', _showDebug => false);
+
+    SELECT authorized
+    INTO _authorized
+    FROM public.verify_sp_authorized(_nameWithSchema, _schemaName, _logError => true);
+
+    If Not _authorized Then
+        -- Commit changes to persist the message logged to public.t_log_entries
+        COMMIT;
+
+        _message := format('User %s cannot use procedure %s', CURRENT_USER, _nameWithSchema);
+        RAISE EXCEPTION '%', _message;
+    End If;
+
+    _batchGroupID = Coalesce(_batchGroupID, 0);
+    _mode = Coalesce(_mode, '');
+
+    If _batchGroupID < 1 Then
+        RAISE EXCEPTION 'Batch group ID must be a positive number' USING ERRCODE = 'U5201';
+    End If;
+
+    _mode := Trim(Coalesce(_mode, ''));
+
+    ---------------------------------------------------
+    -- Is batch group in table?
+    ---------------------------------------------------
+
+    If Not Exists (SELECT Batch_Group_ID FROM T_Requested_Run_Batch_Group WHERE Batch_Group_ID = _batchGroupID) Then
+        _message = format('Batch group does not exist: %s', _batchGroupID);
+        RAISE EXCEPTION '%', _message USING ERRCODE = 'U5202';
+    End If;
+
+    ---------------------------------------------------
+    -- Delete batch group
+    ---------------------------------------------------
+
+    If _mode::citext = 'Delete' Then
+         -- Assure that the batch group is not used by any batches
+        If Exists (Select * From T_Requested_Run_Batches Where Batch_Group_ID = _batchGroupID) Then
+            _message = format('Cannot delete batch group since used by one or more requested run batches: %s', _batchGroupID);
+            RAISE EXCEPTION '%', _message USING ERRCODE = 'U5203';
+        Else
+            INSERT INTO T_Deleted_Requested_Run_Batch_Group (Batch_Group_ID, Batch_Group, Description, Owner_User_ID, Created)
+            SELECT Batch_Group_ID, Batch_Group, Description, Owner_User_ID, Created
+            FROM T_Requested_Run_Batch_Group
+            WHERE Batch_Group_ID = _batchGroupID;
+
+            DELETE FROM T_Requested_Run_Batch_Group
+            WHERE Batch_Group_ID = _batchGroupID;
+
+            RETURN;
+        End If;
+    End If;
+
+    ---------------------------------------------------
+    -- Check for invalid mode
+    ---------------------------------------------------
+
+    If _mode = '' Then
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Mode was unrecognized
+    ---------------------------------------------------
+
+    _message := format('Mode "%s" was unrecognized', _mode);
+    RAISE WARNING '%', _message;
+
+    _returnCode := 'U5204';
+
+END
+$$;
+
+
+ALTER PROCEDURE public.do_requested_run_batch_group_operation(IN _batchgroupid integer, IN _mode text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
---
+++ dms/do_requested_run_batch_operation.sql
@@ -0,0 +1,229 @@
+--
+-- Name: do_requested_run_batch_operation(integer, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.do_requested_run_batch_operation(IN _batchid integer, IN _mode text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Lock, unlock, or delete a requested run batch
+**
+**  Arguments:
+**    _mode   'LockBatch', 'UnlockBatch', 'Lock', 'Unlock', 'Delete'
+**            Supported, but unused modes (as of July 2017): 'FreeMembers', 'GrantHiPri', 'DenyHiPri'
+**
+**  Auth:   grk
+**  Date:   01/12/2006
+**          09/20/2006 jds - Added support for Granting High Priority and Denying High Priority for fields Actual_Bath_Priority and Requested_Batch_Priority
+**          08/27/2009 grk - Delete batch fixes requested run references in history table
+**          02/26/2010 grk - merged T_Requested_Run_History with T_Requested_Run
+**          06/16/2017 mem - Restrict access using VerifySPAuthorized
+**          07/25/2017 mem - Remove mode BatchOrder since unused
+**          08/01/2017 mem - Use THROW if not authorized
+**          08/01/2022 mem - Exit the procedure if _batchID is 0
+**          02/10/2023 mem - Call update_cached_requested_run_batch_stats
+**          03/31/2023 mem - Ported to PostgreSQL
+**                         - When deleting a batch, archive it in T_Deleted_Requested_Run_Batch
+**
+*****************************************************/
+DECLARE
+    _schemaName text;
+    _nameWithSchema text;
+    _authorized boolean;
+
+    _batchExists boolean := false;
+    _locked citext;
+    _message2 text;
+BEGIN
+
+    _message := '';
+    _returnCode := '';
+
+    ---------------------------------------------------
+    -- Verify that the user can execute this procedure from the given client host
+    ---------------------------------------------------
+
+    SELECT schema_name, name_with_schema
+    INTO _schemaName, _nameWithSchema
+    FROM get_current_function_info('<auto>', _showDebug => false);
+
+    SELECT authorized
+    INTO _authorized
+    FROM public.verify_sp_authorized(_nameWithSchema, _schemaName, _logError => true);
+
+    If Not _authorized Then
+        -- Commit changes to persist the message logged to public.t_log_entries
+        COMMIT;
+
+        _message := format('User %s cannot use procedure %s', CURRENT_USER, _nameWithSchema);
+        RAISE EXCEPTION '%', _message;
+    End If;
+
+    _batchID := Coalesce(_batchID, 0);
+    _mode := Coalesce(_mode, '');
+
+    If _batchID = 0 Then
+        RAISE EXCEPTION 'Batch operation tasks are not allowed for Batch 0' USING ERRCODE = 'U5201';
+    End If;
+
+    _mode := Trim(Coalesce(_mode, ''));
+
+    ---------------------------------------------------
+    -- Is batch in table?
+    ---------------------------------------------------
+
+    SELECT locked   -- 'Yes' or 'No'
+    INTO _locked
+    FROM t_requested_run_batches
+    WHERE batch_id = _batchID;
+
+    If FOUND Then
+        _batchExists := true;
+    End If;
+
+    ---------------------------------------------------
+    -- Lock run order
+    ---------------------------------------------------
+
+    If _mode::citext IN ('LockBatch', 'Lock') Then
+        If _batchExists Then
+            UPDATE t_requested_run_batches
+            SET locked = 'Yes'
+            WHERE batch_id = _batchID;
+
+        End If;
+
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Unlock run order
+    ---------------------------------------------------
+
+    If _mode::citext IN ('UnlockBatch', 'Unlock') Then
+        If _batchExists Then
+            UPDATE t_requested_run_batches
+            SET locked = 'No'
+            WHERE batch_id = _batchID;
+        End If;
+
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Remove current member requests from batch
+    ---------------------------------------------------
+
+    If _mode::citext = 'FreeMembers'::citext Or _mode = 'Delete' Then
+        If _locked = 'Yes' Then
+            _message := 'Cannot remove member requests of locked batch';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5201';
+            RETURN;
+        Else
+            UPDATE t_requested_run
+            SET batch_id = 0
+            WHERE batch_id = _batchID;
+
+            ---------------------------------------------------
+            -- Update stats in t_cached_requested_run_batch_stats
+            ---------------------------------------------------
+
+            Call update_cached_requested_run_batch_stats (
+                    _batchID,
+                    _message => _message2,          -- Output
+                    _returnCode => _returnCode);    -- Output
+
+            If _mode::citext = 'FreeMembers' Then
+                RETURN;
+            End If;
+        End If;
+    End If;
+
+    ---------------------------------------------------
+    -- Delete batch
+    ---------------------------------------------------
+
+    If _mode::citext = 'Delete' Then
+        If _locked = 'yes' Then
+            _message := 'Cannot delete locked batch';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5202';
+            RETURN;
+        Else
+            INSERT INTO T_Deleted_Requested_Run_Batch (Batch_ID, Batch, Description, Owner_User_ID, Created, Locked,
+                                                       Last_Ordered, Requested_Batch_Priority, Actual_Batch_Priority,
+                                                       Requested_Completion_Date, Justification_for_High_Priority, Comment,
+                                                       Requested_Instrument_Group, Batch_Group_ID, Batch_Group_Order)
+            SELECT Batch_ID, Batch, Description, Owner_User_ID, Created, Locked,
+                   Last_Ordered, Requested_Batch_Priority, Actual_Batch_Priority,
+                   Requested_Completion_Date, Justification_for_High_Priority, Comment,
+                   Requested_Instrument_Group, Batch_Group_ID, Batch_Group_Order
+            FROM T_Requested_Run_Batches
+            WHERE Batch_ID = _batchID;
+
+            DELETE FROM t_requested_run_batches
+            WHERE batch_id = _batchID;
+
+            RETURN;
+        End If;
+    End If;
+
+    ---------------------------------------------------
+    -- Grant High Priority
+    ---------------------------------------------------
+
+    If _mode::citext = 'GrantHiPri' Then
+        UPDATE t_requested_run_batches
+        SET actual_batch_priority = 'High'
+        WHERE batch_id = _batchID;
+
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Deny High Priority
+    ---------------------------------------------------
+
+    If _mode::citext = 'DenyHiPri' Then
+        UPDATE t_requested_run_batches
+        SET actual_batch_priority = 'Normal',
+            requested_batch_priority = 'Normal'
+        WHERE batch_id = _batchID;
+
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Check for invalid mode
+    ---------------------------------------------------
+
+    If _mode = '' Then
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
+    -- Mode was unrecognized
+    ---------------------------------------------------
+
+    _message := format('Mode "%s" was unrecognized', _mode);
+    RAISE WARNING '%', _message;
+
+    _returnCode := 'U5203';
+
+END
+$$;
+
+
+ALTER PROCEDURE public.do_requested_run_batch_operation(IN _batchid integer, IN _mode text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE do_requested_run_batch_operation(IN _batchid integer, IN _mode text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.do_requested_run_batch_operation(IN _batchid integer, IN _mode text, INOUT _message text, INOUT _returncode text) IS 'DoRequestedRunBatchOperation';
+
