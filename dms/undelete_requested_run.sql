6301bf527912bf1657c1d8e450e7a679438630c0
alchemistmatt <matthew.monroe@pnnl.gov>
2023-03-31 20:32:39 -0700

    Restore requested run batches and batch groups if the requested run refers to a deleted batch or batch group

--- dms/undelete_requested_run.sql
+++ dms/undelete_requested_run.sql
@@ -16,6 +16,7 @@ CREATE OR REPLACE PROCEDURE public.undelete_requested_run(IN _requestid integer,
 **
 **  Auth:   mem
 **  Date:   03/30/2023 mem - Initial version
+**          03/31/2023 mem - Restore requested run batches and batch groups if the requested run refers to a deleted batch or batch group
 **
 *****************************************************/
 DECLARE
@@ -26,6 +27,9 @@ DECLARE
     _entryID int;
     _batchID int;
     _eusPersonID int;
+    _batchGroupID int;
+    _deletedBatchEntryID int := 0;
+    _deletedBatchGroupEntryID int := 0;
     _message2 text;
 BEGIN
     _message := '';
@@ -80,7 +84,6 @@ BEGIN
         RETURN;
     End If;
 
-
     ---------------------------------------------------
     -- Make sure the requested run does not already exist
     ---------------------------------------------------
@@ -109,20 +112,104 @@ BEGIN
         RETURN;
     End If;
 
+    ---------------------------------------------------
+    -- See if the deleted requested run references a deleted requested run batch
+    ---------------------------------------------------
+
+    If _batchID > 0 And Not Exists (Select Batch_ID From T_Requested_Run_Batches Where Batch_ID = _batchID) Then
+        -- Need to undelete the batch
+        SELECT Entry_ID,
+               Batch_Group_ID
+        INTO _deletedBatchEntryID, _batchGroupID
+        FROM T_Deleted_Requested_Run_Batch
+        WHERE Batch_ID = _batchID
+        ORDER BY Entry_ID DESC
+        LIMIT 1;
+
+        If Not FOUND Then
+            _message := format('Requested run ID %s refers to batch %s, which does not exist, and cannot be restored from T_Deleted_Requested_Run_Batch; ' ||
+                               'see entry %s in T_Deleted_Requested_Run',
+                                _requestID, _batchID, _entryID);
+
+            RAISE WARNING '%', _message;
+            RETURN;
+        End If;
+
+        ---------------------------------------------------
+        -- See if the deleted requested run batch references a deleted batch group
+        ---------------------------------------------------
+
+        If Coalesce(_batchGroupID, 0) > 0 And Not Exists (Select Batch_Group_ID From T_Requested_Run_Batch_Group Where Batch_Group_ID = _batchGroupID) Then
+            -- Need to undelete the batch group
+            SELECT Entry_ID
+            INTO _deletedBatchGroupEntryID
+            FROM T_Deleted_Requested_Run_Batch_Group
+            WHERE Batch_Group_ID = _batchGroupID
+            ORDER BY Entry_ID DESC
+            LIMIT 1;
+
+            If Not FOUND Then
+                _message := format('Requested run ID %s refers to batch %s, which refers to batch group %s, ' ||
+                               'but that batch group does not exist and cannot be restored from T_Deleted_Requested_Run_Batch_Group; ' ||
+                               'see entry %s in T_Deleted_Requested_Run ' ||
+                               'and entry %s in T_Deleted_Requested_Run_Batch',
+                               _requestID, _batchID, _batchGroupID, _entryID, _deletedBatchEntryID);
+
+                RAISE WARNING '%', _message;
+                RETURN;
+            End If;
+        End If;
+    End If;
+
     BEGIN
+        If _deletedBatchGroupEntryID > 0 Then
+
+            ---------------------------------------------------
+            -- Add the deleted requested run batch group to T_Requested_Run_Batch_Group
+            ---------------------------------------------------
+
+            INSERT INTO T_Requested_Run_Batch_Group (Batch_Group_ID, Batch_Group, Description, Owner_User_ID, Created)
+            OVERRIDING SYSTEM VALUE
+            SELECT Batch_Group_ID, Batch_Group, Description, Owner_User_ID, Created
+            FROM T_Deleted_Requested_Run_Batch_Group
+            WHERE Entry_ID = _deletedBatchGroupEntryID;
+
+        End If;
+
+        If _deletedBatchEntryID > 0 Then
+
+            ---------------------------------------------------
+            -- Add the deleted requested run batch to T_Requested_Run_Batches
+            ---------------------------------------------------
+
+            INSERT INTO T_Requested_Run_Batches (
+                    Batch_ID, Batch, Description, Owner_User_ID, Created, Locked,
+                    Last_Ordered, Requested_Batch_Priority, Actual_Batch_Priority,
+                    Requested_Completion_Date, Justification_for_High_Priority, Comment,
+                    Requested_Instrument_Group, Batch_Group_ID, Batch_Group_Order
+                )
+            OVERRIDING SYSTEM VALUE
+            SELECT Batch_ID, Batch, Description, Owner_User_ID, Created, Locked,
+                   Last_Ordered, Requested_Batch_Priority, Actual_Batch_Priority,
+                   Requested_Completion_Date, Justification_for_High_Priority, Comment,
+                   Requested_Instrument_Group, Batch_Group_ID, Batch_Group_Order
+            FROM T_Deleted_Requested_Run_Batch
+            WHERE Entry_ID = _deletedBatchEntryID;
+
+        End If;
 
         ---------------------------------------------------
         -- Add the requested run to T_Requested_Run
         ---------------------------------------------------
 
         INSERT INTO T_Requested_Run (
-            Request_Id, Request_Name, Requester_Username, Comment, Created, Instrument_Group,
-            Request_Type_Id, Instrument_Setting, Special_Instructions, Wellplate, Well, Priority, Note, Exp_Id,
-            Request_Run_Start, Request_Run_Finish, Request_Internal_Standard, Work_Package, Batch_Id,
-            Blocking_Factor, Block, Run_Order, EUS_Proposal_Id, EUS_Usage_Type_Id,
-            Cart_Id, Cart_Config_Id, Cart_Column, Separation_Group, Mrm_Attachment,
-            Dataset_Id, Origin, State_Name, Request_Name_Code, Vialing_Conc, Vialing_Vol, Location_Id,
-            Queue_State, Queue_Instrument_Id, Queue_Date, Entered, Updated, Updated_By
+                Request_Id, Request_Name, Requester_Username, Comment, Created, Instrument_Group,
+                Request_Type_Id, Instrument_Setting, Special_Instructions, Wellplate, Well, Priority, Note, Exp_Id,
+                Request_Run_Start, Request_Run_Finish, Request_Internal_Standard, Work_Package, Batch_Id,
+                Blocking_Factor, Block, Run_Order, EUS_Proposal_Id, EUS_Usage_Type_Id,
+                Cart_Id, Cart_Config_Id, Cart_Column, Separation_Group, Mrm_Attachment,
+                Dataset_Id, Origin, State_Name, Request_Name_Code, Vialing_Conc, Vialing_Vol, Location_Id,
+                Queue_State, Queue_Instrument_Id, Queue_Date, Entered, Updated, Updated_By
             )
         OVERRIDING SYSTEM VALUE
         SELECT Request_Id, Request_Name, Requester_Username, Comment, Created, Instrument_Group,
@@ -167,7 +254,7 @@ BEGIN
         Call update_cached_requested_run_batch_stats (
                 _batchID,
                 _fullrefresh => false,
-                _message => _message2,         -- Output
+                _message => _message2,          -- Output
                 _returncode => _returncode);    -- Output
 
         If _returnCode <> '' Then
