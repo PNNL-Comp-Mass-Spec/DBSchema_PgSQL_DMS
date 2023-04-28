6311bfd2dfb99f188a8f87a4e773dab473a844d9
alchemistmatt <matthew.monroe@pnnl.gov>
2023-04-28 14:40:41 -0700

    Add dataset count column to the run planning report

--- dms/v_run_planning_report.sql
+++ dms/v_run_planning_report.sql
@@ -11,6 +11,7 @@ CREATE VIEW public.v_run_planning_report AS
         END AS requests,
     groupq.blocked,
     groupq.block_missing,
+    rbs.datasets,
         CASE
             WHEN (requestlookupq.batch_id > 0) THEN groupq.batch_prefix
             ELSE groupq.request_prefix
@@ -53,7 +54,7 @@ CREATE VIEW public.v_run_planning_report AS
             WHEN (groupq.fractionbasedrequestcount > 1) THEN 2
             ELSE 0
         END AS fraction_color_mode
-   FROM ((( SELECT requestq.inst_group,
+   FROM (((( SELECT requestq.inst_group,
             min(requestq.requestid) AS min_request,
             count(requestq.requestid) AS requests,
             min(requestq.request_prefix) AS request_prefix,
@@ -70,6 +71,7 @@ CREATE VIEW public.v_run_planning_report AS
             requestq.locked,
             requestq.batch_prefix,
             requestq.requested_batch_priority,
+            requestq.batch_id,
             requestq.batch_comment,
             requestq.batch_group_id,
             requestq.last_ordered,
@@ -104,7 +106,7 @@ CREATE VIEW public.v_run_planning_report AS
                     ept.abbreviation AS proposal_type,
                     rrb.locked,
                     rrb.requested_batch_priority,
-                    rr.batch_id AS batch,
+                    rr.batch_id,
                     rrb.comment AS batch_comment,
                     rrb.batch_group_id,
                     qs.queue_state_name AS queue_state,
@@ -146,9 +148,10 @@ CREATE VIEW public.v_run_planning_report AS
                      LEFT JOIN public.t_eus_proposal_type ept ON ((eup.proposal_type OPERATOR(public.=) ept.proposal_type)))
                      LEFT JOIN public.t_instrument_name assignedinstrument ON ((rr.queue_instrument_id = assignedinstrument.instrument_id)))
                   WHERE ((rr.state_name OPERATOR(public.=) 'Active'::public.citext) AND (rr.dataset_id IS NULL))) requestq
-          GROUP BY requestq.inst_group, requestq.separation_group, requestq.fraction_count, requestq.ds_type, requestq.request_name_code, requestq.requester, requestq.work_package, requestq.wp_state, requestq.wp_activation_state, requestq.proposal, requestq.proposal_type, requestq.locked, requestq.last_ordered, requestq.queue_state, requestq.queued_instrument, requestq.batch, requestq.batch_prefix, requestq.requested_batch_priority, requestq.batch_comment, requestq.batch_group_id) groupq
+          GROUP BY requestq.inst_group, requestq.separation_group, requestq.fraction_count, requestq.ds_type, requestq.request_name_code, requestq.requester, requestq.work_package, requestq.wp_state, requestq.wp_activation_state, requestq.proposal, requestq.proposal_type, requestq.locked, requestq.last_ordered, requestq.queue_state, requestq.queued_instrument, requestq.batch_id, requestq.batch_prefix, requestq.requested_batch_priority, requestq.batch_comment, requestq.batch_group_id) groupq
      JOIN public.t_requested_run requestlookupq ON ((groupq.min_request = requestlookupq.request_id)))
-     JOIN public.t_eus_usage_type teut ON ((requestlookupq.eus_usage_type_id = teut.eus_usage_type_id)));
+     JOIN public.t_eus_usage_type teut ON ((requestlookupq.eus_usage_type_id = teut.eus_usage_type_id)))
+     LEFT JOIN public.t_cached_requested_run_batch_stats rbs ON ((groupq.batch_id = rbs.batch_id)));
 
 
 ALTER TABLE public.v_run_planning_report OWNER TO d3l243;
