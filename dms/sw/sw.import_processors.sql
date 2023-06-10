de8e27e114265e5682cff7c0f7371cf3d27e11f7
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-09 20:42:17 -0700

    Add procedure that updates sw.t_local_processors using public schema tables

--- dms/sw/sw.import_processors.sql
+++ dms/sw/sw.import_processors.sql
@@ -1,24 +1,22 @@
 --
-CREATE OR REPLACE PROCEDURE sw.import_processors
-(
-    _bypassDMS boolean = false,
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: import_processors(boolean, text, text); Type: PROCEDURE; Schema: sw; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE sw.import_processors(IN _bypassdms boolean DEFAULT false, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Get list of processors
-**
+**      Get list of processors from public.V_Get_Pipeline_Processors
+**      (which references public schema tables t_analysis_job_processors and t_analysis_job_processor_group_membership)
 **
 **  Auth:   grk
 **  Date:   06/03/2008 grk - Initial release (http://prismtrac.pnl.gov/trac/ticket/666)
 **          09/03/2009 mem - Now skipping disabled processors when looking for new processors to import
-**          11/11/2013 mem - Now setting ProcTool_Mgr_ID to 1 for newly added processors
+**          11/11/2013 mem - Now setting Proc_Tool_Mgr_ID to 1 for newly added processors
 **          10/14/2022 mem - Remove invalid update query that aimed to disable local processors that were not in DMS, but didn't actually work
-**          10/15/2022 mem - Ported to PostgreSQL
+**          06/09/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -34,7 +32,7 @@ BEGIN
     -- Add processors from DMS that aren't already in local table
     ---------------------------------------------------
     --
-    INSERT INTO sw.t_local_processors (ID, processor_name, state, Groups, GP_Groups, Machine, proc_tool_mgr_id)
+    INSERT INTO sw.t_local_processors (processor_id, processor_name, state, Groups, GP_Groups, Machine, proc_tool_mgr_id)
     SELECT ID, processor_name, state, groups, GP_Groups, Machine, 1
     FROM public.V_Get_Pipeline_Processors VPP
     WHERE VPP.state = 'E' AND
@@ -45,8 +43,7 @@ BEGIN
     ---------------------------------------------------
     --
     UPDATE sw.t_local_processors
-    SET
-        state = VPP.state,
+    SET state = VPP.state,
         groups = VPP.groups,
         gp_groups = VPP.gp_groups,
         machine = VPP.machine
@@ -67,4 +64,6 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE sw.import_processors IS 'ImportProcessors';
+
+ALTER PROCEDURE sw.import_processors(IN _bypassdms boolean, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
