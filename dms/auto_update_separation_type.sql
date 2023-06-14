01cee9fded92612027d36c1239d6fe65ce2916c9
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-13 22:26:10 -0700

    Implement procedure to auto-update separation type based on the separation type name and acquisition length

--- dms/auto_update_separation_type.sql
+++ dms/auto_update_separation_type.sql
@@ -1,21 +1,26 @@
 --
-CREATE OR REPLACE PROCEDURE public.auto_update_separation_type
-(
-    _separationType text,
-    _acqLengthMinutes int,
-    INOUT _optimalSeparationType text = ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: auto_update_separation_type(text, integer, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.auto_update_separation_type(IN _separationtype text, IN _acqlengthminutes integer, INOUT _optimalseparationtype text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
 **      Update the separation type based on the name and acquisition length
+**      Ignores datasets with an acquisition length under 6 minutes
+**
+**  Arguments:
+**     _separationType          Current separation type
+**     _acqLengthMinutes        Acquisition length, in minutes
+**     _optimalSeparationType   Output: optimal separation type to use
 **
 **  Auth:   mem
 **  Date:   10/09/2020 mem - Initial version
 **          10/10/2020 mem - Adjust threshold for LC-Dionex-Formic_30min
-**          12/15/2023 mem - Ported to PostgreSQL
+**          06/13/2023 mem - Exit the procedure if the acquisition length is <= 5 minutes
+**                         - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -26,52 +31,63 @@ BEGIN
     _acqLengthMinutes := Coalesce(_acqLengthMinutes, 0);
     _optimalSeparationType := '';
 
+    If _acqLengthMinutes <= 5 Then
+        If _acqLengthMinutes <= 0 Then
+            RAISE INFO 'Acquisition length is 0 minutes; not updating separation type';
+        Else
+            RAISE INFO 'Acquisition length is less than 5 minutes; not updating separation type';
+        End If;
+
+        _optimalSeparationType := _separationType;
+        RETURN;
+    End If;
+
     ---------------------------------------------------
     -- Update the separation type name if it matches certain conditions
     ---------------------------------------------------
 
-    If _separationType Like 'LC-Waters-Formic%' AND _acqLengthMinutes > 5 Then
+    If _separationType Like 'LC-Waters-Formic%' Then
         If _acqLengthMinutes < 35 Then
             _optimalSeparationType := 'LC-Waters-Formic_30min';
-        ElsIf _acqLengthMinutes < 48
+        ElsIf _acqLengthMinutes < 48 Then
             _optimalSeparationType := 'LC-Waters-Formic_40min';
-        ElsIf _acqLengthMinutes < 80
+        ElsIf _acqLengthMinutes < 80 Then
             _optimalSeparationType := 'LC-Waters-Formic_60min';
-        ElsIf _acqLengthMinutes < 107
+        ElsIf _acqLengthMinutes < 107 Then
             _optimalSeparationType := 'LC-Waters-Formic_90min';
-        ElsIf _acqLengthMinutes < 165
+        ElsIf _acqLengthMinutes < 165 Then
             _optimalSeparationType := 'LC-Waters-Formic_2hr';
-        ElsIf _acqLengthMinutes < 220
+        ElsIf _acqLengthMinutes < 220 Then
             _optimalSeparationType := 'LC-Waters-Formic_3hr';
-        ElsIf _acqLengthMinutes < 280
+        ElsIf _acqLengthMinutes < 280 Then
             _optimalSeparationType := 'LC-Waters-Formic_4hr';
         Else
             _separationType := 'LC-Waters-Formic_5hr';
         End If;
     End If;
 
-    If _separationType Like 'LC-Dionex-Formic%' AND _acqLengthMinutes > 5 Then
+    If _separationType Like 'LC-Dionex-Formic%' Then
         If _acqLengthMinutes < 50 Then
            _optimalSeparationType := 'LC-Dionex-Formic_30min';
-        ElsIf _acqLengthMinutes < 107
+        ElsIf _acqLengthMinutes < 107 Then
            _optimalSeparationType := 'LC-Dionex-Formic_100min';
-        ElsIf _acqLengthMinutes < 165
+        ElsIf _acqLengthMinutes < 165 Then
            _optimalSeparationType := 'LC-Dionex-Formic_2hr';
-        ElsIf _acqLengthMinutes < 280
+        ElsIf _acqLengthMinutes < 280 Then
            _optimalSeparationType := 'LC-Dionex-Formic_3hr';
         Else
            _optimalSeparationType := 'LC-Dionex-Formic_5hr';
         End If;
     End If;
 
-    If _separationType Like 'LC-Agilent-Formic%' AND _acqLengthMinutes > 5 Then
+    If _separationType Like 'LC-Agilent-Formic%' Then
         If _acqLengthMinutes < 35 Then
            _optimalSeparationType := 'LC-Agilent-Formic_30minute';
-        ElsIf _acqLengthMinutes < 80
+        ElsIf _acqLengthMinutes < 80 Then
            _optimalSeparationType := 'LC-Agilent-Formic_60minute';
-        ElsIf _acqLengthMinutes < 107
+        ElsIf _acqLengthMinutes < 107 Then
            _optimalSeparationType := 'LC-Agilent-Formic_100minute';
-        ElsIf _acqLengthMinutes < 165
+        ElsIf _acqLengthMinutes < 165 Then
            _optimalSeparationType := 'LC-Agilent-Formic_2hr';
         Else
            _optimalSeparationType := 'LC-Agilent-Formic_3hr';
@@ -80,7 +96,7 @@ BEGIN
 
     If _optimalSeparationType <> '' Then
         -- Validate the auto-defined separation type
-        If Not Exists (SELECT * from t_secondary_sep WHERE separation_type = _optimalSeparationType) Then
+        If Not Exists (SELECT * FROM t_secondary_sep WHERE separation_type = _optimalSeparationType) Then
             _message := format('Invalid separation type; %s not found in t_secondary_sep', _optimalSeparationType);
 
             CALL post_log_entry ('Error', _message, 'Auto_Update_Separation_Type', _duplicateEntryHoldoffHours => 1);
@@ -96,4 +112,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE public.auto_update_separation_type IS 'AutoUpdateSeparationType';
+
+ALTER PROCEDURE public.auto_update_separation_type(IN _separationtype text, IN _acqlengthminutes integer, INOUT _optimalseparationtype text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE auto_update_separation_type(IN _separationtype text, IN _acqlengthminutes integer, INOUT _optimalseparationtype text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.auto_update_separation_type(IN _separationtype text, IN _acqlengthminutes integer, INOUT _optimalseparationtype text) IS 'AutoUpdateSeparationType';
+
