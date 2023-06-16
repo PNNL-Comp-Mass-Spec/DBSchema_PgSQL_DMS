b44c177f26e32a73c19a291c465b66def750f40f
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-15 18:20:38 -0700

    Implement procedure that updates dataset state for datasets currently in state 2 (Capture In Progress)

--- dms/do_dataset_completion_actions.sql
+++ dms/do_dataset_completion_actions.sql
@@ -1,19 +1,17 @@
 --
-CREATE OR REPLACE PROCEDURE public.do_dataset_completion_actions
-(
-    _datasetName text,
-    _completionState int = 0,
-    INOUT _message text default '',
-    INOUT _returnCode text default '';
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: do_dataset_completion_actions(text, integer, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.do_dataset_completion_actions(IN _datasetname text, IN _completionstate integer DEFAULT 0, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Sets state of dataset record given by _datasetName
-**      according to given completion code and
-**      adjusts related database entries accordingly.
+**      Updates dataset state based on the value of _completionState,
+**      provided the current dataset state is 2 (Capture In Progress)
+**
+**      If the new state is 3 (complete), calls add_archive_dataset and schedule_predefined_analysis_jobs
 **
 **  Arguments:
 **    _completionState   3 (complete), 5 (capture failed), 6 (received), 8 (prep. failed), 9 (not ready), 14 (Duplicate Dataset Files)
@@ -21,15 +19,15 @@ AS $$
 **  Auth:   grk
 **  Date:   11/04/2002
 **          08/06/2003 grk - Added handling for 'Not Ready' state
-**          07/01/2005 grk - Changed to use 'schedule_predefined_analysis_jobs'
-**          11/18/2010 mem - Now checking dataset rating and not calling schedule_predefined_analysis_jobs if the rating is -10 (unreviewed)
+**          07/01/2005 grk - Changed to use procedure Schedule_Predefined_Analysis_Jobs
+**          11/18/2010 mem - Now checking dataset rating and not calling Schedule_Predefined_Analysis_Jobs if the rating is -10 (unreviewed)
 **                         - Removed CD burn schedule code
-**          02/09/2011 mem - Added back calling schedule_predefined_analysis_jobs regardless of dataset rating
+**          02/09/2011 mem - Added back calling Schedule_Predefined_Analysis_Jobs regardless of dataset rating
 **                         - Required since predefines with Trigger_Before_Disposition should create jobs even if a dataset is unreviewed
 **          06/16/2017 mem - Restrict access using VerifySPAuthorized
 **          08/01/2017 mem - Use THROW if not authorized
 **          08/08/2018 mem - Add state 14 (Duplicate dataset files)
-**          12/15/2023 mem - Ported to PostgreSQL
+**          06/15/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -70,6 +68,9 @@ BEGIN
     -- Resolve dataset into ID and state
     ---------------------------------------------------
 
+    _datasetName := Trim(Coalesce(_datasetName, ''));
+    _completionState := Coalesce(_completionState, 0);
+
     SELECT dataset_id,
            dataset_state_id,
            dataset_rating_id
@@ -78,35 +79,36 @@ BEGIN
     WHERE dataset = _datasetName;
 
     If Not FOUND Then
-        _message := format('Could not get dataset ID for dataset %s', _datasetName);
+        _message := format('Dataset not found in t_dataset: %s', _datasetName);
         RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     ---------------------------------------------------
-    -- Verify that datset is in correct state
+    -- Verify that the dataset is in correct state
     ---------------------------------------------------
 
-    If Not _completionState in (3, 5, 6, 8, 9, 14) Then
+    If Not _completionState In (3, 5, 6, 8, 9, 14) Then
         _message := format('Completion state argument incorrect for %s', _datasetName);
         RAISE WARNING '%', _message;
         RETURN;
     End If;
 
-    If Not _datasetState in (2, 7) Then
-        _message := format('Dataset in incorrect state: %s', _datasetName);
+    -- Require that the current dataset state is 2 (Capture In Progress) or 7 (Prep. In Progress), though state 7 is obsolete
+    If Not _datasetState In (2, 7) Then
+        _message := format('Dataset in incorrect state (%s but expecting 2 or 7): %s', _datasetState, _datasetName);
         RAISE WARNING '%', _message;
         RETURN;
     End If;
 
-    If _datasetState = 2 and not _completionState in (3, 5, 6, 9, 14) Then
-        _message := format('Transition 1 not allowed: %s', _datasetName);
+    If _datasetState = 2 And Not _completionState In (3, 5, 6, 9, 14) Then
+        _message := format('State update not allowed since dataset state is 2 but completion state is not 3, 5, 6, 9, or 14: %s', _datasetName);
         RAISE WARNING '%', _message;
         RETURN;
     End If;
 
-    If _datasetState = 7 and not _completionState in (3, 6, 8) Then
-        _message := format('Transition 2 not allowed: %s', _datasetName);
+    If _datasetState = 7 And Not _completionState In (3, 6, 8) Then
+        _message := format('State update not allowed since dataset state is 7 but completion state is not 3, 6, or 8: %s', _datasetName);
         RAISE WARNING '%', _message;
         RETURN;
     End If;
@@ -116,15 +118,8 @@ BEGIN
     ---------------------------------------------------
 
     UPDATE t_dataset
-    SET dataset_state_id = _completionState,
+    SET dataset_state_id = _completionState
     WHERE dataset_id = _datasetID;
-
-    If Not FOUND Then
-        _returnCode := 'U5252';
-        _message := format('Update was unsuccessful for dataset %s', _datasetName);
-        RAISE WARNING '%', _message;
-        RETURN;
-    End If;
 
     ---------------------------------------------------
     -- Skip further changes if completion was anything
@@ -137,28 +132,31 @@ BEGIN
 
     ---------------------------------------------------
     -- Create a new dataset archive task
-    -- However, if 'ArchiveDisabled" has a value of 1 in T_Misc_Options, the archive task will not be created
+    -- However, if 'ArchiveDisabled' has a value of 1 in t_misc_options, the archive task will not be created
     ---------------------------------------------------
-    --
-    CALL Add_Archive_Dataset (_datasetID, _returnCode => _returnCode);
+
+    CALL add_archive_dataset (_datasetID, _message => _message, _returnCode => _returnCode);
 
     If _returnCode <> '' Then
         ROLLBACK;
 
-        _message := format('Update was unsuccessful for archive table %s', _datasetName);
+        _message := format('Return code %s reported by add_archive_dataset for dataset %s', _returnCode, _datasetName);
         RAISE WARNING '%', _message;
 
         RETURN;
     End If;
 
-    COMMIT;
-
     ---------------------------------------------------
     -- Schedule default analyses for this dataset
-    -- Call schedule_predefined_analysis_jobs even if the rating is -10 = Unreviewed
+    -- Call Schedule_Predefined_Analysis_Jobs even if the rating is -10 = Unreviewed
     ---------------------------------------------------
-    --
-    CALL schedule_predefined_analysis_jobs (_datasetName, _returnCode => _returnCode);
+
+    CALL schedule_predefined_analysis_jobs (
+                _datasetName,
+                _excludeDatasetsNotReleased => true,
+                _preventDuplicateJobs => true,
+                _message => _message,
+                _returnCode => _returnCode);
 
     If _message <> '' Then
         RAISE WARNING '%', _message;
@@ -167,4 +165,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE public.do_dataset_completion_actions IS 'DoDatasetCompletionActions';
+
+ALTER PROCEDURE public.do_dataset_completion_actions(IN _datasetname text, IN _completionstate integer, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE do_dataset_completion_actions(IN _datasetname text, IN _completionstate integer, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.do_dataset_completion_actions(IN _datasetname text, IN _completionstate integer, INOUT _message text, INOUT _returncode text) IS 'DoDatasetCompletionActions';
+
