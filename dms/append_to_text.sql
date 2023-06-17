838548d3622eb2c4e38a4b97cce3e22e5ec35cf3
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-16 19:16:05 -0700

    Change _addDuplicateText to a boolean

--- dms/append_to_text.sql
+++ dms/append_to_text.sql
@@ -1,8 +1,8 @@
 --
--- Name: append_to_text(text, text, integer, text, integer); Type: FUNCTION; Schema: public; Owner: d3l243
+-- Name: append_to_text(text, text, boolean, text, integer); Type: FUNCTION; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext integer DEFAULT 0, _delimiter text DEFAULT '; '::text, _maxlength integer DEFAULT 1024) RETURNS text
+CREATE OR REPLACE FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext boolean DEFAULT false, _delimiter text DEFAULT '; '::text, _maxlength integer DEFAULT 1024) RETURNS text
     LANGUAGE plpgsql
     AS $$
 /****************************************************
@@ -13,7 +13,7 @@ CREATE OR REPLACE FUNCTION public.append_to_text(_basetext text, _addnltext text
 **  Arguments:
 **    _baseText          Text to append to (can be an empty string)
 **    _addnlText         Additional text to add
-**    _addDuplicateText  When 0, prevent duplicate text from being added
+**    _addDuplicateText  When false, prevent duplicate text from being added
 **    _delimiter         Delimiter (semicolon by default)
 **    _maxLength         Maximum length of the returned text
 **
@@ -21,8 +21,10 @@ CREATE OR REPLACE FUNCTION public.append_to_text(_basetext text, _addnltext text
 **  Date:   05/12/2010 mem - Initial version
 **          06/12/2018 mem - Add parameter _maxLength
 **          02/10/2020 mem - Ported to PostgreSQL
-***         08/20/2022 mem - Do not append the delimiter if already present at the end of the base text
+**          08/20/2022 mem - Do not append the delimiter if already present at the end of the base text
 **          05/22/2023 mem - Use format() for string concatenation
+**          06/16/2023 mem - Change _addDuplicateText to a boolean
+**                         - Ignore _maxlength if _addnlText is an empty string
 **
 *****************************************************/
 DECLARE
@@ -30,35 +32,36 @@ DECLARE
     _updatedText text;
 BEGIN
 
-    If COALESCE(_baseText, '') = '' OR _baseText IS NULL THEN
+    If Trim(COALESCE(_baseText, '')) = '' THEN
         _updatedText = '';
     Else
         _updatedText = _baseText;
     End If;
 
-    If COALESCE(_addnlText, '') <> '' THEN
-        _charLoc := position(_addnlText in _updatedText);
-
-        If _charLoc = 0 Or _addDuplicateText > 0 Then
-            If _updatedText = '' Then
-                _updatedText := _addnlText;
-            Else
-                If char_length(Trim(_delimiter)) > 0 And RTrim(_updatedText) Like '%' || RTrim(_delimiter) Then
-                    -- The text already ends with the delimiter, though we may need to add a space
-                    If _delimiter Like '% ' And Not _updatedText Like '% ' Then
-                        _updatedText := format('%s ', _updatedText);
-                    End If;
-                Else
-                    _updatedText := format('%s%s', _updatedText, _delimiter);
-                End If;
-
-                _updatedText := format('%s%s', _updatedText, _addnlText);
-            End If;
-        End If;
-
+    If Trim(COALESCE(_addnlText, '')) = '' THEN
+        RETURN _updatedText;
     End If;
 
-    If _maxLength > 0 AND char_length(_updatedText) > _maxLength THEN
+    _charLoc := Position(Lower(_addnlText) In Lower(_updatedText));
+
+    If _charLoc = 0 Or _addDuplicateText Then
+        If _updatedText = '' Then
+            _updatedText := _addnlText;
+        Else
+            If char_length(Trim(_delimiter)) > 0 And RTrim(_updatedText) Like '%' || RTrim(_delimiter) Then
+                -- The text already ends with the delimiter, though we may need to add a space
+                If _delimiter Like '% ' And Not _updatedText Like '% ' Then
+                    _updatedText := format('%s ', _updatedText);
+                End If;
+            Else
+                _updatedText := format('%s%s', _updatedText, _delimiter);
+            End If;
+
+            _updatedText := format('%s%s', _updatedText, _addnlText);
+        End If;
+    End If;
+
+    If Coalesce(_maxLength, 0) > 0 AND char_length(_updatedText) > _maxLength THEN
         _updatedText := Substring(_updatedText, 1, _maxLength);
     End IF;
 
@@ -67,11 +70,11 @@ END
 $$;
 
 
-ALTER FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext integer, _delimiter text, _maxlength integer) OWNER TO d3l243;
+ALTER FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext boolean, _delimiter text, _maxlength integer) OWNER TO d3l243;
 
 --
--- Name: FUNCTION append_to_text(_basetext text, _addnltext text, _addduplicatetext integer, _delimiter text, _maxlength integer); Type: COMMENT; Schema: public; Owner: d3l243
+-- Name: FUNCTION append_to_text(_basetext text, _addnltext text, _addduplicatetext boolean, _delimiter text, _maxlength integer); Type: COMMENT; Schema: public; Owner: d3l243
 --
 
-COMMENT ON FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext integer, _delimiter text, _maxlength integer) IS 'AppendToText';
+COMMENT ON FUNCTION public.append_to_text(_basetext text, _addnltext text, _addduplicatetext boolean, _delimiter text, _maxlength integer) IS 'AppendToText';
 
