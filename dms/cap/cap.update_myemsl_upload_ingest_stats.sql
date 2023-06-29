99baa538516e6b7cdf2a9c446169c0a812af498c
alchemistmatt <matthew.monroe@pnnl.gov>
2023-06-29 16:49:32 -0700

    Implement procedure for updating metadata for a MyEMSL ingest task

--- dms/cap/cap.update_myemsl_upload_ingest_stats.sql
+++ dms/cap/cap.update_myemsl_upload_ingest_stats.sql
@@ -1,20 +1,15 @@
 --
-CREATE OR REPLACE PROCEDURE cap.update_myemsl_upload_ingest_stats
-(
-    _datasetID int,
-    _statusNum int,
-    _ingestStepsCompleted int,
-    _fatalError boolean default false,
-    _transactionId int default 0,
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: update_myemsl_upload_ingest_stats(integer, integer, integer, boolean, integer, text, text); Type: PROCEDURE; Schema: cap; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE cap.update_myemsl_upload_ingest_stats(IN _datasetid integer, IN _statusnum integer, IN _ingeststepscompleted integer, IN _fatalerror boolean DEFAULT false, IN _transactionid integer DEFAULT 0, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Updates column Ingest_Steps_Completed in cap.t_myemsl_uploads for the given MyEMSL ingest task
+**      Updates column ingest_steps_completed in cap.t_myemsl_uploads for the given MyEMSL ingest task
+**      If _fatalError is true, sets error_code to -1 if null or 0
 **
 **      This procedure is called by the ArchiveStatusCheckPlugin in the DMS Capture Manager
 **
@@ -22,8 +17,8 @@ AS $$
 **    _datasetID                Dataset ID
 **    _statusNum                The status number must match the specified DatasetID (this is a safety check)
 **    _ingestStepsCompleted     Number of ingest steps that were completed for this entry
-**    _fatalError               True if the ingest failed and the Error_Code column needs to be set to -1 (if currently 0 or null)
-**    _transactionId            Transaction ID (null or 0 if unknown); starting in July 2017, transactionId and Status_Num should match
+**    _fatalError               True if the ingest failed and the error_code column needs to be set to -1 (if currently 0 or null)
+**    _transactionId            Transaction ID (null or 0 if unknown); between July 2017 and May 2019, transactionId and status_num would always match; since May 2019, transaction_id is always null
 **    _message                  Output message
 **    _returnCode               Return code
 **
@@ -36,7 +31,7 @@ AS $$
 **          08/01/2017 mem - Use THROW instead of RAISERROR
 **          07/15/2019 mem - Filter on both Status_Num and Dataset_ID when updating T_MyEMSL_Uploads
 **          01/31/2020 mem - Add _returnCode, which duplicates the integer returned by this procedure; _returnCode is varchar for compatibility with Postgres error codes
-**          12/15/2023 mem - Ported to PostgreSQL
+**          06/29/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -44,6 +39,8 @@ DECLARE
     _currentProcedure text;
     _nameWithSchema text;
     _authorized boolean;
+
+    _errorCode int;
 
     _sqlState text;
     _exceptionMessage text;
@@ -79,11 +76,11 @@ BEGIN
         -- Validate the inputs
         ---------------------------------------------------
 
-        _datasetID := Coalesce(_datasetID, 0);
-        _statusNum := Coalesce(_statusNum, 0);
+        _datasetID            := Coalesce(_datasetID, 0);
+        _statusNum            := Coalesce(_statusNum, 0);
         _ingestStepsCompleted := Coalesce(_ingestStepsCompleted, 0);
-        _fatalError := Coalesce(_fatalError, false);
-        _transactionId := Coalesce(_transactionId, 0);
+        _fatalError           := Coalesce(_fatalError, false);
+        _transactionId        := Coalesce(_transactionId, 0);
 
         _message := '';
 
@@ -97,7 +94,7 @@ BEGIN
         -- Make sure the _statusNum exists in cap.t_myemsl_uploads
         ---------------------------------------------------
 
-        If Not Exists (SELECT * FROM cap.t_myemsl_uploads MU WHERE status_num = _statusNum) Then
+        If Not Exists (SELECT * FROM cap.t_myemsl_uploads WHERE status_num = _statusNum) Then
             _message := format('status_num %s not found in cap.t_myemsl_uploads', _statusNum);
             _returnCode := 'U5202';
             RETURN;
@@ -115,7 +112,7 @@ BEGIN
               dataset_id = _datasetID;
 
         If Not FOUND Then
-            _message := format('The DatasetID for Status_Num %s is not %s; will not update Ingest_Steps_Completed', _statusNum, _datasetID)
+            _message := format('The DatasetID for Status_Num %s is not %s; will not update Ingest_Steps_Completed', _statusNum, _datasetID);
             _returnCode := 'U5203';
             RETURN;
         End If;
@@ -124,8 +121,8 @@ BEGIN
         -- Possibly update the error code
         ---------------------------------------------------
 
-        If _fatalError And Coalesce(_returnCode, '') = '' Then
-            _returnCode := 'U5204';
+        If _fatalError And Coalesce(_errorCode, 0) = 0 Then
+            _errorCode := -1;
         End If;
 
         ---------------------------------------------------
@@ -138,9 +135,10 @@ BEGIN
             transaction_id = CASE WHEN _transactionId = 0 THEN transaction_id ELSE _transactionId END
         WHERE status_num = _statusNum AND
               dataset_id = _datasetID AND
-              (Coalesce(ingest_steps_completed, 0) <> _ingestStepsCompleted OR
-               Coalesce(error_code, 0) <> Coalesce(_errorCode, 0) OR
-               Coalesce(transaction_id, 0) <> Coalesce(_transactionId, 0) );
+              (ingest_steps_completed IS DISTINCT FROM _ingestStepsCompleted OR
+               error_code             IS DISTINCT FROM _errorCode OR
+               transaction_id         IS DISTINCT FROM _transactionId
+              );
 
     EXCEPTION
         WHEN OTHERS THEN
@@ -162,5 +160,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE cap.update_myemsl_upload_ingest_stats IS 'UpdateMyEMSLUploadIngestStats';
+
+ALTER PROCEDURE cap.update_myemsl_upload_ingest_stats(IN _datasetid integer, IN _statusnum integer, IN _ingeststepscompleted integer, IN _fatalerror boolean, IN _transactionid integer, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE update_myemsl_upload_ingest_stats(IN _datasetid integer, IN _statusnum integer, IN _ingeststepscompleted integer, IN _fatalerror boolean, IN _transactionid integer, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: cap; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE cap.update_myemsl_upload_ingest_stats(IN _datasetid integer, IN _statusnum integer, IN _ingeststepscompleted integer, IN _fatalerror boolean, IN _transactionid integer, INOUT _message text, INOUT _returncode text) IS 'UpdateMyEMSLUploadIngestStats';
 
