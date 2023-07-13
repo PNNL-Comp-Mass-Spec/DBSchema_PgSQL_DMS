954b24a65bae2be4d70a9be1e85fcb8b0b109d22
alchemistmatt <matthew.monroe@pnnl.gov>
2023-07-12 19:28:30 -0700

    Use XMLTable to extract values from XML

--- pending/dms/sw/sw.validate_extension_script_for_job.sql
+++ pending/dms/sw/sw.validate_extension_script_for_job.sql
@@ -65,6 +65,28 @@ BEGIN
 
     ---------------------------------------------------
     -- Get the XML for both job scripts
+    --
+    -- Example value for _currentScriptXML:
+    --
+    -- <JobScript Name="MSGFPlus_MzML">
+    --   <Step Number="1" Tool="MSXML_Gen" />
+    --   <Step Number="2" Tool="Mz_Refinery">   <Depends_On Step_Number="1" /></Step>
+    --   <Step Number="3" Tool="MSGFPlus">      <Depends_On Step_Number="2" /></Step>
+    --   <Step Number="4" Tool="DataExtractor"> <Depends_On Step_Number="3" /></Step>
+    --   <Step Number="5" Tool="MSGF">          <Depends_On Step_Number="4" /></Step>
+    --   <Step Number="6" Tool="IDPicker">      <Depends_On Step_Number="5" /></Step>
+    --   <Step Number="7" Tool="Results_Transfer">
+    --     <Depends_On Step_Number="1" Test="Target_Skipped" />
+    --     <Depends_On Step_Number="6" Enable_Only="1" />
+    --   </Step>
+    --   <Step Number="8" Tool="Results_Transfer">
+    --     <Depends_On Step_Number="2" Test="Target_Skipped" />
+    --     <Depends_On Step_Number="6" Enable_Only="1" />
+    --   </Step>
+    --   <Step Number="9" Tool="Results_Transfer">
+    --     <Depends_On Step_Number="6" />
+    --   </Step>
+    -- </JobScript>
     ---------------------------------------------------
 
     SELECT contents
@@ -91,73 +113,83 @@ BEGIN
         RAISE WARNING '%', _message;
 
         _returnCode := 'U6202';
-
         RETURN;
     End If;
 
     -- Make sure there is no overlap in step numbers between the two scripts
 
-    -- ToDo: Convert these to use XMLTABLE
-
     SELECT COUNT(*)
     INTO _overlapCount
-    FROM (    SELECT
-                xmlNode.value('@Number', 'text') Step_Number
-            FROM
-                _currentScriptXML.nodes('//Step') AS R(xmlNode)
-        ) C
-        INNER JOIN
-        (    SELECT
-                xmlNode.value('@Number', 'text') Step_Number
-            FROM
-                _extensionScriptXML.nodes('//Step') AS R(xmlNode)
-        ) E ON C.Step_Number = E.Step_Number
+    FROM ( SELECT xmltable.step_number
+           FROM ( SELECT _currentScriptXML AS rooted_xml
+                ) Src,
+                XMLTABLE('//JobScript/Step'
+                         PASSING Src.rooted_xml
+                         COLUMNS step_number int PATH '@Number')
+         ) C
+         INNER JOIN
+         ( SELECT xmltable.step_number
+           FROM ( SELECT _extensionScriptXML AS rooted_xml
+                ) Src,
+                XMLTABLE('//JobScript/Step'
+                         PASSING Src.rooted_xml
+                         COLUMNS step_number int PATH '@Number')
+         ) E ON C.step_number = E.step_number;
 
     If _overlapCount > 0 Then
         _message := format('One or more steps overlap between scripts "%s" and "%s"', _currentScript, _extensionScriptName);
 
         -- ToDo: Show the conflicts using RAISE INFO
-        -- ToDo: Replace xmlNode.Value with XMLTable
 
         -- Show the conflicting steps
-        -- Yes, this query is a bit more complex than was needed
-        --
-        WITH ConflictQ (Step_Number)
-        AS (    SELECT C.Step_Number
-                FROM (    SELECT
-                            xmlNode.value('@Number', 'text') Step_Number
-                        FROM
-                            _currentScriptXML.nodes('//Step') AS R(xmlNode)
-                    ) C
-                    INNER JOIN
-                    (    SELECT
-                            xmlNode.value('@Number', 'text') Step_Number
-                        FROM
-                            _extensionScriptXML.nodes('//Step') AS R(xmlNode)
-                    ) E ON C.Step_Number = E.Step_Number
+        WITH ConflictQ (step_number)
+        AS ( SELECT C.step_number
+             FROM ( SELECT xmltable.step_number
+                    FROM ( SELECT _currentScriptXML AS rooted_xml
+                         ) Src,
+                         XMLTABLE('//JobScript/Step'
+                                  PASSING Src.rooted_xml
+                                  COLUMNS step_number int PATH '@Number')
+                  ) C
+                  INNER JOIN
+                  ( SELECT xmltable.step_number
+                    FROM ( SELECT _extensionScriptXML AS rooted_xml
+                         ) Src,
+                         XMLTABLE('//JobScript/Step'
+                                  PASSING Src.rooted_xml
+                                  COLUMNS step_number int PATH '@Number')
+                  ) E ON C.step_number = E.step_number
         )
-        SELECT  ScriptSteps.Script,
-                ScriptSteps.Step_Number,
-                ScriptSteps.Step_Tool,
-                Case When ConflictQ.Step_Number Is Null Then 0 Else 1 End As Conflict
+        SELECT ScriptSteps.Script,
+               ScriptSteps.Step_Number,
+               ScriptSteps.Step_Tool,
+               CASE WHEN ConflictQ.Step_Number Is Null THEN 0 ELSE 1 END As Conflict
         FROM (
-            SELECT _currentScript AS Script,
-                xmlNode.value('@Number', 'text') Step_Number,
-                xmlNode.value('@Tool', 'text') Step_Tool
-            FROM
-                _currentScriptXML.nodes('//Step') AS R(xmlNode)
-            ) ScriptSteps LEFT OUTER JOIN ConflictQ ON ScriptSteps.Step_Number = ConflictQ.Step_Number
+                SELECT _currentScript AS Script,
+                       xmltable.step_number,
+                       xmltable.step_tool
+                FROM ( _currentScript AS rooted_xml
+                     ) Src,
+                     XMLTABLE('//JobScript/Step'
+                              PASSING Src.rooted_xml
+                              COLUMNS step_number int PATH '@Number',
+                                      step_tool   citext PATH '@Tool')
+            ) ScriptSteps LEFT OUTER JOIN ConflictQ ON ScriptSteps.step_number = ConflictQ.step_number
         UNION
         SELECT  ScriptSteps.Script,
                 ScriptSteps.Step_Number,
                 ScriptSteps.Step_Tool,
-                Case When ConflictQ.Step_Number Is Null Then 0 Else 1 End As Conflict
+                CASE WHEN ConflictQ.Step_Number Is Null THEN 0 ELSE 1 END As Conflict
         FROM (
-            SELECT _extensionScriptName AS Script,
-                xmlNode.value('@Number', 'text') Step_Number,
-                xmlNode.value('@Tool', 'text') Step_Tool
-            FROM
-                _extensionScriptXML.nodes('//Step') AS R(xmlNode)
+                SELECT _extensionScriptName AS Script,
+                       xmltable.step_number,
+                       xmltable.step_tool
+                FROM ( _extensionScriptName AS rooted_xml
+                     ) Src,
+                     XMLTABLE('//JobScript/Step'
+                              PASSING Src.rooted_xml
+                              COLUMNS step_number int PATH '@Number',
+                                      step_tool   citext PATH '@Tool')
             ) ScriptSteps LEFT OUTER JOIN ConflictQ ON ScriptSteps.Step_Number = ConflictQ.Step_Number
         ORDER BY Script, Step_Number;
 
