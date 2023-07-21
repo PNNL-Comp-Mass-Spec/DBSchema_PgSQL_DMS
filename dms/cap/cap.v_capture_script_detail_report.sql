de491d4e60560a3ea7b1fa4b2faef0b9f30b516e
alchemistmatt <matthew.monroe@pnnl.gov>
2023-07-20 19:25:16 -0700

    Add contents column to capture script view

--- dms/cap/cap.v_capture_script_detail_report.sql
+++ dms/cap/cap.v_capture_script_detail_report.sql
@@ -7,7 +7,8 @@ CREATE VIEW cap.v_capture_script_detail_report AS
     t_scripts.script,
     t_scripts.description,
     t_scripts.enabled,
-    t_scripts.results_tag
+    t_scripts.results_tag,
+    (('<pre>'::text || replace(replace(TRIM(BOTH FROM replace((t_scripts.contents)::text, '<'::text, ((chr(13) || chr(10)) || '<'::text))), '<'::text, '&lt;'::text), '>'::text, '&gt;'::text)) || '</pre>'::text) AS contents
    FROM cap.t_scripts;
 
 
