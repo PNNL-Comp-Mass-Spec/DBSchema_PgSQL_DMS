6891be587ba1b901862a7b183ac2ce27d7b47284
alchemistmatt <matthew.monroe@pnnl.gov>
2023-07-26 15:34:22 -0700

    Rename view to avoid confusion with public.v_analysis_job_processors_list_report

--- dms/mc/mc.v_manager_list_report.sql
+++ dms/mc/mc.v_manager_list_report.sql
@@ -1,8 +1,8 @@
 --
--- Name: v_analysis_job_processors_list_report; Type: VIEW; Schema: mc; Owner: d3l243
+-- Name: v_manager_list_report; Type: VIEW; Schema: mc; Owner: d3l243
 --
 
-CREATE VIEW mc.v_analysis_job_processors_list_report AS
+CREATE VIEW mc.v_manager_list_report AS
  SELECT m.mgr_id AS id,
     m.mgr_name AS name,
     mt.mgr_type_name AS type
@@ -10,12 +10,12 @@ CREATE VIEW mc.v_analysis_job_processors_list_report AS
      JOIN mc.t_mgr_types mt ON ((m.mgr_type_id = mt.mgr_type_id)));
 
 
-ALTER TABLE mc.v_analysis_job_processors_list_report OWNER TO d3l243;
+ALTER TABLE mc.v_manager_list_report OWNER TO d3l243;
 
 --
--- Name: TABLE v_analysis_job_processors_list_report; Type: ACL; Schema: mc; Owner: d3l243
+-- Name: TABLE v_manager_list_report; Type: ACL; Schema: mc; Owner: d3l243
 --
 
-GRANT SELECT ON TABLE mc.v_analysis_job_processors_list_report TO readaccess;
-GRANT SELECT ON TABLE mc.v_analysis_job_processors_list_report TO writeaccess;
+GRANT SELECT ON TABLE mc.v_manager_list_report TO readaccess;
+GRANT SELECT ON TABLE mc.v_manager_list_report TO writeaccess;
 
