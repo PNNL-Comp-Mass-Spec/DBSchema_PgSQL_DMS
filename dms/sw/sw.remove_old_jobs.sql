3679624c01e26b9f3f662c71910f0ce6ed6193f1
alchemistmatt <matthew.monroe@pnnl.gov>
2023-08-08 19:52:13 -0700

    Implement procedures for removing old jobs

--- dms/sw/sw.remove_old_jobs.sql
+++ dms/sw/sw.remove_old_jobs.sql
@@ -1,29 +1,24 @@
 --
-CREATE OR REPLACE PROCEDURE sw.remove_old_jobs
-(
-    _intervalDaysForSuccess real = 45,
-    _intervalDaysForFail int = 135,
-    _infoOnly boolean = false,
-    INOUT _message text default '',
-    INOUT _returnCode text default '',
-    _validateJobStepSuccess boolean = false,
-    _jobListOverride text = '',
-    _maxJobsToProcess int = 25000,
-    _logDeletions boolean = false,
-    _logToConsoleOnly boolean = false
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: remove_old_jobs(integer, integer, boolean, text, text, boolean, text, integer, boolean, boolean); Type: PROCEDURE; Schema: sw; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE sw.remove_old_jobs(IN _intervaldaysforsuccess integer DEFAULT 45, IN _intervaldaysforfail integer DEFAULT 135, IN _infoonly boolean DEFAULT false, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text, IN _validatejobstepsuccess boolean DEFAULT false, IN _joblistoverride text DEFAULT ''::text, IN _maxjobstoprocess integer DEFAULT 25000, IN _logdeletions boolean DEFAULT false, IN _logtoconsoleonly boolean DEFAULT false)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Delete jobs past their expiration date from the main tables in the database
+**      Delete old jobs from sw.t_jobs, sw.t_job_steps, etc.
 **
 **  Arguments:
 **    _intervalDaysForSuccess   Successful jobs must be this old to be deleted (0 -> no deletion)
 **    _intervalDaysForFail      Failed jobs must be this old to be deleted (0 -> no deletion)
+**    _infoOnly                 When true, preview deletes
+**    _message                  Output: status message
+**    _returnCode               Output: return code
 **    _validateJobStepSuccess   When true, remove any jobs that have failed, in progress, or holding job steps
 **    _jobListOverride          Comma-separated list of jobs to remove from T_Jobs, T_Job_Steps, and T_Job_Parameters
+**    _maxJobsToProcess         Maximum number of jobs to process
 **    _logDeletions             When true, logs each deleted job number in sw.T_Log_Entries
 **    _logToConsoleOnly         When _logDeletions is true, optionally set this to true to only show deleted job info in the output console (via RAISE INFO messages)
 **
@@ -39,7 +34,7 @@ AS $$
 **          01/18/2017 mem - Now counting job state 7 (No Intermediate Files Created) as Success
 **          08/17/2021 mem - When looking for completed or inactive jobs, use the Start time if Finish is null
 **                         - Also look for jobs with state 14 = Failed, Ignore Job Step States
-**          12/15/2023 mem - Ported to PostgreSQL
+**          08/08/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -47,29 +42,10 @@ DECLARE
     _saveTime timestamp := CURRENT_TIMESTAMP;
     _cutoffDateTimeForSuccess timestamp;
     _cutoffDateTimeForFail timestamp;
-    _jobInfo record
+    _jobInfo record;
 BEGIN
     _message := '';
     _returnCode := '';
-
-    ---------------------------------------------------
-    -- Create table to track the list of affected jobs
-    ---------------------------------------------------
-
-    CREATE TEMP TABLE Tmp_Selected_Jobs (
-        Job int not null,
-        State int
-    );
-
-    CREATE INDEX IX_Tmp_Selected_Jobs_Job ON Tmp_Selected_Jobs (Job);
-
-    CREATE TEMP TABLE Tmp_JobsNotInHistory (
-        Job int not null,
-        State int,
-        JobFinish timestamp
-    )
-
-    CREATE INDEX IX_Tmp_JobsNotInHistory ON Tmp_JobsNotInHistory (Job);
 
     ---------------------------------------------------
     -- Validate the inputs
@@ -93,13 +69,36 @@ BEGIN
     _validateJobStepSuccess := Coalesce(_validateJobStepSuccess, false);
 
     ---------------------------------------------------
+    -- Create table to track the list of affected jobs
+    ---------------------------------------------------
+
+    CREATE TEMP TABLE Tmp_Selected_Jobs (
+        Job int not null,
+        State int
+    );
+
+    CREATE INDEX IX_Tmp_Selected_Jobs_Job ON Tmp_Selected_Jobs (Job);
+
+    CREATE TEMP TABLE Tmp_JobsNotInHistory (
+        Job int not null,
+        State int,
+        JobFinish timestamp
+    );
+
+    CREATE INDEX IX_Tmp_JobsNotInHistory ON Tmp_JobsNotInHistory (Job);
+
+    ---------------------------------------------------
     -- Make sure the job Start and Finish values are up-to-date
     ---------------------------------------------------
 
-    CALL sw.synchronize_job_stats_with_job_steps (_infoOnly => false);
+    CALL sw.synchronize_job_stats_with_job_steps (
+                _infoOnly => false,
+                _completedJobsOnly => false,
+                _message => _message,           -- Output
+                _returnCode => _returnCode);    -- Output
 
     ---------------------------------------------------
-    -- Add old successful jobs to be removed to list
+    -- Add old successful jobs that should be deleted
     ---------------------------------------------------
 
     If _intervalDaysForSuccess > 0 Then
@@ -114,20 +113,23 @@ BEGIN
         ORDER BY finish
         LIMIT _maxJobsToProcess;
 
-        If _validateJobStepSuccess Then
+        If FOUND And _validateJobStepSuccess Then
             -- Remove any jobs that have failed, in progress, or holding job steps
-            DELETE Tmp_Selected_Jobs
-            FROM sw.t_job_steps JS
-            WHERE Tmp_Selected_Jobs.job = JS.job AND
-                  NOT JS.state IN (3, 5);               -- 3=Skipped, 5=Complete
+            DELETE FROM Tmp_Selected_Jobs
+            WHERE EXISTS ( SELECT 1
+                           FROM sw.t_job_steps JS
+                           WHERE Tmp_Selected_Jobs.job = JS.job AND
+                                 NOT JS.state IN (3, 5));               -- 3=Skipped, 5=Complete
             --
             GET DIAGNOSTICS _deleteCount = ROW_COUNT;
 
+            RAISE INFO '';
+
             If _deleteCount > 0 Then
-                _message := format('Warning: Removed %s %s with one or more steps that was not skipped or complete',
+                _message := format('Warning: Removed %s %s with one or more steps that are not skipped or complete',
                                     _deleteCount, public.check_plural(_deleteCount, 'job', 'jobs'));
                 RAISE WARNING '%', _message;
-            Else
+            ElsIf _infoOnly Then
                 RAISE INFO 'Successful jobs have been confirmed to all have successful (or skipped) steps';
             End If;
         End If;
@@ -135,7 +137,7 @@ BEGIN
     End If;
 
     ---------------------------------------------------
-    -- Add old failed jobs to be removed to list
+    -- Add old failed jobs that should be deleted
     ---------------------------------------------------
 
     If _intervalDaysForFail > 0 Then
@@ -162,7 +164,20 @@ BEGIN
         FROM sw.t_jobs
         WHERE job IN ( SELECT DISTINCT Value
                        FROM public.parse_delimited_integer_list ( _jobListOverride, ',' ) ) AND
-              NOT job IN ( SELECT job FROM Tmp_Selected_Jobs )
+              NOT job IN ( SELECT job FROM Tmp_Selected_Jobs );
+    End If;
+
+    If Not Exists (SELECT job FROM Tmp_Selected_Jobs) Then
+        If _infoOnly Then
+            _message := 'Did not find any old jobs to delete';
+
+            RAISE INFO '';
+            RAISE INFO '%', _message;
+        End If;
+
+        DROP TABLE Tmp_Selected_Jobs;
+        DROP TABLE Tmp_JobsNotInHistory;
+        RETURN;
     End If;
 
     ---------------------------------------------------
@@ -178,23 +193,27 @@ BEGIN
            ON Tmp_Selected_Jobs.job = JH.job
     WHERE JH.job IS NULL;
 
-    If Exists (Select * from Tmp_JobsNotInHistory) Then
+    If Exists (SELECT job FROM Tmp_JobsNotInHistory) Then
 
         UPDATE Tmp_JobsNotInHistory Target
         SET JobFinish = Coalesce(J.Finish, J.Start, CURRENT_TIMESTAMP)
         FROM sw.t_jobs J
         WHERE Target.job = J.job;
 
+        If _infoOnly Then
+            RAISE INFO '';
+        End If;
+
         FOR _jobInfo IN
-            SELECT Job As JobToAdd
+            SELECT Job As JobToAdd,
                    State,
-                   JobFinish Aa SaveTimeOverride
+                   JobFinish As SaveTimeOverride
             FROM Tmp_JobsNotInHistory
             WHERE Job > _jobToAdd
             ORDER BY Job
         LOOP
             If _infoOnly Then
-                RAISE INFO 'Call copy_job_to_history for job % with date %', _jobInfo.JobToAdd, public.timestamp_text(_jobInfo.SaveTimeOverride);
+                RAISE INFO 'Call sw.copy_job_to_history for job % with date %', _jobInfo.JobToAdd, public.timestamp_text(_jobInfo.SaveTimeOverride);
             Else
                 CALL sw.copy_job_to_history (
                             _jobInfo.JobToAdd,
@@ -202,8 +221,7 @@ BEGIN
                             _overrideSaveTime => true,
                             _saveTimeOverride => _jobInfo.JobToAdd.SaveTimeOverride,
                             _message => _message,                   -- Output
-                            _returnCode => _returnCode              -- Output
-                            );
+                            _returnCode => _returnCode);            -- Output
             End If;
         END LOOP;
 
@@ -214,8 +232,9 @@ BEGIN
     ---------------------------------------------------
 
     CALL sw.remove_selected_jobs (
-            _infoOnly,
+            _infoOnly => _infoOnly,
             _message => _message,
+            _returncode => _returncode,
             _logDeletions => _logDeletions,
             _logToConsoleOnly => _logToConsoleOnly);
 
@@ -224,4 +243,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE sw.remove_old_jobs IS 'RemoveOldJobs';
+
+ALTER PROCEDURE sw.remove_old_jobs(IN _intervaldaysforsuccess integer, IN _intervaldaysforfail integer, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _validatejobstepsuccess boolean, IN _joblistoverride text, IN _maxjobstoprocess integer, IN _logdeletions boolean, IN _logtoconsoleonly boolean) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE remove_old_jobs(IN _intervaldaysforsuccess integer, IN _intervaldaysforfail integer, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _validatejobstepsuccess boolean, IN _joblistoverride text, IN _maxjobstoprocess integer, IN _logdeletions boolean, IN _logtoconsoleonly boolean); Type: COMMENT; Schema: sw; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE sw.remove_old_jobs(IN _intervaldaysforsuccess integer, IN _intervaldaysforfail integer, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _validatejobstepsuccess boolean, IN _joblistoverride text, IN _maxjobstoprocess integer, IN _logdeletions boolean, IN _logtoconsoleonly boolean) IS 'RemoveOldJobs';
+
