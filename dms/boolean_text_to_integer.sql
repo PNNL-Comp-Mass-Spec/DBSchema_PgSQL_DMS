c0022ba61f0dd653ce492fee65b699e14cd9e9e5
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-05 19:32:29 -0700

    Rename function after renaming file; return an integer instead of a smallint

--- dms/boolean_text_to_integer.sql
+++ dms/boolean_text_to_integer.sql
@@ -1,8 +1,8 @@
 --
--- Name: boolean_text_to_tinyint(public.citext); Type: FUNCTION; Schema: public; Owner: d3l243
+-- Name: boolean_text_to_integer(text); Type: FUNCTION; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION public.boolean_text_to_tinyint(_booleantext public.citext) RETURNS smallint
+CREATE OR REPLACE FUNCTION public.boolean_text_to_integer(_booleantext text) RETURNS integer
     LANGUAGE plpgsql
     AS $$
 /****************************************************
@@ -16,16 +16,19 @@ CREATE OR REPLACE FUNCTION public.boolean_text_to_tinyint(_booleantext public.ci
 **          06/17/2022 mem - Ported to PostgreSQL
 **          05/22/2023 mem - Capitalize reserved word
 **          09/08/2023 mem - Adjust capitalization of keywords
+**          10/05/2023 mem - Rename function to boolean_text_to_integer and return an integer instead of a smallint
 **
 *****************************************************/
 DECLARE
-    _value int := 0;
+    _value int;
 BEGIN
 
     _booleanText := Trim(Coalesce(_booleanText, ''));
 
-    If _booleanText = 'Yes' Or _booleanText = 'Y' Or _booleanText = '1' Or _booleanText = 'True' Or _booleanText = 'T' Then
+    If _booleanText::citext IN ('Yes', 'Y', '1', 'True', 'T') Then
         _value := 1;
+    Else
+        _value := 0;
     End If;
 
     RETURN _value;
@@ -33,11 +36,11 @@ END
 $$;
 
 
-ALTER FUNCTION public.boolean_text_to_tinyint(_booleantext public.citext) OWNER TO d3l243;
+ALTER FUNCTION public.boolean_text_to_integer(_booleantext text) OWNER TO d3l243;
 
 --
--- Name: FUNCTION boolean_text_to_tinyint(_booleantext public.citext); Type: COMMENT; Schema: public; Owner: d3l243
+-- Name: FUNCTION boolean_text_to_integer(_booleantext text); Type: COMMENT; Schema: public; Owner: d3l243
 --
 
-COMMENT ON FUNCTION public.boolean_text_to_tinyint(_booleantext public.citext) IS 'BooleanTextToTinyint';
+COMMENT ON FUNCTION public.boolean_text_to_integer(_booleantext text) IS 'BooleanTextToInteger or BooleanTextToTinyint';
 
