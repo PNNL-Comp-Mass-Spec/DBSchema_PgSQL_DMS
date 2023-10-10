d1497ba15e93808d2569919ae217beee5a92baed
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-09 19:57:18 -0700

    Rename parameter _secSep to _separationGroup

--- dms/lookup_instrument_run_info_from_experiment_sample_prep.sql
+++ dms/lookup_instrument_run_info_from_experiment_sample_prep.sql
@@ -2,7 +2,7 @@
 -- Name: lookup_instrument_run_info_from_experiment_sample_prep(text, text, text, text, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text DEFAULT ''::text, INOUT _datasettype text DEFAULT ''::text, INOUT _instrumentsettings text DEFAULT ''::text, INOUT _secsep text DEFAULT ''::text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+CREATE OR REPLACE PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text DEFAULT ''::text, INOUT _datasettype text DEFAULT ''::text, INOUT _instrumentsettings text DEFAULT ''::text, INOUT _separationgroup text DEFAULT ''::text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
     LANGUAGE plpgsql
     AS $$
 /****************************************************
@@ -15,7 +15,7 @@ CREATE OR REPLACE PROCEDURE public.lookup_instrument_run_info_from_experiment_sa
 **    _instrumentGroup      Input/output: Instrument group;    if this is '(lookup)', will override with the instrument info from the sample prep request (if found)
 **    _datasetType          Input/output: Dataset type;        if this is '(lookup)', will override with the instrument info from the sample prep request (if found)
 **    _instrumentSettings   Input/output: Instrument settings; if this is '(lookup)', will be set to 'na'
-**    _secSep               Input/Output: LC separation type
+**    _separationGroup      Input/Output: LC separation group
 **    _message              Status message
 **    _returnCode           Return code
 **
@@ -24,9 +24,10 @@ CREATE OR REPLACE PROCEDURE public.lookup_instrument_run_info_from_experiment_sa
 **          01/09/2012 grk - Added _secSep
 **          03/28/2013 mem - Now returning more explicit error messages when the experiment does not have an associated sample prep request
 **          06/10/2014 mem - Now using Instrument_Group in T_Sample_Prep_Request
-**          08/20/2014 mem - Switched from Instrument_Name to Instrument_Group
-**                         - Renamed parameter _instrumentName to _instrumentGroup
+**          08/20/2014 mem - Switch from Instrument_Name to Instrument_Group
+**                         - Rename parameter _instrumentName to _instrumentGroup
 **          09/13/2023 mem - Ported to PostgreSQL
+**          10/09/2023 mem - Rename parameter _secSep to _separationGroup
 **
 *****************************************************/
 DECLARE
@@ -45,7 +46,7 @@ BEGIN
     _instrumentGroup    := Trim(Coalesce(_instrumentGroup, ''));
     _datasetType        := Trim(Coalesce(_datasetType, ''));
     _instrumentSettings := Trim(Coalesce(_instrumentSettings, ''));
-    _secSep             := Trim(Coalesce(_secSep, ''));
+    _separationGroup    := Trim(Coalesce(_separationGroup, ''));
 
     ---------------------------------------------------
     -- Find associated sample prep request for experiment
@@ -97,7 +98,7 @@ BEGIN
     SELECT COALESCE(instrument_group, instrument_name, '') As InstrumentGroup,
            COALESCE(dataset_type, '') As DatasetType,
            COALESCE(instrument_analysis_specifications, '') As InstrumentSettings,
-           COALESCE(separation_type, '') As SeparationType
+           COALESCE(separation_type, '') As SeparationGroup
     INTO _instrumentInfo
     FROM t_sample_prep_request
     WHERE prep_request_id = _prepRequestID;
@@ -109,17 +110,17 @@ BEGIN
     _instrumentGroup    := CASE WHEN _instrumentGroup::citext    = _ovr THEN _instrumentInfo.InstrumentGroup    ELSE _instrumentGroup END;
     _datasetType        := CASE WHEN _datasetType::citext        = _ovr THEN _instrumentInfo.DatasetType        ELSE _datasetType END;
     _instrumentSettings := CASE WHEN _instrumentSettings::citext = _ovr THEN _instrumentInfo.InstrumentSettings ELSE _instrumentSettings END;
-    _secSep             := CASE WHEN _secSep::citext             = _ovr THEN _instrumentInfo.SeparationType     ELSE _secSep END;
+    _separationGroup    := CASE WHEN _separationGroup::citext    = _ovr THEN _instrumentInfo.SeparationGroup    ELSE _separationGroup END;
 
 END
 $$;
 
 
-ALTER PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _secsep text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+ALTER PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _separationgroup text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
 
 --
--- Name: PROCEDURE lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _secsep text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+-- Name: PROCEDURE lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _separationgroup text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
 --
 
-COMMENT ON PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _secsep text, INOUT _message text, INOUT _returncode text) IS 'LookupInstrumentRunInfoFromExperimentSamplePrep';
+COMMENT ON PROCEDURE public.lookup_instrument_run_info_from_experiment_sample_prep(IN _experimentname text, INOUT _instrumentgroup text, INOUT _datasettype text, INOUT _instrumentsettings text, INOUT _separationgroup text, INOUT _message text, INOUT _returncode text) IS 'LookupInstrumentRunInfoFromExperimentSamplePrep';
 
