542d914221defce98da393abfdb1c79d0d62325c
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-10 19:49:54 -0700

    Overload function months_between()

--- dms/months_between.sql
+++ dms/months_between.sql
@@ -1,6 +1,7 @@
 --
 -- Name: months_between(timestamp without time zone, timestamp without time zone); Type: FUNCTION; Schema: public; Owner: d3l243
 --
+-- Overload 1
 
 CREATE OR REPLACE FUNCTION public.months_between(_start timestamp without time zone, _end timestamp without time zone) RETURNS integer
     LANGUAGE plpgsql IMMUTABLE STRICT
@@ -10,6 +11,10 @@ CREATE OR REPLACE FUNCTION public.months_between(_start timestamp without time z
 **  Desc:
 **      Returns the calendar month difference between two timestamps
 **      (from https://stackoverflow.com/a/51928739/1179467)
+**
+**  Arguments:
+**    _start    First timestamp  (as a timestamp without a timezone)
+**    _end      Second timestamp (as a timestamp without a timezone)
 **
 **  Example results:
 **      SELECT * FROM public.months_between('2022-02-28', '2022-03-02');         -- Result:  1
@@ -31,4 +36,41 @@ $_$;
 
 
 ALTER FUNCTION public.months_between(_start timestamp without time zone, _end timestamp without time zone) OWNER TO d3l243;
+
+--
+-- Name: months_between(timestamp without time zone, timestamp with time zone); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+-- Overload 2
+
+CREATE OR REPLACE FUNCTION public.months_between(_start timestamp without time zone, _end timestamp with time zone) RETURNS integer
+    LANGUAGE plpgsql IMMUTABLE STRICT
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Returns the calendar month difference between two timestamps
+**
+**  Arguments:
+**    _start    First timestamp  (as a timestamp without a timezone)
+**    _end      Second timestamp (as a timestamp with a timezone)
+**
+**  Example results:
+**      SELECT * FROM public.months_between('2022-02-28', '2022-03-02'::timestamptz);   -- Result:  1
+**      SELECT * FROM public.months_between('2022-01-01', '2022-03-02'::timestamptz);   -- Result:  2
+**      SELECT * FROM public.months_between('2022-03-01', '2022-03-02'::timestamptz);   -- Result:  0
+**      SELECT * FROM public.months_between('2021-10-20', '2022-03-02'::timestamptz);   -- Result:  5
+**      SELECT * FROM public.months_between('2022-10-20', '2022-03-30'::timestamptz);   -- Result: -7
+**      SELECT * FROM public.months_between(null, CURRENT_TIMESTAMP);                   -- Result: null
+**
+**  Auth:   mem
+**  Date:   10/10/2023 mem - Initial version
+**
+*****************************************************/
+BEGIN
+    RETURN public.months_between(_start, _end::timestamp);
+END
+$$;
+
+
+ALTER FUNCTION public.months_between(_start timestamp without time zone, _end timestamp with time zone) OWNER TO d3l243;
 
