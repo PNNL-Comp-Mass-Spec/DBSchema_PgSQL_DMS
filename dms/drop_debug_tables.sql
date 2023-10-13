8e0f2f42ba222446da22411ce0b11c1f44aac85b
alchemistmatt <matthew.monroe@pnnl.gov>
2023-10-13 16:29:45 -0700

    Also drop the t_debug_tmp tables created by make_local_job_in_broker and make_local_task_in_broker

--- dms/drop_debug_tables.sql
+++ dms/drop_debug_tables.sql
@@ -10,7 +10,9 @@ CREATE OR REPLACE PROCEDURE public.drop_debug_tables(IN _infoonly boolean DEFAUL
 **  Desc:
 **      Removes tables created for debugging purposes by the following procedures
 **      - public.predefined_analysis_datasets
+**      - cap.make_local_task_in_broker
 **      - cap.move_tasks_to_main_tables
+**      - sw.make_local_job_in_broker
 **      - sw.move_jobs_to_main_tables
 **
 **  Arguments:
@@ -18,6 +20,7 @@ CREATE OR REPLACE PROCEDURE public.drop_debug_tables(IN _infoonly boolean DEFAUL
 **
 **  Auth:   mem
 **  Date:   10/12/2023 mem - Initial release
+**          10/13/2023 mem - Also drop the t_debug_tmp tables created by make_local_job_in_broker and make_local_task_in_broker
 **
 *****************************************************/
 DECLARE
@@ -45,13 +48,15 @@ BEGIN
     SELECT schemaname, tablename
     FROM pg_catalog.pg_tables
     WHERE schemaname = 'public' AND tablename::citext IN ('T_Tmp_PredefinedAnalysisDatasets') OR
-          schemaname = 'cap'    AND tablename::citext IN ('T_Tmp_New_Tasks', 'T_Tmp_New_Task_Steps', 'T_Tmp_New_Task_Step_Dependencies', 'T_Tmp_New_Task_Parameters') OR
-          schemaname = 'sw'     AND tablename::citext IN ('T_Tmp_New_Jobs',  'T_Tmp_New_Job_Steps',  'T_Tmp_New_Job_Step_Dependencies',  'T_Tmp_New_Job_Parameters');
+          schemaname = 'cap'    AND tablename::citext IN ('t_tmp_new_tasks',   't_tmp_new_task_steps',   't_tmp_new_task_step_dependencies',   't_tmp_new_task_parameters',
+                                                          't_debug_tmp_tasks', 't_debug_tmp_task_steps', 't_debug_tmp_task_step_dependencies', 't_debug_tmp_task_parameters') OR
+          schemaname = 'sw'     AND tablename::citext IN ('t_tmp_new_jobs',    't_tmp_new_job_steps',    't_tmp_new_job_step_dependencies',    't_tmp_new_job_parameters',
+                                                          't_debug_tmp_jobs',  't_debug_tmp_job_steps',  't_debug_tmp_job_step_dependencies',  't_debug_tmp_job_parameters');
 
     GET DIAGNOSTICS _tableCount = ROW_COUNT;
 
     If _tableCount = 0 Then
-        _message := 'Did not find any T_Tmp debug tables';
+        _message := 'Did not find any temp debug tables';
         RAISE INFO '%', _message;
 
         DROP TABLE T_Tmp_TablesToDelete;
@@ -63,7 +68,7 @@ BEGIN
     FROM T_Tmp_TablesToDelete;
 
     If _infoOnly Then
-        _message := format('Would delete the following T_Tmp debug %s:', public.check_plural(_tableCount, 'table', 'tables'));
+        _message := format('Would delete the following temp debug %s:', public.check_plural(_tableCount, 'table', 'tables'));
         RAISE INFO '%', _message;
 
         _message := format('%s %s', _message, _existingTables);
@@ -84,7 +89,7 @@ BEGIN
     END LOOP;
 
     If Not _infoOnly Then
-        _message := format('Deleted %s T_Tmp debug %s: %s', _tableCount, public.check_plural(_tableCount, 'table', 'tables'), _existingTables);
+        _message := format('Deleted %s temp debug %s: %s', _tableCount, public.check_plural(_tableCount, 'table', 'tables'), _existingTables);
         RAISE INFO '%', _message;
     End If;
 
