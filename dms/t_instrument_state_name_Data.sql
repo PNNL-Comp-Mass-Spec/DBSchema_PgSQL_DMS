10f95672c8671a0ab780a7aa03f1b61218ba1eae
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-02 15:12:54 -0800

    Whitespace and comment updates after updating to pg_dump v16.1

--- dms/t_instrument_state_name_Data.sql
+++ dms/t_instrument_state_name_Data.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 15.4
--- Dumped by pg_dump version 15.1
+-- Dumped from database version 15.5
+-- Dumped by pg_dump version 16.1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
