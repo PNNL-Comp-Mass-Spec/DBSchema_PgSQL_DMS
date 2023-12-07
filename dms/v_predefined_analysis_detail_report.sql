17128905b229011f351feec06f9817f5ab9a03de
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-06 21:11:58 -0800

    Add support for scan type criteria in predefined analysis rules

--- dms/v_predefined_analysis_detail_report.sql
+++ dms/v_predefined_analysis_detail_report.sql
@@ -4,9 +4,9 @@
 
 CREATE VIEW public.v_predefined_analysis_detail_report AS
  SELECT pa.predefine_id AS id,
+    pa.instrument_class_criteria,
     pa.predefine_level AS level,
     pa.predefine_sequence AS sequence,
-    pa.instrument_class_criteria,
     pa.next_level,
         CASE
             WHEN (pa.trigger_before_disposition = 1) THEN 'Before Disposition'::public.citext
@@ -16,17 +16,19 @@ CREATE VIEW public.v_predefined_analysis_detail_report AS
             WHEN 0 THEN 'Export'::public.citext
             ELSE 'No Export'::public.citext
         END AS export_mode,
+    pa.instrument_name_criteria AS instrument_criteria,
+    pa.instrument_excl_criteria AS instrument_exclusion,
+    pa.organism_name_criteria AS organism_criteria,
     pa.campaign_name_criteria AS campaign_criteria,
     pa.campaign_excl_criteria AS campaign_exclusion,
     pa.experiment_name_criteria AS experiment_criteria,
     pa.experiment_excl_criteria AS experiment_exclusion,
-    pa.instrument_name_criteria AS instrument_criteria,
-    pa.instrument_excl_criteria AS instrument_exclusion,
-    pa.organism_name_criteria AS organism_criteria,
+    pa.exp_comment_criteria AS experiment_comment_criteria,
     pa.dataset_name_criteria AS dataset_criteria,
     pa.dataset_excl_criteria AS dataset_exclusion,
     pa.dataset_type_criteria,
-    pa.exp_comment_criteria AS experiment_comment_criteria,
+    pa.scan_type_criteria,
+    pa.scan_type_excl_criteria AS scan_type_exclusion,
     pa.labelling_incl_criteria AS experiment_labelling_criteria,
     pa.labelling_excl_criteria AS experiment_labelling_exclusion,
     pa.separation_type_criteria AS separation_criteria,
@@ -39,13 +41,13 @@ CREATE VIEW public.v_predefined_analysis_detail_report AS
     pa.organism_db_name,
     pa.protein_collection_list,
     pa.protein_options_list,
-    pa.special_processing,
     pa.priority,
     pa.enabled,
-    pa.created,
-    pa.last_affected,
     pa.description,
-    pa.creator
+    pa.special_processing,
+    pa.created,
+    pa.creator,
+    pa.last_affected
    FROM (public.t_predefined_analysis pa
      JOIN public.t_organisms org ON ((pa.organism_id = org.organism_id)));
 
--- dms/v_predefined_analysis_entry.sql
+++ dms/v_predefined_analysis_entry.sql
@@ -3,23 +3,32 @@
 --
 
 CREATE VIEW public.v_predefined_analysis_entry AS
- SELECT pa.predefine_level AS level,
+ SELECT pa.predefine_id AS id,
+    pa.predefine_level AS level,
     pa.predefine_sequence AS sequence,
+    pa.next_level,
+    pa.trigger_before_disposition,
+        CASE pa.propagation_mode
+            WHEN 0 THEN 'Export'::text
+            ELSE 'No Export'::text
+        END AS propagation_mode,
     pa.instrument_class_criteria,
-    pa.campaign_name_criteria,
-    pa.experiment_name_criteria,
     pa.instrument_name_criteria,
     pa.instrument_excl_criteria,
+    pa.campaign_name_criteria,
+    pa.campaign_excl_criteria,
+    pa.experiment_name_criteria,
+    pa.experiment_excl_criteria,
+    pa.exp_comment_criteria,
     pa.organism_name_criteria,
     pa.dataset_name_criteria,
-    pa.exp_comment_criteria,
+    pa.dataset_excl_criteria,
+    pa.dataset_type_criteria,
+    pa.scan_type_criteria,
+    pa.scan_type_excl_criteria,
     pa.labelling_incl_criteria,
     pa.labelling_excl_criteria,
     pa.separation_type_criteria,
-    pa.campaign_excl_criteria,
-    pa.experiment_excl_criteria,
-    pa.dataset_excl_criteria,
-    pa.dataset_type_criteria,
     pa.analysis_tool_name,
     pa.param_file_name,
     pa.settings_file_name,
@@ -29,17 +38,10 @@ CREATE VIEW public.v_predefined_analysis_entry AS
     pa.protein_options_list AS prot_coll_options_list,
     pa.priority,
     pa.enabled,
-    pa.created,
     pa.description,
-    pa.creator,
-    pa.predefine_id AS id,
-    pa.next_level,
-    pa.trigger_before_disposition,
-        CASE pa.propagation_mode
-            WHEN 0 THEN 'Export'::text
-            ELSE 'No Export'::text
-        END AS propagation_mode,
-    pa.special_processing
+    pa.special_processing,
+    pa.created,
+    pa.creator
    FROM (public.t_predefined_analysis pa
      JOIN public.t_organisms org ON ((pa.organism_id = org.organism_id)));
 
