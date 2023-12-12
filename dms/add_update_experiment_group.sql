9cd1baf9f58c9e1c6203cdbc83feb294ff67bc3c
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-11 19:23:04 -0800

    Implement procedure for adding/updating experiment groups

--- dms/add_update_experiment_group.sql
+++ dms/add_update_experiment_group.sql
@@ -1,31 +1,27 @@
 --
-CREATE OR REPLACE PROCEDURE public.add_update_experiment_group
-(
-    INOUT _id int,
-    _groupType text,
-    _groupName text,
-    _description text,
-    _experimentList text,
-    _parentExp text,
-    _researcher text,
-    _mode text = 'add',
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: add_update_experiment_group(integer, text, text, text, text, text, text, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.add_update_experiment_group(INOUT _id integer, IN _grouptype text, IN _groupname text, IN _description text, IN _experimentlist text, IN _parentexp text, IN _researcher text, IN _mode text DEFAULT 'add'::text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
 **      Adds new or edits an existing experiment group
 **
+**      Used by web page https://dms2.pnl.gov/experiment_group/create to group together related experiments as a general experiment group
+**      Used by web page https://dms2.pnl.gov/experiment_group/edit to edit both general experiment groups and fraction-based experiment groups
+**
+**      Note that the DMS website does not allow users to change the parent experiment name when editing an experiment group
+**
 **  Arguments:
 **    _id               Input/output: experiment group ID
 **    _groupType        Experiment group type: 'General' or 'Fraction'
-**    _groupName        Experiment group name (previously _tab)
+**    _groupName        Experiment group name (previously _tab); allowed to be an empty string and allowed to be the same group name as another experiment group
 **    _description      Description
 **    _experimentList   Comma-separated list of experiment names
-**    _parentExp        Parent experiment name
+**    _parentExp        Parent experiment name; auto-defined as 'Placeholder' by https://dms2.pnl.gov/experiment_group/create
 **    _researcher       Researcher username
 **    _mode             Mode: 'add' or 'update'
 **    _message          Output message
@@ -44,7 +40,7 @@ AS $$
 **          12/06/2018 mem - Call update_experiment_group_member_count to update T_Experiment_Groups
 **          12/08/2020 mem - Lookup Username from T_Users using the validated user ID
 **          11/18/2022 mem - Rename parameter to _groupName
-**          12/15/2024 mem - Ported to PostgreSQL
+**          12/11/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -85,6 +81,25 @@ BEGIN
     End If;
 
     ---------------------------------------------------
+    -- Validate the inputs
+    ---------------------------------------------------
+
+    _groupType   := Trim(Coalesce(_groupType, ''));
+    _groupName   := Trim(Coalesce(_groupName, ''));
+    _description := Trim(Coalesce(_description, ''));
+    _parentExp   := Trim(Coalesce(_parentExp, ''));
+    _researcher  := Trim(Coalesce(_researcher, ''));
+    _mode        := Trim(Lower(Coalesce(_mode, '')));
+
+    If Not _groupType::citext IN ('General', 'Fraction') Then
+        _message := format('"%s" is not a valid group type; the only supported group types are "General" or "Fraction"', _groupType);
+        RAISE WARNING '%', _message;
+
+        _returnCode := 'U5201';
+        RETURN;
+    End If;
+
+    ---------------------------------------------------
     -- Resolve parent experiment name to ID
     ---------------------------------------------------
 
@@ -93,7 +108,15 @@ BEGIN
         SELECT exp_id
         INTO _parentExpID
         FROM t_experiments
-        WHERE experiment = _parentExp;
+        WHERE experiment = _parentExp::citext;
+
+        If Not FOUND Then
+            _message := 'Unable to determine the experiment ID of experiment "%"', _parentExp;
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5202';
+            RETURN;
+        End If;
 
     End If;
 
@@ -101,29 +124,11 @@ BEGIN
         SELECT exp_id
         INTO _parentExpID
         FROM t_experiments
-        WHERE experiment = 'Placeholder'
+        WHERE experiment = 'Placeholder';
 
         If Coalesce(_parentExpID, 0) = 0 Then
             _logErrors := true;
-            _message := 'Unable to determine the Exp_ID for the Placeholder experiment';
-            RAISE WARNING '%', _message;
-
-            _returnCode := 'U5202';
-            RETURN;
-        End If;
-    End If;
-
-    _mode := Trim(Lower(Coalesce(_mode, '')));
-
-    ---------------------------------------------------
-    -- Is entry already in database? (only applies to updates)
-    ---------------------------------------------------
-
-    If _mode = 'update' Then
-        -- Cannot update a non-existent entry
-        --
-        If Not Exists (SELECT group_id FROM t_experiment_groups WHERE group_id = _id) Then
-            _message := format('Cannot update: GroupID does not exist in database: %s', _id)
+            _message := 'Unable to determine the experiment ID of the Placeholder experiment';
             RAISE WARNING '%', _message;
 
             _returnCode := 'U5203';
@@ -131,44 +136,68 @@ BEGIN
         End If;
     End If;
 
+    ---------------------------------------------------
+    -- Is entry already in database? (only applies to updates)
+    ---------------------------------------------------
+
+    If _mode = 'update' Then
+        -- Cannot update a non-existent entry
+
+        If Not Exists (SELECT group_id FROM t_experiment_groups WHERE group_id = _id) Then
+            _message := format('Cannot update: GroupID %s does not exist in the database', _id);
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5204';
+            RETURN;
+        End If;
+    End If;
+
     _logErrors := true;
 
     ---------------------------------------------------
-    -- Create temporary table for experiments in list
+    -- Create a temporary table for the experiment names
     ---------------------------------------------------
 
     CREATE TEMP TABLE Tmp_Experiments (
-        Experiment text,
+        Experiment citext,
         Exp_ID     int
-    )
+    );
 
     ---------------------------------------------------
-    -- Populate temporary table from list
+    -- Populate the temporary table
     ---------------------------------------------------
 
     INSERT INTO Tmp_Experiments ( Experiment,
                                   Exp_ID )
     SELECT Value AS Experiment,
            0 AS Exp_ID
-    FROM public.parse_delimited_list(_experimentList)
+    FROM public.parse_delimited_list(_experimentList);
+
+    If Not Exists (SELECT Exp_ID FROM Tmp_Experiments) Then
+        _logErrors := false;
+        _message := format('One or more experiment names must be specified');
+        RAISE WARNING '%', _message;
+
+        _returnCode := 'U5205';
+        DROP TABLE Tmp_Experiments;
+        RETURN;
+    End If;
 
     ---------------------------------------------------
-    -- Resolve experiment name to ID in temp table
+    -- Resolve experiment name to ID
     ---------------------------------------------------
 
-    UPDATE Tmp_Experiments T
-    SET T.exp_id = S.exp_id
-    FROM t_experiments S
-    WHERE T.experiment = S.experiment;
+    UPDATE Tmp_Experiments AS Target
+    SET exp_id = Src.exp_id
+    FROM t_experiments Src
+    WHERE Target.experiment = Src.experiment;
 
     ---------------------------------------------------
     -- Check status of prospective member experiments
     ---------------------------------------------------
 
-    -- Do all experiments in list actually exist?
-    --
-    _count := 0;
-    --
+    -- Do all experiments in the list actually exist?
+
     SELECT COUNT(*)
     INTO _count
     FROM Tmp_Experiments
@@ -181,10 +210,16 @@ BEGIN
         WHERE Exp_ID = 0;
 
         _logErrors := false;
-        _message := format('Experiment run list contains experiments that do not exist: %s', _invalidExperiments);
+
+        If _invalidExperiments Like '%,%' Then
+            _message := format('These experiments do not exist: %s', _invalidExperiments);
+        Else
+            _message := format('Experiment does not exist: %s', _invalidExperiments);
+        End If;
+
         RAISE WARNING '%', _message;
 
-        _returnCode := 'U5204';
+        _returnCode := 'U5206';
         DROP TABLE Tmp_Experiments;
         RETURN;
     End If;
@@ -198,7 +233,7 @@ BEGIN
     If _userID > 0 Then
         -- Function get_user_id() recognizes both a username and the form 'LastName, FirstName (Username)'
         -- Assure that _researcher contains simply the username
-        --
+
         SELECT username
         INTO _researcher
         FROM t_users
@@ -221,7 +256,7 @@ BEGIN
             _message := format('Could not find entry in database for researcher username "%s"', _researcher);
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5205';
+            _returnCode := 'U5207';
             DROP TABLE Tmp_Experiments;
             RETURN;
         End If;
@@ -234,21 +269,18 @@ BEGIN
 
     If _mode = 'add' Then
 
-        INSERT INTO t_experiment_groups (
-            group_type,
-            created,
-            description,
-            parent_exp_id,
-            researcher,
-            group_name
-        ) VALUES (
-            _groupType,
-            CURRENT_TIMESTAMP,
-            _description,
-            _parentExpID,
-            _researcher,
-            _groupName
-        )
+        INSERT INTO t_experiment_groups( group_type,
+                                         created,
+                                         description,
+                                         parent_exp_id,
+                                         researcher_username,
+                                         group_name )
+        VALUES(_groupType,
+               CURRENT_TIMESTAMP,
+               _description,
+               _parentExpID,
+               _researcher,
+               _groupName)
         RETURNING group_id
         INTO _id;
 
@@ -261,11 +293,11 @@ BEGIN
     If _mode = 'update' Then
 
         UPDATE t_experiment_groups
-        SET group_type = _groupType,
-            description = _description,
-            parent_exp_id = _parentExpID,
-            researcher = _researcher,
-            group_name = _groupName
+        SET group_type             = _groupType,
+            description            = _description,
+            parent_exp_id          = _parentExpID,
+            researcher_username    = _researcher,
+            group_name             = _groupName
         WHERE group_id = _id;
 
     End If;
@@ -274,13 +306,13 @@ BEGIN
     -- Update member experiments
     ---------------------------------------------------
 
-    If _mode = 'add' Or _mode = 'update' Then
+    If _mode IN ('add', 'update') Then
 
         -- Remove any existing group members that are not in the temporary table
         --
         DELETE FROM t_experiment_group_members
-        WHERE (group_id = _id) AND
-              (NOT exp_id IN ( SELECT exp_id FROM Tmp_Experiments ))
+        WHERE group_id = _id AND
+              NOT exp_id IN ( SELECT exp_id FROM Tmp_Experiments );
 
         -- Add group members from temporary table that are not already members
         --
@@ -289,11 +321,11 @@ BEGIN
             exp_id
         )
         SELECT _id,
-               Tmp_Experiments.exp_id
+               exp_id
         FROM Tmp_Experiments
-        WHERE NOT Tmp_Experiments.exp_id IN ( SELECT exp_id
-                                              FROM t_experiment_group_members
-                                              WHERE group_id = _id );
+        WHERE NOT exp_id IN ( SELECT exp_id
+                              FROM t_experiment_group_members
+                              WHERE group_id = _id );
 
         -- Update MemberCount
         --
@@ -302,10 +334,24 @@ BEGIN
                         _message    => _message,        -- Output
                         _returnCode => _returnCode);    -- Output
 
+        -- Change _message to an empty string if no errors occurred
+
+        If _returnCode = '' Then
+            _message := '';
+        End If;
+
     End If;
 
     DROP TABLE Tmp_Experiments;
 END
 $$;
 
-COMMENT ON PROCEDURE public.add_update_experiment_group IS 'AddUpdateExperimentGroup';
+
+ALTER PROCEDURE public.add_update_experiment_group(INOUT _id integer, IN _grouptype text, IN _groupname text, IN _description text, IN _experimentlist text, IN _parentexp text, IN _researcher text, IN _mode text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE add_update_experiment_group(INOUT _id integer, IN _grouptype text, IN _groupname text, IN _description text, IN _experimentlist text, IN _parentexp text, IN _researcher text, IN _mode text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.add_update_experiment_group(INOUT _id integer, IN _grouptype text, IN _groupname text, IN _description text, IN _experimentlist text, IN _parentexp text, IN _researcher text, IN _mode text, INOUT _message text, INOUT _returncode text) IS 'AddUpdateExperimentGroup';
+
