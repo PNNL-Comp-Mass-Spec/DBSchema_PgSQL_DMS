6d8f563c8f5a4ab083e11ebdfd8e93b38cf7b6d4
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-13 21:03:55 -0800

    Add Hanford ID column to view

--- dms/v_charge_code_owner_dms_user_map.sql
+++ dms/v_charge_code_owner_dms_user_map.sql
@@ -6,7 +6,8 @@ CREATE VIEW public.v_charge_code_owner_dms_user_map AS
  SELECT cc.charge_code,
     u.username,
     u.name,
-    u.payroll
+    u.payroll,
+    u.hid
    FROM (public.t_charge_code cc
      JOIN public.t_users u ON (((('H'::text || (cc.resp_hid)::text))::public.citext OPERATOR(public.=) u.hid)))
   WHERE (u.status OPERATOR(public.<>) 'Obsolete'::public.citext);
