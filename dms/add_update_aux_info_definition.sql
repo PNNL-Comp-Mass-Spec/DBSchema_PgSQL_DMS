211c41b5dd3ed4bc4ab998adfb403bd4746cb01d
alchemistmatt <matthew.monroe@pnnl.gov>
2023-12-27 18:44:11 -0800

    Implement procedure for adding / updating aux info definition items

--- dms/add_update_aux_info_definition.sql
+++ dms/add_update_aux_info_definition.sql
@@ -1,32 +1,22 @@
 --
-CREATE OR REPLACE PROCEDURE public.add_update_aux_info_definition
-(
-    _mode citext = 'UpdateItem',
-    _targetName text = 'Biomaterial',
-    _categoryName text = 'Prokaryote',
-    _subCategoryName text = 'Starter Culture Conditions',
-    _itemName text = 'Date Started',
-    _seq int = 1,
-    _param1 text = '',
-    _param2 text = '',
-    _param3 text = '',
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: add_update_aux_info_definition(public.citext, text, text, text, text, integer, text, text, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.add_update_aux_info_definition(IN _mode public.citext DEFAULT 'UpdateItem'::public.citext, IN _targetname text DEFAULT 'Biomaterial'::text, IN _categoryname text DEFAULT 'Prokaryote'::text, IN _subcategoryname text DEFAULT 'Starter Culture Conditions'::text, IN _itemname text DEFAULT 'Date Started'::text, IN _seq integer DEFAULT 1, IN _param1 text DEFAULT ''::text, IN _param2 text DEFAULT ''::text, IN _param3 text DEFAULT ''::text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Adds new or updates definition of auxiliary information
+**      Adds new or updates existing auxiliary information definition
 **
 **  Arguments:
-**    _mode                 Mode: 'AddTarget', 'AddCategory', 'AddSubcategory', 'AddItem', 'AddAllowedValue'
-**    _targetName           Target name: Biomaterial, 'Dataset', 'SamplePrepRequest', or 'Experiment'
+**    _mode                 Mode: 'AddTarget', 'AddCategory', 'AddSubcategory', 'AddItem', 'AddAllowedValue', 'UpdateItem'
+**    _targetName           Target name: 'Biomaterial', 'Dataset', 'SamplePrepRequest', or 'Experiment'
 **    _categoryName         Category,    e.g. 'Growth Conditions' or 'Lysis Method';   see column aux_category    in t_aux_info_category
 **    _subCategoryName      Subcategory, e.g. 'Alkylation' or 'Centrifugation';        see column aux_subcategory in t_aux_info_subcategory
 **    _itemName             Item name,   e.g. 'Alkylation Agent' or 'Centrifuge Time'; see column aux_description in t_aux_info_description
-**    _seq                  Sequence number, corresponding to column sequence in t_aux_info_description
+**    _seq                  Sequence number, corresponding to column sequence in t_aux_info_description; only used when _mode is 'UpdateItem'
 **    _param1               See parameter argument usage below
 **    _param2               See below
 **    _param3               See below
@@ -35,7 +25,7 @@ AS $$
 **
 **  Parameter argument usage:
 **      When _mode is 'AddTarget' parameters correspond to columns target_table, target_id_col, and target_name_col in table t_aux_info_target
-**      When _mode is 'AddItem', parameters _param1 and _param2 correspond to columns data_size and helper_append in table t_aux_info_description
+**      When _mode is 'AddItem' or 'UpdateItem', parameters _param1 and _param2 correspond to columns data_size and helper_append in table t_aux_info_description
 **      When _mode is 'AddAllowedValue', parameter _param1 corresponds to column value in table t_aux_info_allowed_values
 **
 **  Auth:   grk
@@ -46,7 +36,7 @@ AS $$
 **          07/06/2022 mem - Use new aux info definition view name
 **          08/15/2022 mem - Use new column names
 **          11/21/2022 mem - Use new column names in t_aux_info_target
-**          12/15/2024 mem - Ported to PostgreSQL
+**          12/27/2023 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -64,7 +54,9 @@ DECLARE
     _msg text;
     _sequence int;
     _dataSize int;
+    _dataSizeCurrent int;
     _helperAppend text;
+    _helperAppendCurrent text;
 BEGIN
     _message := '';
     _returnCode := '';
@@ -93,9 +85,83 @@ BEGIN
     -- Validate the inputs
     ---------------------------------------------------
 
-    _mode := Trim(Lower(Coalesce(_mode, '')));
+    _mode            := Trim(Lower(Coalesce(_mode, '')));
+    _targetName      := Trim(Coalesce(_targetName, ''));
+    _categoryName    := Trim(Coalesce(_categoryName, ''));
+    _subCategoryName := Trim(Coalesce(_subCategoryName, ''));
+    _itemName        := Trim(Coalesce(_itemName, ''));
+    _seq             := Coalesce(_seq, 1);
+    _param1          := Trim(Coalesce(_param1, ''));
+    _param2          := Trim(Coalesce(_param2, ''));
+    _param3          := Trim(Coalesce(_param3, ''));
 
-    If _mode <> Lower('AddTarget') And _targetName = 'Cell Culture' And Exists (SELECT target_type_id FROM t_aux_info_target WHERE target_type_name = 'Biomaterial') Then
+    If _targetName = '' Then
+        _message := 'Cannot add or update: target name is an empty string';
+        RAISE WARNING '%', _message;
+
+        _returnCode := 'U5201';
+        RETURN;
+    End If;
+
+    If _mode::citext IN ('AddCategory', 'AddSubcategory', 'AddItem', 'AddAllowedValue') Then
+
+        If _mode::citext IN ('AddCategory', 'AddSubcategory', 'AddItem', 'AddAllowedValue') And _categoryName = '' Then
+            _message := 'Cannot add: category is an empty string';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5202';
+            RETURN;
+        End If;
+
+        If _mode::citext IN ('AddSubcategory', 'AddItem', 'AddAllowedValue') And _subcategoryName = '' Then
+            _message := 'Cannot add: subcategory is an empty string';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5203';
+            RETURN;
+        End If;
+
+        If _mode::citext IN ('AddItem', 'AddAllowedValue') And _itemName = '' Then
+            _message := 'Cannot add: item is an empty string';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5204';
+            RETURN;
+        End If;
+
+    End If;
+
+    If _mode::citext IN ('AddItem', 'UpdateItem') Then
+        _dataSize := public.try_cast(_param1, null::int);
+
+        If _mode::citext IN ('AddItem') And _dataSize Is Null Then
+            _message := 'When adding or updating an item, argument _param1 must be an integer';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5205';
+            RETURN;
+        End If;
+
+        If _mode::citext IN ('AddItem') And _dataSize <= 0 Then
+            _message := 'When adding or updating an item, argument _param1 must be a positive integer';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5206';
+            RETURN;
+        End If;
+
+        If (_mode::citext IN ('AddItem') Or _mode::citext IN ('UpdateItem') And _param2 <> '') And Not _param2::citext IN ('N', 'Y') Then
+            _message := 'When adding or updating an item, argument _param2 must be Y or N';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5207';
+            RETURN;
+        End If;
+
+        _helperAppend := Upper(_param2);
+    End If;
+
+    If _mode <> Lower('AddTarget') And _targetName::citext = 'Cell Culture' And Exists (SELECT target_type_id FROM t_aux_info_target WHERE target_type_name = 'Biomaterial') Then
         _targetName := 'Biomaterial';
     End If;
 
@@ -104,26 +170,48 @@ BEGIN
     ---------------------------------------------------
 
     If _mode = Lower('AddTarget') Then
-        -- future: verify correctness of
-        -- Target_Table, Target_ID_Col, Target_Name_Col
+
+        If _param1 = '' Then
+            _message := 'Cannot add: target table is an empty string (argument _param1)';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5208';
+            RETURN;
+        End If;
+
+        If _param2 = '' Then
+            _message := 'Cannot add: target ID column is an empty string (argument _param2)';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5209';
+            RETURN;
+        End If;
+
+        If _param3 = '' Then
+            _message := 'Cannot add: target name column is an empty string (argument _param3)';
+            RAISE WARNING '%', _message;
+
+            _returnCode := 'U5210';
+            RETURN;
+        End If;
 
         -- Is target already in table?
-        --
+
         SELECT target_type_id
         INTO _tmpID
         FROM t_aux_info_target
-        WHERE target_type_name = _targetName;
+        WHERE target_type_name = _targetName::citext;
 
         If FOUND Then
-            _message := 'Cannot add: target already exists';
+            _message := format('Cannot add: target "%s" already exists', _targetName);
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5201';
+            _returnCode := 'U5211';
             RETURN;
         End If;
 
         -- Insert new target into table
-        --
+
         INSERT INTO t_aux_info_target (
             target_type_name,
             target_table,
@@ -144,13 +232,13 @@ BEGIN
         SELECT target_type_id
         INTO _targetTypeID
         FROM t_aux_info_target
-        WHERE target_type_name = _targetName;
+        WHERE target_type_name = _targetName::citext;
 
         If _targetTypeID = 0 Then
             _message := 'Could not resolve parent target type';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5202';
+            _returnCode := 'U5212';
             RETURN;
         End If;
 
@@ -160,13 +248,13 @@ BEGIN
         INTO _tmpID
         FROM t_aux_info_category
         WHERE target_type_id = _targetTypeID AND
-              aux_category = _categoryName;
+              aux_category = _categoryName::citext;
 
         If FOUND Then
             _message := 'Cannot add: category already exists for this target';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5203';
+            _returnCode := 'U5213';
             RETURN;
         End If;
 
@@ -191,21 +279,21 @@ BEGIN
     ---------------------------------------------------
 
     If _mode = Lower('AddSubcategory') Then
-        -- Resolve parent category names to ID
+        -- Resolve parent category name to ID
 
         SELECT t_aux_info_category.aux_category_id
         INTO _categoryID
         FROM t_aux_info_target
              INNER JOIN t_aux_info_category
-               ON t_aux_info_target.aux_category_id = t_aux_info_category.target_type_id
-        WHERE t_aux_info_target.target_type_name = _targetName AND
-              t_aux_info_category.aux_category = _categoryName;
+               ON t_aux_info_target.target_type_id = t_aux_info_category.target_type_id
+        WHERE t_aux_info_target.target_type_name = _targetName::citext AND
+              t_aux_info_category.aux_category = _categoryName::citext;
 
         If _categoryID = 0 Then
             _message := 'Could not resolve parent category name for given target type';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5204';
+            _returnCode := 'U5214';
             RETURN;
         End If;
 
@@ -215,13 +303,13 @@ BEGIN
         INTO _tmpID
         FROM t_aux_info_subcategory
         WHERE aux_category_id = _categoryID AND
-              aux_subcategory = _subcategoryName;
+              aux_subcategory = _subcategoryName::citext;
 
         If FOUND Then
             _message := 'Cannot add: subcategory already exists for this target';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5204';
+            _returnCode := 'U5215';
             RETURN;
         End If;
 
@@ -246,24 +334,24 @@ BEGIN
     ---------------------------------------------------
 
     If _mode = Lower('AddItem') Then
-        -- Resolve parent subcategory names to ID
+        -- Resolve parent names to ID
 
         SELECT t_aux_info_subcategory.aux_subcategory_id
         INTO _subcategoryID
         FROM t_aux_info_target
              INNER JOIN t_aux_info_category
-               ON t_aux_info_target.aux_subcategory_id = t_aux_info_category.target_type_id
+               ON t_aux_info_target.target_type_id = t_aux_info_category.target_type_id
              INNER JOIN t_aux_info_subcategory
-               ON t_aux_info_category.aux_subcategory_id = t_aux_info_subcategory.aux_category_id
-        WHERE t_aux_info_target.target_type_name = _targetName AND
-              t_aux_info_category.aux_category = _categoryName AND
-              t_aux_info_subcategory.aux_subcategory = _subcategoryName;
+               ON t_aux_info_category.aux_category_id = t_aux_info_subcategory.aux_category_id
+        WHERE t_aux_info_target.target_type_name = _targetName::citext AND
+              t_aux_info_category.aux_category = _categoryName::citext AND
+              t_aux_info_subcategory.aux_subcategory = _subcategoryName::citext;
 
         If Not FOUND Then
             _message := 'Could not resolve parent subcategory for given category and target type';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5205';
+            _returnCode := 'U5216';
             RETURN;
         End If;
 
@@ -273,13 +361,13 @@ BEGIN
         INTO _tmpID
         FROM t_aux_info_description
         WHERE aux_subcategory_id = _subcategoryID AND
-              aux_description = _itemName;
+              aux_description = _itemName::citext;
 
         If FOUND Then
             _message := 'Cannot add: item already exists for this target';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5206';
+            _returnCode := 'U5217';
             RETURN;
         End If;
 
@@ -295,13 +383,13 @@ BEGIN
         -- Insert new item for parent subcategory
 
         INSERT INTO t_aux_info_description (
-            Name,
-            Aux_Subcategory_ID,
+            aux_description,
+            aux_subcategory_id,
             sequence,
             data_size,
             helper_append
         )
-        VALUES (_itemName, _subcategoryID, _tmpSeq, _param1, _param2);
+        VALUES (_itemName, _subcategoryID, _tmpSeq, _dataSize, _helperAppend);
 
     End If;
 
@@ -310,7 +398,7 @@ BEGIN
     ---------------------------------------------------
 
     If _mode = Lower('AddAllowedValue') Then
-        -- Resolve parent description names to ID
+        -- Resolve parent names to ID
 
         SELECT t_aux_info_description.aux_description_id
         INTO _descriptionID
@@ -321,16 +409,16 @@ BEGIN
                ON t_aux_info_category.aux_category_id = t_aux_info_subcategory.aux_category_id
              INNER JOIN t_aux_info_description
                ON t_aux_info_subcategory.aux_subcategory_id = t_aux_info_description.aux_subcategory_id
-        WHERE t_aux_info_target.Target_Type_Name = _targetName AND
-              t_aux_info_category.Aux_Category = _categoryName AND
-              t_aux_info_subcategory.Aux_Subcategory = _subcategoryName AND
-              t_aux_info_description.aux_description = _itemName;
+        WHERE t_aux_info_target.Target_Type_Name = _targetName::citext AND
+              t_aux_info_category.Aux_Category = _categoryName::citext AND
+              t_aux_info_subcategory.Aux_Subcategory = _subcategoryName::citext AND
+              t_aux_info_description.aux_description = _itemName::citext;
 
         If Not FOUND Then
-            _message := 'Could not resolve parent description ID for given subcategory, category, and target type';
+            _message := 'Could not resolve parent description ID for given target type, category, subcategory, and item';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5207';
+            _returnCode := 'U5218';
             RETURN;
         End If;
 
@@ -340,20 +428,20 @@ BEGIN
         INTO _tmpID
         FROM t_aux_info_allowed_values
         WHERE aux_description_id = _descriptionID AND
-              value = _param1;
+              value = _param1::citext;
 
         If FOUND Then
             _message := 'Cannot add: allowed value already exists for this target';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5208';
+            _returnCode := 'U5219';
             RETURN;
         End If;
 
         -- Insert new allowed value for parent description ID
 
         INSERT INTO t_aux_info_allowed_values (Aux_Description_ID, value)
-        VALUES (_descriptionID, _param1)
+        VALUES (_descriptionID, _param1);
 
     End If;
 
@@ -363,29 +451,29 @@ BEGIN
 
     If _mode = Lower('UpdateItem') Then
         -- Find item ID
-        --
+
         SELECT Item_ID
         INTO _tmpID
         FROM V_Aux_Info_Definition
-        WHERE Target = _targetName AND
-              Category = _categoryName AND
-              Subcategory = _subcategoryName AND
-              Item = _itemName;
+        WHERE Target = _targetName::citext AND
+              Category = _categoryName::citext AND
+              Subcategory = _subcategoryName::citext AND
+              Item = _itemName::citext;
 
         If Not FOUND Then
-            _message := 'Cannot resolve item name to ID';
+            _message := 'Cannot resolve item name, category, and subcategory to ID';
             RAISE WARNING '%', _message;
 
-            _returnCode := 'U5209';
+            _returnCode := 'U5220';
             RETURN;
         End If;
 
-        -- Get current values of stuff so that blank input values can default
+        -- Get current values so that blank input values can default
 
         SELECT sequence,
                data_size,
                helper_append
-        INTO _sequence, _dataSize, _helperAppend
+        INTO _sequence, _dataSizeCurrent, _helperAppendCurrent
         FROM t_aux_info_description
         WHERE aux_description_id = _tmpID;
 
@@ -393,21 +481,20 @@ BEGIN
             _sequence := _seq;
         End If;
 
-        If _param1 <> '' Then
-            _dataSize := _param1;
+        If Coalesce(_dataSize, 0) > 0 Then
+            _dataSizeCurrent := _dataSize;
         End If;
 
-        If _param2 <> '' Then
-            _helperAppend := _param2;
+        If Coalesce(_helperAppend, '') <> '' Then
+            _helperAppendCurrent := _helperAppend;
         End If;
 
         -- Update item
 
         UPDATE t_aux_info_description
-        SET
-            sequence = _sequence,
-            data_size = _dataSize,
-            helper_append = _helperAppend
+        SET sequence = _sequence,
+            data_size = _dataSizeCurrent,
+            helper_append = _helperAppendCurrent
         WHERE aux_description_id = _tmpID;
 
     End If;
@@ -415,4 +502,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE public.add_update_aux_info_definition IS 'AddUpdateAuxInfoDefinition';
+
+ALTER PROCEDURE public.add_update_aux_info_definition(IN _mode public.citext, IN _targetname text, IN _categoryname text, IN _subcategoryname text, IN _itemname text, IN _seq integer, IN _param1 text, IN _param2 text, IN _param3 text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE add_update_aux_info_definition(IN _mode public.citext, IN _targetname text, IN _categoryname text, IN _subcategoryname text, IN _itemname text, IN _seq integer, IN _param1 text, IN _param2 text, IN _param3 text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.add_update_aux_info_definition(IN _mode public.citext, IN _targetname text, IN _categoryname text, IN _subcategoryname text, IN _itemname text, IN _seq integer, IN _param1 text, IN _param2 text, IN _param3 text, INOUT _message text, INOUT _returncode text) IS 'AddUpdateAuxInfoDefinition';
+
