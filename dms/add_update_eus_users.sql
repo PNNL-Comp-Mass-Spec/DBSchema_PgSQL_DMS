e3741ac5a94ac79b72317d42289f3e93ad86d94e
alchemistmatt <matthew.monroe@pnnl.gov>
2024-01-01 20:24:18 -0800

    Implement procedure for adding/updating an EUS user

--- dms/add_update_eus_users.sql
+++ dms/add_update_eus_users.sql
@@ -1,25 +1,19 @@
 --
-CREATE OR REPLACE PROCEDURE public.add_update_eus_users
-(
-    _eusPersonID text,
-    _eusNameFm text,
-    _eusSiteStatus text,
-    _hanfordID text,
-    _mode text = 'add',
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: add_update_eus_users(text, text, text, text, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.add_update_eus_users(IN _euspersonid text, IN _eusnamefm text, IN _eussitestatus text, IN _hanfordid text, IN _mode text DEFAULT 'add'::text, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
 **      Add new or update an existing EUS user
 **
 **  Arguments:
-**    _eusPersonID      EUS Proposal ID
-**    _eusNameFm        EUS Proposal State
-**    _eusSiteStatus    EUS Proposal Title
+**    _eusPersonID      EUS person ID (integer, as text)
+**    _eusNameFm        Person's name, in the form "LastName, FirstName"
+**    _eusSiteStatus    EUS site status ID: 0 for 'Undefined', 1 for 'ONSITE', 2 for 'REMOTE_OTHER' (see table t_eus_site_status)
 **    _hanfordID        Hanford ID
 **    _mode             Mode: add' or 'update'
 **    _message          Status message
@@ -30,7 +24,7 @@ AS $$
 **          03/19/2012 mem - Added _hanfordID
 **          06/16/2017 mem - Restrict access using VerifySPAuthorized
 **          08/01/2017 mem - Use THROW if not authorized
-**          12/15/2024 mem - Ported to PostgreSQL
+**          01/01/2024 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -39,6 +33,8 @@ DECLARE
     _nameWithSchema text;
     _authorized boolean;
 
+    _eusPersonIdValue int;
+    _eusSiteStatusValue smallint;
     _existingCount int := 0;
     _msg text;
     _tempEUSPersonID int;
@@ -70,27 +66,47 @@ BEGIN
     -- Validate the inputs
     ---------------------------------------------------
 
-    If char_length(_eusPersonID) < 1 Then
+    _eusPersonID   := Trim(Coalesce(_eusPersonID, ''));
+    _eusNameFm     := Trim(Coalesce(_eusNameFm, ''));
+    _eusSiteStatus := Trim(Coalesce(_eusSiteStatus, ''));
+    _hanfordID     := Trim(Coalesce(_hanfordID, ''));
+    _mode          := Trim(Lower(Coalesce(_mode, '')));
+
+    If _eusPersonID = '' Then
         _returnCode := 'U5201';
         _message := 'EUS Person ID must be specified';
         RAISE EXCEPTION '%', _message;
     End If;
 
-    If char_length(_eusNameFm) < 1 Then
+    _eusPersonIdValue := public.try_cast(_eusPersonID, null::int);
+
+    If _eusPersonIdValue Is Null Then
         _returnCode := 'U5202';
-        RAISE EXCEPTION 'EUS Person''s Name must be specified';
+        _message := 'EUS person ID must be an integer';
+        RAISE EXCEPTION '%', _message;
     End If;
 
-    If char_length(_eusSiteStatus) < 1 Then
+    If _eusNameFm = '' Then
         _returnCode := 'U5203';
-        RAISE EXCEPTION 'EUS Site Status must be specified';
+        RAISE EXCEPTION 'EUS person''s name must be specified';
     End If;
 
-    If char_length(Coalesce(_hanfordID, '')) = 0 Then
+    If _eusSiteStatus = '' Then
+        _returnCode := 'U5204';
+        RAISE EXCEPTION 'EUS site status must be specified';
+    End If;
+
+    _eusSiteStatusValue := public.try_cast(_eusSiteStatus, null::smallint);
+
+    If _eusSiteStatusValue Is Null Then
+        _returnCode := 'U5202';
+        _message := 'EUS site status must be an integer';
+        RAISE EXCEPTION '%', _message;
+    End If;
+
+    If _hanfordID = '' Then
         _hanfordID := Null;
     End If;
-
-    _mode := Trim(Lower(Coalesce(_mode, '')));
 
     ---------------------------------------------------
     -- Is entry already in database?
@@ -99,33 +115,33 @@ BEGIN
     SELECT person_id
     INTO _tempEUSPersonID
     FROM t_eus_users
-    WHERE person_id = _eusPersonID;
+    WHERE person_id = _eusPersonIdValue;
     --
     GET DIAGNOSTICS _existingCount = ROW_COUNT;
 
     -- Cannot create an entry that already exists
-    --
+
     If _mode = 'add' And _existingCount > 0 Then
-        _msg := format('Cannot add: EUS Person ID "%s" is already in the database ', _eusPersonID);
-        RAISE EXCEPTION '%', _msg;
-
-        _message := 'message';
-        RAISE WARNING '%', _message;
-
-        _returnCode := 'U5204';
-        RETURN;
-    End If;
-
-    -- Cannot update a non-existent entry
-    --
-    If _mode = 'update' And _existingCount = 0 Then
-        _msg := format('Cannot update: EUS Person ID "%s" is not in the database ', _eusPersonID);
+        _msg := format('Cannot add: EUS Person ID "%s" already exists', _eusPersonID);
         RAISE EXCEPTION '%', _msg;
 
         _message := 'message';
         RAISE WARNING '%', _message;
 
         _returnCode := 'U5205';
+        RETURN;
+    End If;
+
+    -- Cannot update a non-existent entry
+
+    If _mode = 'update' And _existingCount = 0 Then
+        _msg := format('Cannot update: EUS Person ID "%s" does not exist', _eusPersonID);
+        RAISE EXCEPTION '%', _msg;
+
+        _message := 'message';
+        RAISE WARNING '%', _message;
+
+        _returnCode := 'U5206';
         RETURN;
     End If;
 
@@ -142,14 +158,14 @@ BEGIN
             hid,
             last_affected
         ) VALUES (
-            _eusPersonID,
+            _eusPersonIdValue,
             _eusNameFm,
-            _eusSiteStatus,
+            _eusSiteStatusValue,
             _hanfordID,
             CURRENT_TIMESTAMP
-        )
+        );
 
-    End If; -- add mode
+    End If;
 
     ---------------------------------------------------
     -- Action for update mode
@@ -157,17 +173,24 @@ BEGIN
 
     If _mode = 'update' Then
 
-        UPDATE  t_eus_users
-        SET
-            name_fm = _eusNameFm,
-            site_status_id =  _eusSiteStatus,
-            hid = _hanfordID,
-            last_affected = CURRENT_TIMESTAMP
-        WHERE (person_id = _eusPersonID)
+        UPDATE t_eus_users
+        SET name_fm        = _eusNameFm,
+            site_status_id =  _eusSiteStatusValue,
+            hid            = _hanfordID,
+            last_affected  = CURRENT_TIMESTAMP
+        WHERE person_id = _eusPersonIdValue;
 
-    End If; -- update mode
+    End If;
 
 END
 $$;
 
-COMMENT ON PROCEDURE public.add_update_eus_users IS 'AddUpdateEUSUsers';
+
+ALTER PROCEDURE public.add_update_eus_users(IN _euspersonid text, IN _eusnamefm text, IN _eussitestatus text, IN _hanfordid text, IN _mode text, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE add_update_eus_users(IN _euspersonid text, IN _eusnamefm text, IN _eussitestatus text, IN _hanfordid text, IN _mode text, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.add_update_eus_users(IN _euspersonid text, IN _eusnamefm text, IN _eussitestatus text, IN _hanfordid text, IN _mode text, INOUT _message text, INOUT _returncode text) IS 'AddUpdateEUSUsers';
+
