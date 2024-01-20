be638a9c9040fb7984d76c5bdd9655fbd88d233c
alchemistmatt <matthew.monroe@pnnl.gov>
2024-01-19 20:38:27 -0800

    Obtain instrument group names from t_requested_run

--- dms/get_batch_group_instrument_group_list.sql
+++ dms/get_batch_group_instrument_group_list.sql
@@ -9,21 +9,24 @@ CREATE OR REPLACE FUNCTION public.get_batch_group_instrument_group_list(_batchgr
 **
 **  Desc:
 **      Build a delimited list of the instrument groups associated with a requested run batch group
-**      These are based on instrument group names in t_requested_run_batches
+**      These are based on instrument group names in t_requested_run
 **
 **  Return value: Comma-separated list
 **
 **  Auth:   mem
 **  Date:   02/09/2023 mem - Initial version
+**          01/19/2024 mem - Obtain instrument group names from t_requested_run
 **
 *****************************************************/
 DECLARE
     _result text := '';
 BEGIN
-    SELECT string_agg(LookupQ.requested_instrument_group, ', ' ORDER BY LookupQ.requested_instrument_group)
+    SELECT string_agg(LookupQ.instrument_group, ', ' ORDER BY LookupQ.instrument_group)
     INTO _result
-    FROM ( SELECT DISTINCT RRB.requested_instrument_group
+    FROM ( SELECT DISTINCT RR.instrument_group
            FROM t_requested_run_batches RRB
+                LEFT OUTER JOIN t_requested_run RR
+                  ON RRB.batch_id = RR.batch_id
            WHERE RRB.batch_group_id = _batchGroupID) As LookupQ;
 
     RETURN Coalesce(_result, '');
