add572fda92179d3a9568cda383b90232806ffbe
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-13 19:05:51 -0800

    Use "Similar To" instead of "ILike" for items with underscores or square brackets

--- dms/create_like_clause_from_separated_string.sql
+++ dms/create_like_clause_from_separated_string.sql
@@ -2,73 +2,69 @@
 -- Name: create_like_clause_from_separated_string(text, text, text); Type: FUNCTION; Schema: public; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION public.create_like_clause_from_separated_string(_instring text, _fieldname text, _separator text) RETURNS text
+CREATE OR REPLACE FUNCTION public.create_like_clause_from_separated_string(_filterlist text, _fieldname text, _separator text DEFAULT ','::text) RETURNS text
     LANGUAGE plpgsql
     AS $$
 /****************************************************
 **
-**  Desc:   Parses the text in _inString, looking for _separator
-**          and generating a valid SQL Like clause for field _fieldName
+**  Desc:
+**      Parses the text in _filterList, splitting on _separator,
+**      and generating a valid SQL ILike or Similar To clause for field _fieldName
+**
+**  Arguments:
+**    _filterList   Item name(s) to find in the given field (supports wildcards); separate multiple names using the given separator
+**    _fieldName    Field name (aka column name)
+**    _separator    Separator, typically a comma
 **
 **  Auth:   jds
 **  Date:   12/16/2004
-**          07/26/2005 mem - Now trimming white space from beginning and end of text extracted from _inString
+**          07/26/2005 mem - Now trimming white space from beginning and end of text extracted from _filterList
 **                         - Increased size of return variable from 2048 to 4096 characters
 **          06/17/2022 mem - Ported to PostgreSQL
 **          05/22/2023 mem - Capitalize reserved words
 **          05/30/2023 mem - Use format() for string concatenation
 **          09/11/2023 mem - Adjust capitalization of keywords
+**          02/13/2024 mem - Use "Similar To" instead of "ILike" for items with underscores (which are treated as wildcards) or square brackets
+**                         - Use public.parse_delimited_list() to split
 **
 *****************************************************/
 DECLARE
-    _sepPosition int;
+    _currentValue text;
     _result text;
-    _i int;
+    _useSimilarTo boolean;
 BEGIN
-    _inString := Replace(_inString, '_', '[_]');
-    _result := ' ';
-    _i := 1;
+    _result := '';
 
-    WHILE char_length(_inString) > 0
+    FOR _currentValue IN
+        SELECT DISTINCT Replace(Value, '_', '[_]')
+        FROM public.parse_delimited_list(_filterList, _separator)
     LOOP
-        _sepPosition := Position(_separator In _inString);
+        _useSimilarTo := Position('_' In _currentValue) > 0 OR
+                         Position('[' In _currentValue) > 0 OR
+                         Position(']' In _currentValue) > 0;
 
-        If _sepPosition > 0 Then
-            If _i = 1 Then
-                _result := format('((%s ILike ''%s'')',
-                                    _fieldName, Trim(Substring(_inString, 1, _sepPosition - 1)) );
-            Else
-                _result := format('%s OR (%s ILike ''%s'')',
-                                    _result, _fieldName, Trim(Substring(_inString, 1, _sepPosition - 1)) );
-            End If;
-
-            _i := _i + 1;
-            _inString := Substring(_inString, _sepPosition + 1, char_length(_inString) - 1);
-
-            CONTINUE;
-        Else
-            If _i = 1 Then
-                _result := format('((%s ILike ''%s''))', _fieldName, Trim(_inString) );
-            Else
-                _result := format('%s OR ((%s ILike ''%s''))', _result, _fieldName, Trim(_inString) );
-            End If;
-
-            -- Break out of the while loop
-            EXIT;
+        If _result <> '' Then
+            _result := format('%s OR ', _result);
         End If;
+
+        If _useSimilarTo Then
+            _result := format('%s((%s Similar To ''%s''))', _result, _fieldName, _currentValue );
+        Else
+            _result := format('%s((%s ILike ''%s''))',       _result, _fieldName, _currentValue );
+        End If;
+
     END LOOP;
 
-    -- Only remove spaces from the end of _result, since it should start wqith a space
-    RETURN RTrim(_result);
+    RETURN _result;
 END
 $$;
 
 
-ALTER FUNCTION public.create_like_clause_from_separated_string(_instring text, _fieldname text, _separator text) OWNER TO d3l243;
+ALTER FUNCTION public.create_like_clause_from_separated_string(_filterlist text, _fieldname text, _separator text) OWNER TO d3l243;
 
 --
--- Name: FUNCTION create_like_clause_from_separated_string(_instring text, _fieldname text, _separator text); Type: COMMENT; Schema: public; Owner: d3l243
+-- Name: FUNCTION create_like_clause_from_separated_string(_filterlist text, _fieldname text, _separator text); Type: COMMENT; Schema: public; Owner: d3l243
 --
 
-COMMENT ON FUNCTION public.create_like_clause_from_separated_string(_instring text, _fieldname text, _separator text) IS 'CreateLikeClauseFromSeparatedString';
+COMMENT ON FUNCTION public.create_like_clause_from_separated_string(_filterlist text, _fieldname text, _separator text) IS 'CreateLikeClauseFromSeparatedString';
 
