84032fca9a5f2ca3e35e23d72e90dfc048716251
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-13 21:13:40 -0800

    Implement legacy procedure for generating LC cart loading lists

--- dms/Legacy/generate_lc_cart_loading_list.sql
+++ dms/Legacy/generate_lc_cart_loading_list.sql
@@ -1,42 +1,33 @@
 --
-CREATE OR REPLACE FUNCTION public.generate_lc_cart_loading_list
-(
-    _lcCartName text,
-    _blanksFollowingRequests text,
-    _columnsWithLeadingBlanks text
-)
-RETURNS TABLE
-(
-	"sequence" int4,
-	"name" citext,
-	request int4,
-	column_number int4,
-	experiment citext,
-	priority int2,
-	"type" citext,
-	batch int4,
-	block int4,
-	run_order int4,
-	emsl_usage_type citext,
-	emsl_proposal_id citext,
-	emsl_users_list citext
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: generate_lc_cart_loading_list(text, text, text); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE FUNCTION public.generate_lc_cart_loading_list(_lccartname text, _blanksfollowingrequests text DEFAULT ''::text, _columnswithleadingblanks text DEFAULT ''::text) RETURNS TABLE(sequence integer, name public.citext, request integer, column_number integer, experiment public.citext, priority smallint, type public.citext, batch integer, block integer, run_order integer, emsl_usage_type public.citext, emsl_proposal_id public.citext, emsl_users_list public.citext)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
-**      Generate a sample loading list for given LC cart
+**      Generate a sample loading list for the given LC cart
 **
 **      This function is likely unused in 2022, since cart_column (cart column ID) in t_requested_run has only had null values since September 2020,
-**      and the function raises a warning and exits if any of the matching requested runs has a null cart column ID value
+**      and this function raises a warning and exits if any of the matching requested runs has a null cart column ID value
 **
-**      SELECT Extract(year from Entered) AS RR_Year,
-**             Sum(CASE WHEN NOT Cart_Column IS NULL THEN 1 ELSE 0 END) AS Requests_with_Non_Null_Cart_Col,
-**             Sum(CASE WHEN     Cart_Column > 1     THEN 1 ELSE 0 END) AS Requests_with_Cart_Col_Over_One
-**      FROM T_Requested_Run
-**      GROUP BY RR_Year
-**      ORDER BY RR_Year DESC;
+**      Stats by year:
+**        SELECT Extract(year from Entered) AS RR_Year,
+**               Sum(CASE WHEN NOT Cart_Column IS NULL THEN 1 ELSE 0 END) AS Requests_with_Non_Null_Cart_Col,
+**               Sum(CASE WHEN     Cart_Column > 1     THEN 1 ELSE 0 END) AS Requests_with_Cart_Col_Over_One
+**        FROM T_Requested_Run
+**        GROUP BY RR_Year
+**        ORDER BY RR_Year DESC;
+**
+**      Additionally, all active requested runs in 2024 have cart 'Unknown' or 'No_Cart':
+**        SELECT C.cart_name, COUNT(*) as Run_Requests
+**        FROM t_requested_run RR
+**             INNER JOIN t_lc_cart C
+**               ON RR.cart_id = C.cart_id
+**        WHERE RR.state_name = 'Active'
+**        GROUP BY C.cart_name;
 **
 **  Arguments:
 **    _lcCartName                   LC cart name
@@ -50,7 +41,7 @@ AS $$
 **          07/31/2007 mem - Now returning Dataset Type for each request (Ticket #505)
 **          08/27/2007 grk - Add ability to start columns with a blank (Ticket #517)
 **          09/17/2009 grk - Added check for requests that don't have column assignments
-**          12/15/2024 mem - Ported to PostgreSQL
+**          02/13/2024 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -65,11 +56,17 @@ DECLARE
     _requestCountTotal int;
     _dsTypeForBlanks text;
 BEGIN
-    _message := '';
-    _returnCode := '';
 
     ---------------------------------------------------
-    -- Create temporary table to hold requested runs assigned to cart
+    -- Validate the inputs
+    ---------------------------------------------------
+
+    _lcCartName               := Trim(Coalesce(_lcCartName, ''));
+    _blanksFollowingRequests  := Trim(Coalesce(_blanksFollowingRequests, ''));
+    _columnsWithLeadingBlanks := Trim(Coalesce(_columnsWithLeadingBlanks, ''));
+
+    ---------------------------------------------------
+    -- Create a temporary table to hold the requested runs assigned to the cart
     ---------------------------------------------------
 
     CREATE TEMP TABLE Tmp_XR (
@@ -79,31 +76,29 @@ BEGIN
     );
 
     ---------------------------------------------------
-    -- Populate temporary table with run requests
-    -- assigned to this cart
+    -- Populate temporary table with active requested runs assigned to this cart
     ---------------------------------------------------
 
-    INSERT INTO Tmp_XR( request_id, cart_column_id )
+    INSERT INTO Tmp_XR ( request_id, cart_column_id )
     SELECT RR.request_id,
            RR.cart_column      -- Cart column ID: null for all requested runs since 2020-09-24
     FROM t_requested_run RR
          INNER JOIN t_lc_cart C
            ON RR.cart_id = C.cart_id
-    WHERE C.cart_name = _lcCartName::citext
+    WHERE C.cart_name = _lcCartName::citext AND
+          RR.state_name = 'Active'
     ORDER BY RR.priority DESC,
              RR.batch_id,
              RR.run_order,
-             RR.request_id
+             RR.request_id;
 
     ---------------------------------------------------
     -- Verify that all requests have column assignments
     ---------------------------------------------------
 
     If Exists (SELECT request_id FROM Tmp_XR WHERE cart_column_id IS NULL) Then
-        _message := 'Some requests do not have column assignments';
-        RAISE WARNING '%', _message;
+        RAISE WARNING 'Some requests do not have column assignments';
 
-        _returnCode := 'U5201';
         DROP TABLE Tmp_XR;
         RETURN;
     End If;
@@ -120,18 +115,12 @@ BEGIN
     -- Add following blanks to table
     ---------------------------------------------------
 
-    -- Use OVERRIDING SYSTEM VALUE to insert an explicit value for the identity column, for example:
-    --
-    -- INSERT INTO mc.t_log_entries (entry_id, posted_by, posting_time, type, message)
-    -- OVERRIDING SYSTEM VALUE
-    -- VALUES (12345, 'Test', CURRENT_TIMESTAMP, 'Test', 'message');
-
     If _blanksFollowingRequests <> '' Then
         INSERT INTO Tmp_XR (request_id, cart_column_id, entry_id)
         OVERRIDING SYSTEM VALUE
         SELECT 0, cart_column_id, entry_id + 1
         FROM Tmp_XR
-        WHERE request_id In (SELECT value FROM public.parse_delimited_integer_list(_blanksFollowingRequests))
+        WHERE request_id IN (SELECT value FROM public.parse_delimited_integer_list(_blanksFollowingRequests));
     End If;
 
     ---------------------------------------------------
@@ -224,7 +213,7 @@ BEGIN
                       Row_Number() OVER (ORDER BY entry_id) * 10 As Seq
                FROM Tmp_XS
                WHERE cart_column_id = _columnNumber) CountQ
-        WHERE Tmp_XS.entry_id = CountQ.entry_id
+        WHERE Tmp_XS.entry_id = CountQ.entry_id;
 
     END LOOP;
 
@@ -257,13 +246,13 @@ BEGIN
     ---------------------------------------------------
     -- Check whether all of the entries in Tmp_XF have the same dataset type.
     -- If they do, that type will be reported for the blanks.
-    -- If not, but if the type is the same in 75% of the the entries, then the most common dataset type will be used.
+    -- If not, but if the type is the same in 75% of the the entries, the most common dataset type will be used.
     -- Otherwise use Null for the dataset type for blanks.
     ---------------------------------------------------
 
-    _matchCount := 0;
+    _matchCount        := 0;
     _requestCountTotal := 0;
-    _dsTypeForBlanks := Null;
+    _dsTypeForBlanks   := null;
 
     SELECT DSType.Dataset_Type,
            COUNT(RR.request_id)
@@ -284,7 +273,7 @@ BEGIN
            ON RR.request_id = Tmp_XF.request_id;
 
     If _matchCount < _requestCountTotal * 0.75 Then
-        _dsTypeForBlanks := Null;
+        _dsTypeForBlanks := null;
     End If;
 
     ---------------------------------------------------
@@ -300,7 +289,7 @@ BEGIN
            FROM Tmp_XF RankSrc
            WHERE RankSrc.request_id = 0 ) CountQ
     WHERE Tmp_XF.request_id = 0 AND
-          Tmp_XF.seq = CountQ.Seq
+          Tmp_XF.seq = CountQ.Seq;
 
     ---------------------------------------------------
     -- Output final report
@@ -314,7 +303,7 @@ BEGIN
         Tmp_XF.cart_column_id AS Column_Number,
         E.experiment AS Experiment,
         RR.priority AS Priority,
-        CASE WHEN Tmp_XF.request_id = 0 THEN _dsTypeForBlanks ELSE DSType.Dataset_Type END AS Type,
+        CASE WHEN Tmp_XF.request_id = 0 THEN _dsTypeForBlanks::citext ELSE DSType.Dataset_Type END AS Type,
         RR.batch_id AS Batch,
         RR.block As Block,
         RR.run_order AS Run_Order,
@@ -330,7 +319,7 @@ BEGIN
            ON RR.request_type_id = DSType.dataset_type_id
          RIGHT OUTER JOIN Tmp_XF
            ON RR.request_id = Tmp_XF.request_id
-    ORDER BY Tmp_XF.seq
+    ORDER BY Tmp_XF.seq;
 
     DROP TABLE Tmp_XR;
     DROP TABLE Tmp_XS;
@@ -338,4 +327,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE public.generate_lc_cart_loading_list IS 'GenerateLCCartLoadingList';
+
+ALTER FUNCTION public.generate_lc_cart_loading_list(_lccartname text, _blanksfollowingrequests text, _columnswithleadingblanks text) OWNER TO d3l243;
+
+--
+-- Name: FUNCTION generate_lc_cart_loading_list(_lccartname text, _blanksfollowingrequests text, _columnswithleadingblanks text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON FUNCTION public.generate_lc_cart_loading_list(_lccartname text, _blanksfollowingrequests text, _columnswithleadingblanks text) IS 'GenerateLCCartLoadingList';
+
