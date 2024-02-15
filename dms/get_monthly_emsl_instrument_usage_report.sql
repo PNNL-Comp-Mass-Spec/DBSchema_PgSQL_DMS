6e872ea413c606c3f044be1d702ebc5cb196e790
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-14 18:32:59 -0800

    Implement procedures for getting instrument usage

--- dms/get_monthly_emsl_instrument_usage_report.sql
+++ dms/get_monthly_emsl_instrument_usage_report.sql
@@ -1,34 +1,18 @@
 --
-CREATE OR REPLACE FUNCTION public.get_monthly_emsl_instrument_usage_report
-(
-    _year text,
-    _month text,
-    _infoOnly boolean = false,
-    _getUsageReportData boolean = true
-)
-RETURNS TABLE
-(
-    EMSL_Inst_ID int,
-    DMS_Instrument citext,
-    Type citext,
-    Start timestamp,
-    Minutes int,
-    Proposal citext,
-    Usage citext,
-    Users citext,
-    Operator citext,
-    Comment citext
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: get_monthly_emsl_instrument_usage_report(text, text, boolean, boolean); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE FUNCTION public.get_monthly_emsl_instrument_usage_report(_year text, _month text, _infoonly boolean DEFAULT false, _getusagereportdata boolean DEFAULT true) RETURNS TABLE(emsl_inst_id integer, dms_instrument public.citext, type public.citext, start timestamp without time zone, minutes integer, proposal public.citext, usage public.citext, users public.citext, operator public.citext, comment public.citext)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
 **      Create a monthly usage report for multiple instruments for given year and month
 **
 **  Arguments:
-**    _year                 Usage report year
-**    _month                Usage report month
+**    _year                 Year (as text, for compatibility with the website)
+**    _month                Month (as text)
 **    _infoOnly             When true, show debug information
 **    _getUsageReportData   When _infoOnly is true, if this is false, do not call procedure get_monthly_instrument_usage_report
 **
@@ -37,14 +21,14 @@ AS $$
 **          02/23/2016 mem - Add set XACT_ABORT on
 **          04/12/2017 mem - Log exceptions to T_Log_Entries
 **          05/03/2019 mem - Add support for DMS instruments that share a single eusInstrumentId
-**          02/14/2022 mem - Add new columns to temporary table Tmp_InstrumentUsage (to match data returned by GetMonthlyInstrumentUsageReport)
+**          02/14/2022 mem - Add new columns to temporary table Tmp_InstrumentUsageData (to match data returned by GetMonthlyInstrumentUsageReport)
 **                         - Add _infoOnly parameter
-**          12/15/2024 mem - Ported to PostgreSQL
+**          02/14/2024 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
     _instrument text;
-    _eusInstrumentId As int := -1;
+    _eusInstrumentId int;
     _message text := '';
     _returnCode text := '';
 
@@ -60,13 +44,16 @@ BEGIN
     -- Validate the inputs
     ---------------------------------------------------
 
-    _infoOnly := Coalesce(_infoOnly, false);
+    _year               := Trim(Coalesce(_year, ''));
+    _month              := Trim(Coalesce(_month, ''));
+    _infoOnly           := Coalesce(_infoOnly, false);
+    _getUsageReportData := Coalesce(_getUsageReportData, true);
 
     ---------------------------------------------------
     -- Temp table to hold results
     ---------------------------------------------------
 
-    CREATE TEMP TABLE Tmp_InstrumentUsage (
+    CREATE TEMP TABLE Tmp_InstrumentUsageData (
         EMSL_Inst_ID int,
         Instrument citext,
         Type citext,
@@ -103,8 +90,8 @@ BEGIN
     ---------------------------------------------------
     -- Accumulate data for all instruments, one at a time
     ---------------------------------------------------
-    BEGIN
 
+    BEGIN
         ---------------------------------------------------
         -- Find production instruments that we need to process by EUS_Instrument_ID
         -- because two (or more) DMS Instruments have the same EUS_Instrument_ID
@@ -121,9 +108,10 @@ BEGIN
                                INNER JOIN t_emsl_dms_instrument_mapping InstMapping
                                  ON InstName.instrument_id = InstMapping.dms_instrument_id
                           GROUP BY eus_instrument_id
-                          HAVING COUNT(InstMapping.dms_instrument_id) > 1 ) LookupQ
+                          HAVING COUNT(InstMapping.dms_instrument_id) > 1
+                        ) LookupQ
                ON InstMapping.eus_instrument_id = LookupQ.eus_instrument_id
-        WHERE InstName.status = 'active' AND
+        WHERE InstName.status = 'Active' AND
               InstName.operations_role = 'Production';
 
         ---------------------------------------------------
@@ -133,27 +121,25 @@ BEGIN
         INSERT INTO Tmp_Instruments( instrument )
         SELECT instrument
         FROM t_instrument_name
-        WHERE status = 'active' And
+        WHERE status = 'Active' And
               operations_role = 'Production' And
               NOT instrument IN ( SELECT instrument
                                   FROM Tmp_InstrumentsToProcessByID );
 
         If _infoOnly Then
-
             RAISE INFO '';
 
             SELECT string_agg(instrument, ', ' ORDER BY instrument)
             INTO _previewData
             FROM Tmp_Instruments;
 
-            RAISE INFO 'Active production instruments: %', _previewData;
+            RAISE INFO 'Active production instruments: %', Coalesce(_previewData, '');
 
             SELECT string_agg(format('EUS ID %s: %s', EUS_Instrument_ID, Instrument), ', ' ORDER BY EUS_Instrument_ID)
             INTO _previewData
             FROM Tmp_InstrumentsToProcessByID;
 
-            RAISE INFO 'Instruments to process by EUS ID: %', _previewData;
-
+            RAISE INFO 'Instruments to process by EUS ID: %', Coalesce(_previewData, '');
         End If;
 
         ---------------------------------------------------
@@ -167,25 +153,25 @@ BEGIN
         LOOP
 
             If _infoOnly Then
-                RAISE INFO 'SELECT * FROM get_monthly_instrument_usage_report (%, 0, %, %, ''report'')', _instrument, _year, _month;
+                RAISE INFO 'SELECT * FROM get_monthly_instrument_usage_report (%, 0, %, %, ''report'');', _instrument, _year, _month;
             End If;
 
             If Not _infoOnly Or _infoOnly And _getUsageReportData Then
-                INSERT INTO Tmp_InstrumentUsage (Instrument, EMSL_Inst_ID, Start, Type, Minutes, Usage, Proposal, Users, Operator, Comment, Year, Month, ID)
-                SELECT Instrument,
-                       EMSL_Inst_ID,
-                       Start,
-                       Type,
-                       Minutes,
-                       Usage,
-                       Proposal,
-                       Users,
-                       Operator,
-                       Comment,
-                       Year,
+                INSERT INTO Tmp_InstrumentUsageData (Instrument, EMSL_Inst_ID, Start, Type, Minutes, Usage, Proposal, Users, Operator, Comment, Year, Month, ID)
+                SELECT U.Instrument,
+                       U.EMSL_Inst_ID,
+                       U.Start,
+                       U.Type,
+                       U.Minutes,
+                       U.Usage,
+                       U.Proposal,
+                       U.Users,
+                       U.Operator,
+                       U.Comment,
+                       U.Year,
                        Month,
                        Dataset_ID
-                FROM public.get_monthly_instrument_usage_report(_instrument, 0, _year, _month, 'report');
+                FROM public.get_monthly_instrument_usage_report(_instrument, 0, _year, _month, 'report') U;
             End If;
 
         END LOOP;
@@ -204,22 +190,22 @@ BEGIN
                 RAISE INFO 'SELECT * FROM public.get_monthly_instrument_usage_report('''', %, %, %, ''report'')', _eusInstrumentId, _year, _month;
             End If;
 
-            If Not _skipProcedureCall Then
-                INSERT INTO Tmp_InstrumentUsage (Instrument, EMSL_Inst_ID, Start, Type, Minutes, Usage, Proposal, Users, Operator, Comment, Year, Month, ID)
-                SELECT Instrument,
-                       EMSL_Inst_ID,
-                       Start,
-                       Type,
-                       Minutes,
-                       Usage,
-                       Proposal,
-                       Users,
-                       Operator,
-                       Comment,
-                       Year,
-                       Month,
-                       Dataset_ID
-                FROM public.get_monthly_instrument_usage_report('', _eusInstrumentId, _year, _month, 'report');
+            If Not _infoOnly Or _infoOnly And _getUsageReportData Then
+                INSERT INTO Tmp_InstrumentUsageData (Instrument, EMSL_Inst_ID, Start, Type, Minutes, Usage, Proposal, Users, Operator, Comment, Year, Month, ID)
+                SELECT U.Instrument,
+                       U.EMSL_Inst_ID,
+                       U.Start,
+                       U.Type,
+                       U.Minutes,
+                       U.Usage,
+                       U.Proposal,
+                       U.Users,
+                       U.Operator,
+                       U.Comment,
+                       U.Year,
+                       U.Month,
+                       U.Dataset_ID
+                FROM public.get_monthly_instrument_usage_report('', _eusInstrumentId, _year, _month, 'report') U;
             End If;
 
         END LOOP;
@@ -229,18 +215,17 @@ BEGIN
         ---------------------------------------------------
 
         RETURN QUERY
-        SELECT
-            EMSL_Inst_ID,
-            Instrument AS DMS_Instrument,
-            Type,
-            Start,
-            Minutes,
-            Proposal,
-            Usage,
-            Users,
-            Operator,
-            Comment
-        FROM Tmp_InstrumentUsage;
+        SELECT U.EMSL_Inst_ID,
+               U.Instrument AS DMS_Instrument,
+               U.Type,
+               U.Start,
+               U.Minutes,
+               U.Proposal,
+               U.Usage,
+               U.Users,
+               U.Operator,
+               U.Comment
+        FROM Tmp_InstrumentUsageData U;
 
     EXCEPTION
         WHEN OTHERS THEN
@@ -263,10 +248,18 @@ BEGIN
         RAISE INFO '%', _message;
     End If;
 
-    DROP TABLE IF EXISTS Tmp_InstrumentUsage;
+    DROP TABLE IF EXISTS Tmp_InstrumentUsageData;
     DROP TABLE IF EXISTS Tmp_Instruments;
     DROP TABLE IF EXISTS Tmp_InstrumentsToProcessByID;
 END
 $$;
 
-COMMENT ON PROCEDURE public.get_monthly_emsl_instrument_usage_report IS 'GetMonthlyEMSLInstrumentUsageReport';
+
+ALTER FUNCTION public.get_monthly_emsl_instrument_usage_report(_year text, _month text, _infoonly boolean, _getusagereportdata boolean) OWNER TO d3l243;
+
+--
+-- Name: FUNCTION get_monthly_emsl_instrument_usage_report(_year text, _month text, _infoonly boolean, _getusagereportdata boolean); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON FUNCTION public.get_monthly_emsl_instrument_usage_report(_year text, _month text, _infoonly boolean, _getusagereportdata boolean) IS 'GetMonthlyEMSLInstrumentUsageReport';
+
