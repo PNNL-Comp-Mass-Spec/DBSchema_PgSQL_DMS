202afdb0605a897bb145ec844e3531de2cae0e69
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-15 19:55:02 -0800

    Implement procedure for moving material containers to a new freezer

--- dms/move_material_containers.sql
+++ dms/move_material_containers.sql
@@ -1,19 +1,10 @@
 --
-CREATE OR REPLACE PROCEDURE public.move_material_containers
-(
-    _freezerTagOld text,
-    _shelfOld int,
-    _rackOld int,
-    _freezerTagNew text,
-    _shelfNew int,
-    _rackNew int,
-    _infoOnly boolean = true,
-    INOUT _message text default '',
-    INOUT _returnCode text default '',
-    _callingUser text = ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: move_material_containers(text, integer, integer, text, integer, integer, text, boolean, text, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.move_material_containers(IN _freezertagold text, IN _shelfold integer, IN _rackold integer, IN _freezertagnew text, IN _shelfnew integer, IN _racknew integer, IN _materiallogcomment text DEFAULT 'Rearrange after replacing freezer 1206A'::text, IN _infoonly boolean DEFAULT true, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text, IN _callinguser text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
@@ -37,7 +28,7 @@ AS $$
 **  Date:   08/03/2016 mem - Initial version
 **          08/27/2018 mem - Rename the view Material Location list report view
 **          06/21/2022 mem - Use new column name Container_Limit in view V_Material_Location_List_Report
-**          12/15/2024 mem - Ported to PostgreSQL
+**          02/15/2024 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -70,37 +61,50 @@ BEGIN
     _freezerTagNew := Trim(Coalesce(_freezerTagNew, ''));
     _shelfNew      := Coalesce(_shelfNew, -1);
     _rackNew       := Coalesce(_rackNew, -1);
+
+    _materialLogComment := Trim(Coalesce(_materialLogComment, ''));
+
     _infoOnly      := Coalesce(_infoOnly, true);
     _callingUser   := Trim(Coalesce(_callingUser, ''));
 
     If _freezerTagOld = '' Then
-        _message := 'Old freezer tag cannot be empty';
+        _message := 'Old freezer tag must be specified';
+        RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _freezerTagNew = '' Then
-        _message := 'New freezer tag cannot be empty';
+        _message := 'New freezer tag must be specified';
+        RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _shelfOld <= 0 Then
         _message := 'Old shelf must be a positive integer';
+        RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _rackOld <= 0 Then
         _message := 'Old rack must be a positive integer';
+        RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _shelfNew <= 0 Then
         _message := 'New shelf must be a positive integer';
+        RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _rackNew <= 0 Then
         _message := 'New rack must be a positive integer';
+        RAISE WARNING '%', _message;
         RETURN;
+    End If;
+
+    If _materialLogComment = '' Then
+        _materialLogComment := 'Moved container using move_material_containers';
     End If;
 
     If _callingUser = '' Then
@@ -112,7 +116,7 @@ BEGIN
     ---------------------------------------------------
 
     CREATE TEMP TABLE Tmp_ContainersToProcess (
-        Entry_ID     int PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1)
+        Entry_ID     int PRIMARY KEY GENERATED ALWAYS AS IDENTITY(START WITH 1 INCREMENT BY 1),
         Container_ID int NOT NULL,
         Container    text NOT NULL,
         Type         text NOT NULL,
@@ -139,7 +143,7 @@ BEGIN
     -- Populate the table
     ---------------------------------------------------
 
-    INSERT INTO Tmp_ContainersToProcess (Container_ID, container, type, location_id, Location_Tag, Shelf, Rack, Row, Col )
+    INSERT INTO Tmp_ContainersToProcess (Container_ID, Container, Type, Location_ID, Location_Tag, Shelf, Rack, Row, Col )
     SELECT MC.container_id,
            MC.container,
            MC.type,
@@ -153,13 +157,19 @@ BEGIN
          INNER JOIN t_material_locations ML
            ON MC.location_id = ML.location_id
     WHERE ML.freezer_tag = _freezerTagOld AND
-          ML.shelf = CAST(_shelfOld AS text) AND
-          ML.rack = CAST(_rackOld AS text)
-    ORDER BY ML.location;
+          ML.shelf = _shelfOld::text AND
+          ML.rack = _rackOld::text
+    ORDER BY ML.location, MC.container_ID;
 
-    If Not Exists (SELECT * FROM Tmp_ContainersToProcess) Then
+    If Not Exists (SELECT Entry_ID FROM Tmp_ContainersToProcess) Then
         _message := format('No containers found in freezer %s, shelf %s, rack %s',
-                            _freezerTagOld, _shelfOld, _rackOld);
+                           _freezerTagOld, _shelfOld, _rackOld);
+
+        RAISE INFO '';
+        RAISE INFO '%', _message;
+
+        DROP TABLE Tmp_ContainersToProcess;
+        DROP TABLE Tmp_Move_Status;
         RETURN;
     End If;
 
@@ -198,8 +208,9 @@ BEGIN
                Container,
                Type,
                Location_ID,
-               Location_Tag,
+               Location_Tag
         FROM Tmp_ContainersToProcess
+        ORDER BY Entry_ID
     LOOP
         _infoData := format(_formatSpecifier,
                             _previewData.Entry_ID,
@@ -217,8 +228,6 @@ BEGIN
     -- Step through the containers and update their location
     ---------------------------------------------------
 
-    _continue := true;
-
     FOR _containerInfo In
         SELECT Container_ID As ContainerID,
                Container As ContainerName,
@@ -230,29 +239,27 @@ BEGIN
                Col
         FROM Tmp_ContainersToProcess
         ORDER BY Entry_ID
-
     LOOP
         _locationTagNew := format('%s.%s.%s.%s.%s', _freezerTagNew, _shelfNew, _rackNew, _containerInfo.Row, _containerInfo.Col);
-        _numContainers := 1;
+        _numContainers  := 1;
 
-        SELECT ml.location_id,
-               ml.status,
-               ml.container_limit,
-               COUNT(mc.location_id) AS containers
+        SELECT ML.location_id,
+               ML.status,
+               ML.container_limit,
+               COUNT(MC.location_id) AS containers
         INTO _locationIDNew, _locStatus, _containterCountLimit, _containterCount
-        FROM public.t_material_locations ml
-             INNER JOIN public.t_material_freezers f
-               ON ml.freezer_tag = f.freezer_tag
-             LEFT OUTER JOIN public.t_material_containers mc
-               ON ml.location_id = mc.location_id
-        WHERE ml.location = _locationTagNew
-        GROUP BY ml.location_id, ml.status, ml.container_limit;
+        FROM public.t_material_locations ML
+             INNER JOIN public.t_material_freezers F
+               ON ML.freezer_tag = F.freezer_tag
+             LEFT OUTER JOIN public.t_material_containers MC
+               ON ML.location_id = MC.location_id
+        WHERE ML.location = _locationTagNew
+        GROUP BY ML.location_id, ML.status, ML.container_limit;
 
         If Not FOUND Then
-            _message := format('Destination location "%s" does not exist', _locationTagNew);
+            _message := format('Destination location "%s" does not exist in t_material_locations', _locationTagNew);
+            RAISE WARNING '%', _message;
             ROLLBACK;
-
-            DROP TABLE Tmp_ContainersToProcess;
             RETURN;
         End If;
 
@@ -261,18 +268,16 @@ BEGIN
         ---------------------------------------------------
 
         If _locStatus <> 'Active' Then
-            _message := format('Location "%s" is not in the "Active" state', _locationTagNew);
+            _message := format('Location "%s" is not in the "Active" state; see t_material_locations', _locationTagNew);
+            RAISE WARNING '%', _message;
             ROLLBACK;
-
-            DROP TABLE Tmp_ContainersToProcess;
             RETURN;
         End If;
 
         If _containterCount + _numContainers > _containterCountLimit Then
-            _message := format('The maximum container capacity (%s) of location "%s" would be exceeded by the move', _containterCountLimit, _locationTagNew);
+            _message := format('The maximum container capacity (%s) of location "%s" would be exceeded by the move; see t_material_locations', _containterCountLimit, _locationTagNew);
+            RAISE WARNING '%', _message;
             ROLLBACK;
-
-            DROP TABLE Tmp_ContainersToProcess;
             RETURN;
         End If;
 
@@ -303,23 +308,24 @@ BEGIN
                 _containerInfo.LocationTagOld,
                 _locationTagNew,
                 'pnl\d3l243',
-                'Rearrange after replacing freezer 1206A'
-            )
+                _materialLogComment
+            );
 
         End If;
 
         INSERT INTO Tmp_Move_Status (Container_ID, Container, Type, Location_Old, Location_Current, Location_New, LocationIDNew, Status)
-        SELECT mc.container_id AS Container_ID,
-               mc.container,
-               mc.type,
+        SELECT MC.container_id AS Container_ID,
+               MC.container,
+               MC.type,
                _containerInfo.LocationTagOld AS Location_Old,
-               ml.location AS Location_Current,
+               ML.location AS Location_Current,
                _locationTagNew AS Location_New,
                _locationIDNew AS LocationIDNew,
                _moveStatus
-        FROM public.t_material_containers mc
-             INNER JOIN public.t_material_locations ml ON mc.location_id = ml.location_id
-        WHERE mc.container_id = _containerInfo.ContainerID;
+        FROM public.t_material_containers MC
+             INNER JOIN public.t_material_locations ML
+               ON MC.location_id = ML.location_id
+        WHERE MC.container_id = _containerInfo.ContainerID;
 
     END LOOP;
 
@@ -329,61 +335,61 @@ BEGIN
 
     RAISE INFO '';
 
-    _formatSpecifier := '%-12s %-20s %-10s %-25s %-25s %-25s %-15s %-10s';
+    If Not Exists (SELECT Container_ID FROM Tmp_Move_Status) Then
+        RAISE INFO 'No containers were moved';
+    Else
+        _formatSpecifier := '%-12s %-20s %-10s %-25s %-25s %-25s %-15s %-10s';
 
-    _infoHead := format(_formatSpecifier,
-                        'Container_ID',
-                        'Container',
-                        'Type',
-                        'Location_Old',
-                        'Location_Current',
-                        'Location_New',
-                        'Location_ID_New',
-                        'Status'
-                       );
-
-    _infoHeadSeparator := format(_formatSpecifier,
-                                 '------------',
-                                 '--------------------',
-                                 '----------',
-                                 '-------------------------',
-                                 '-------------------------',
-                                 '-------------------------',
-                                 '---------------',
-                                 '----------'
-                                );
-
-    RAISE INFO '%', _infoHead;
-    RAISE INFO '%', _infoHeadSeparator;
-
-    FOR _previewData IN
-        SELECT Container_ID,
-               Container,
-               Type,
-               Location_Old,
-               Location_Current,
-               Location_New,
-               LocationIDNew,
-               Status
-        FROM Tmp_Move_Status
-        ORDER BY Container_ID
-    LOOP
-        _infoData := format(_formatSpecifier,
-                            _previewData.Container_ID,
-                            _previewData.Container,
-                            _previewData.Type,
-                            _previewData.Location_Old,
-                            _previewData.Location_Current,
-                            _previewData.Location_New,
-                            _previewData.LocationIDNew,
-                            _previewData.Status
+        _infoHead := format(_formatSpecifier,
+                            'Container_ID',
+                            'Container',
+                            'Type',
+                            'Location_Old',
+                            'Location_Current',
+                            'Location_New',
+                            'Location_ID_New',
+                            'Status'
                            );
 
-        RAISE INFO '%', _infoData;
-    END LOOP;
+        _infoHeadSeparator := format(_formatSpecifier,
+                                     '------------',
+                                     '--------------------',
+                                     '----------',
+                                     '-------------------------',
+                                     '-------------------------',
+                                     '-------------------------',
+                                     '---------------',
+                                     '----------'
+                                    );
 
-    If _message <> '' Then
-        RAISE INFO '%', _message;
+        RAISE INFO '%', _infoHead;
+        RAISE INFO '%', _infoHeadSeparator;
+
+        FOR _previewData IN
+            SELECT Container_ID,
+                   Container,
+                   Type,
+                   Location_Old,
+                   Location_Current,
+                   Location_New,
+                   LocationIDNew,
+                   Status
+            FROM Tmp_Move_Status
+            ORDER BY Container_ID
+        LOOP
+            _infoData := format(_formatSpecifier,
+                                _previewData.Container_ID,
+                                _previewData.Container,
+                                _previewData.Type,
+                                _previewData.Location_Old,
+                                _previewData.Location_Current,
+                                _previewData.Location_New,
+                                _previewData.LocationIDNew,
+                                _previewData.Status
+                               );
+
+            RAISE INFO '%', _infoData;
+        END LOOP;
     End If;
 
     DROP TABLE Tmp_ContainersToProcess;
@@ -391,4 +397,12 @@ BEGIN
 END
 $$;
 
-COMMENT ON PROCEDURE public.move_material_containers IS 'MoveMaterialContainers';
+
+ALTER PROCEDURE public.move_material_containers(IN _freezertagold text, IN _shelfold integer, IN _rackold integer, IN _freezertagnew text, IN _shelfnew integer, IN _racknew integer, IN _materiallogcomment text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _callinguser text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE move_material_containers(IN _freezertagold text, IN _shelfold integer, IN _rackold integer, IN _freezertagnew text, IN _shelfnew integer, IN _racknew integer, IN _materiallogcomment text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _callinguser text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.move_material_containers(IN _freezertagold text, IN _shelfold integer, IN _rackold integer, IN _freezertagnew text, IN _shelfnew integer, IN _racknew integer, IN _materiallogcomment text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text, IN _callinguser text) IS 'MoveMaterialContainers';
+
