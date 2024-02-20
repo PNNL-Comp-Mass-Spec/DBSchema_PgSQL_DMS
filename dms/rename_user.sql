ece9ed7eb5b761abf97b3eb1ca73f763bcb88c66
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-19 19:38:17 -0800

    Implement procedure for renaming a DMS user

--- dms/rename_user.sql
+++ dms/rename_user.sql
@@ -1,31 +1,27 @@
 --
-CREATE OR REPLACE PROCEDURE public.rename_user
-(
-    _oldUserName text = '',
-    _newUserName text = '',
-    _infoOnly boolean = true,
-    INOUT _message text default '',
-    INOUT _returnCode text default ''
-)
-LANGUAGE plpgsql
-AS $$
+-- Name: rename_user(text, text, boolean, text, text); Type: PROCEDURE; Schema: public; Owner: d3l243
+--
+
+CREATE OR REPLACE PROCEDURE public.rename_user(IN _oldusername text DEFAULT ''::text, IN _newusername text DEFAULT ''::text, IN _infoonly boolean DEFAULT true, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
+    LANGUAGE plpgsql
+    AS $$
 /****************************************************
 **
 **  Desc:
 **      Rename a user in t_users and other tracking tables
 **
 **  Arguments:
-**    _oldUserName  Username to change, e.g. 'D3L243'
-**    _newUserName  New username
-*     _infoOnly     When true, preview updates
-**    _message      Status message
-**    _returnCode   Return code
+**    _oldUserName      Username to change, e.g. 'D3L243'
+**    _newUserName      New username
+*     _infoOnly         When true, preview updates
+**    _message          Status message
+**    _returnCode       Return code
 **
 **  Auth:   10/31/2014 mem - Initial version
 **  Date:   06/16/2017 mem - Restrict access using VerifySPAuthorized
 **          08/01/2017 mem - Use THROW if not authorized
 **          08/06/2018 mem - Rename Operator PRN column to RDS_Requestor_PRN
-**          12/15/2024 mem - Ported to PostgreSQL
+**          02/19/2024 mem - Ported to PostgreSQL
 **
 *****************************************************/
 DECLARE
@@ -69,15 +65,18 @@ BEGIN
 
     _oldUserName := Trim(Coalesce(_oldUserName, ''));
     _newUserName := Trim(Coalesce(_newUserName, ''));
+    _infoOnly    := Coalesce(_infoOnly, true);
+
+    RAISE INFO '';
 
     If _oldUserName = '' Then
-        _message := '_oldUserName is empty; unable to continue';
+        _message := 'Old username must be specified; unable to continue';
         RAISE WARNING '%', _message;
         RETURN;
     End If;
 
     If _newUserName = '' Then
-        _message := '_newUserName is empty; unable to continue';
+        _message := 'New username must be specified; unable to continue';
         RAISE WARNING '%', _message;
         RETURN;
     End If;
@@ -110,77 +109,75 @@ BEGIN
             RAISE WARNING '%', _message;
             RETURN;
         End If;
-    Else
 
-        If _infoOnly Then
+    ElsIf _infoOnly Then
 
-            RAISE INFO '';
-            RAISE INFO 'Preview user rename from % to %', _oldUserName, _newUserName;
+        RAISE INFO 'Preview user rename from % to %', _oldUserName, _newUserName;
+        RAISE INFO '';
 
-            _formatSpecifier := '%-8s %-10s %-35s %-11s %-10s %-40s %-20s';
+        _formatSpecifier := '%-8s %-10s %-35s %-11s %-10s %-40s %-20s';
 
-            _infoHead := format(_formatSpecifier,
-                                'User_ID',
-                                'Username',
-                                'Name',
-                                'HID',
-                                'Status',
-                                'Email',
-                                'Created'
+        _infoHead := format(_formatSpecifier,
+                            'User_ID',
+                            'Username',
+                            'Name',
+                            'HID',
+                            'Status',
+                            'Email',
+                            'Created'
+                           );
+
+        _infoHeadSeparator := format(_formatSpecifier,
+                                     '--------',
+                                     '----------',
+                                     '-----------------------------------',
+                                     '-----------',
+                                     '----------',
+                                     '----------------------------------------',
+                                     '--------------------'
+                                    );
+
+        RAISE INFO '%', _infoHead;
+        RAISE INFO '%', _infoHeadSeparator;
+
+        FOR _previewData IN
+            SELECT User_ID,
+                   Username,
+                   Name,
+                   HID,
+                   Status,
+                   Email,
+                   public.timestamp_text(created) AS created
+            FROM t_users
+            WHERE username IN (_oldUserName::citext, _newUserName::citext)
+            ORDER BY username
+        LOOP
+            _infoData := format(_formatSpecifier,
+                                _previewData.User_ID,
+                                _previewData.Username,
+                                _previewData.Name,
+                                _previewData.HID,
+                                _previewData.Status,
+                                _previewData.Email,
+                                _previewData.Created
                                );
 
-            _infoHeadSeparator := format(_formatSpecifier,
-                                         '--------',
-                                         '----------',
-                                         '-----------------------------------',
-                                         '-----------',
-                                         '----------',
-                                         '----------------------------------------',
-                                         '--------------------'
-                                        );
+            RAISE INFO '%', _infoData;
+        END LOOP;
 
-            RAISE INFO '%', _infoHead;
-            RAISE INFO '%', _infoHeadSeparator;
+    Else
+        RAISE INFO 'Renaming user % to %', _oldUserName, _newUserName;
 
-            FOR _previewData IN
-                SELECT User_ID,
-                       Username,
-                       Name,
-                       HID,
-                       Status,
-                       Email,
-                       public.timestamp_text(created) AS created
-                FROM t_users
-                WHERE username IN (_oldUserName::citext, _newUserName::citext)
-                ORDER BY username
-            LOOP
-                _infoData := format(_formatSpecifier,
-                                    _previewData.User_ID,
-                                    _previewData.Username,
-                                    _previewData.Name,
-                                    _previewData.HID,
-                                    _previewData.Status,
-                                    _previewData.Email,
-                                    _previewData.Created
-                                   );
-
-                RAISE INFO '%', _infoData;
-            END LOOP;
-
-        Else
-            RAISE INFO 'Renaming % to %', _oldUserName, _newUserName;
-
-            UPDATE t_users
-            SET username = _newUserName
-            WHERE username = _oldUserName::citext;
-        End If;
-
+        UPDATE t_users
+        SET username = _newUserName
+        WHERE username = _oldUserName::citext;
     End If;
 
     If _infoOnly Then
 
         --------------------------------------------
         -- Show the items owned by _oldUserName or _newUserName
+        -- Only shows the newest 25 items of each entity type
         --------------------------------------------
 
         RAISE INFO '';
@@ -214,7 +211,7 @@ BEGIN
                        'Operator' AS Role,
                        operator_username AS Username,
                        1 AS Sort,
-                       Row_Number() OVER (ORDER BY Dataset_ID DESC)
+                       Row_Number() OVER (ORDER BY Dataset_ID DESC) AS ItemRank
                 FROM t_dataset
                 WHERE operator_username IN (_oldUserName::citext, _newUserName::citext)
                 UNION
@@ -224,7 +221,7 @@ BEGIN
                        'Researcher' AS Role,
                        researcher_username AS Username,
                        2 AS Sort,
-                       Row_Number() OVER (ORDER BY Exp_ID DESC)
+                       Row_Number() OVER (ORDER BY Exp_ID DESC) AS ItemRank
                 FROM t_experiments
                 WHERE researcher_username IN (_oldUserName::citext, _newUserName::citext)
                 UNION
@@ -234,7 +231,7 @@ BEGIN
                        'Requester' AS Role,
                        requester_username AS Username,
                        3 AS Sort,
-                       Row_Number() OVER (ORDER BY Request_ID DESC)
+                       Row_Number() OVER (ORDER BY Request_ID DESC) AS ItemRank
                 FROM t_requested_run
                 WHERE requester_username IN (_oldUserName::citext, _newUserName::citext)
                 UNION
@@ -244,7 +241,7 @@ BEGIN
                        'Owner' AS Role,
                        owner_username AS Username,
                        4 AS Sort,
-                       Row_Number() OVER (ORDER BY data_pkg_id DESC)
+                       Row_Number() OVER (ORDER BY data_pkg_id DESC) AS ItemRank
                 FROM dpkg.t_data_package
                 WHERE owner_username IN (_oldUserName::citext, _newUserName::citext)
                 UNION
@@ -254,7 +251,7 @@ BEGIN
                        'Requester' AS Role,
                        Requester AS Username,
                        5 AS Sort,
-                       Row_Number() OVER (ORDER BY data_pkg_id DESC)
+                       Row_Number() OVER (ORDER BY data_pkg_id DESC) AS ItemRank
                 FROM dpkg.t_data_package
                 WHERE requester IN (_oldUserName::citext, _newUserName::citext)
             )
@@ -284,7 +281,6 @@ BEGIN
         END LOOP;
 
     Else
-
         UPDATE t_dataset
         SET operator_username = _newUserName
         WHERE operator_username = _oldUserName::citext;
@@ -298,20 +294,28 @@ BEGIN
         WHERE requester_username = _oldUserName::citext;
 
         UPDATE dpkg.t_data_package
-        SET Owner = _newUserName
-        WHERE Owner = _oldUserName::citext;
+        SET owner_username = _newUserName
+        WHERE owner_username = _oldUserName::citext;
 
         UPDATE dpkg.t_data_package
         SET Requester = _newUserName
         WHERE Requester = _oldUserName::citext;
 
+        RAISE INFO 'User % has been renamed to %', _oldUserName, _newUserName;
     End If;
 
     If _message <> '' Then
         RAISE INFO '%', _message;
     End If;
-
 END
 $$;
 
-COMMENT ON PROCEDURE public.rename_user IS 'RenameUser';
+
+ALTER PROCEDURE public.rename_user(IN _oldusername text, IN _newusername text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
+
+--
+-- Name: PROCEDURE rename_user(IN _oldusername text, IN _newusername text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text); Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON PROCEDURE public.rename_user(IN _oldusername text, IN _newusername text, IN _infoonly boolean, INOUT _message text, INOUT _returncode text) IS 'RenameUser';
+
