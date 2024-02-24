bfc74f75e744882b0c2c9e38835e0530a99f3588
alchemistmatt <matthew.monroe@pnnl.gov>
2024-02-23 17:17:23 -0800

    Rename arguments on test procedures and add example usage

--- dms/test/test.test_get_data.sql
+++ dms/test/test.test_get_data.sql
@@ -1,25 +1,49 @@
 --
--- Name: test_get_data(integer, text, refcursor, refcursor, text); Type: PROCEDURE; Schema: test; Owner: d3l243
+-- Name: test_get_data(integer, refcursor, refcursor, text, text); Type: PROCEDURE; Schema: test; Owner: d3l243
 --
 
-CREATE OR REPLACE PROCEDURE test.test_get_data(IN _itemid integer, INOUT _message text DEFAULT ''::text, INOUT _result_one refcursor DEFAULT 'rs_resultone'::refcursor, INOUT _result_two refcursor DEFAULT 'rs_resulttwo'::refcursor, INOUT _returncode text DEFAULT ''::text)
+CREATE OR REPLACE PROCEDURE test.test_get_data(IN _itemid integer, INOUT _resultsa refcursor DEFAULT '_resultsa'::refcursor, INOUT _resultsb refcursor DEFAULT '_resultsb'::refcursor, INOUT _message text DEFAULT ''::text, INOUT _returncode text DEFAULT ''::text)
     LANGUAGE plpgsql
     AS $$
+/****************************************************
+**
+**  Desc:
+**      Test procedure demonstrating how to return multiple result sets using cursors
+**
+**  Arguments:
+**    _itemID       Item ID to mention in _message
+**    _resultsA     Output: cursor for retrieving result set 1
+**    _resultsB     Output: cursor for retrieving result set 2
+**    _message      Status message
+**    _returnCode   Return code
+**
+**  Use this to view the data returned by the _results cursors
+**
+**      BEGIN;
+**          CALL test.test_get_data (5);
+**          FETCH ALL FROM _resultsa;
+**          FETCH ALL FROM _resultsb;
+**      END;
+**
+**  Alternatively, use an anonymous code block (though it cannot return query results; it can only store them in a table or display them with RAISE INFO)
+**
+**/
 BEGIN
     _message := 'Test message for item ' || COALESCE(_itemID, 0);
     _returnCode := '';
 
-  open _result_one for
+  Open _resultsA For
     SELECT *
-    FROM (values (1,2,3, 'fruit', current_timestamp - INTERVAL '5 seconds'), (4,5,6, 'veggie', current_timestamp)) as t(a,b,c,d,e);
+    FROM (VALUES (1,2,3, 'fruit', current_timestamp - INTERVAL '5 seconds'),
+                 (4,5,6, 'veggie', current_timestamp)) AS t(a,b,c,d,e);
 
-  open _result_two for
+  Open _resultsB For
     SELECT *
-    FROM (values ('one'), ('two'), ('three'), ('four')) as p(name);
+    FROM (VALUES ('one'), ('two'), ('three'), ('four')) AS p(name);
 
 END;
 $$;
 
 
-ALTER PROCEDURE test.test_get_data(IN _itemid integer, INOUT _message text, INOUT _result_one refcursor, INOUT _result_two refcursor, INOUT _returncode text) OWNER TO d3l243;
+ALTER PROCEDURE test.test_get_data(IN _itemid integer, INOUT _resultsa refcursor, INOUT _resultsb refcursor, INOUT _message text, INOUT _returncode text) OWNER TO d3l243;
 
