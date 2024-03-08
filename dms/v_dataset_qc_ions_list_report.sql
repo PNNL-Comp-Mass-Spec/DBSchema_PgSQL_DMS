e0283a3894c966183769cbc339a0f78312ed8ba2
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-08 10:39:40 -0800

    Add qc_link column to dataset QC ions view

--- dms/v_dataset_qc_ions_list_report.sql
+++ dms/v_dataset_qc_ions_list_report.sql
@@ -15,14 +15,16 @@ CREATE VIEW public.v_dataset_qc_ions_list_report AS
     ds.comment,
     dsn.dataset_state AS state,
     ds.acq_length_minutes AS acq_length,
-    dtn.dataset_type
-   FROM ((((((public.t_dataset_qc_ions dqi
+    dtn.dataset_type,
+    dl.qc_link
+   FROM (((((((public.t_dataset_qc_ions dqi
      JOIN public.t_dataset ds ON ((dqi.dataset_id = ds.dataset_id)))
      JOIN public.t_experiments e ON ((ds.exp_id = e.exp_id)))
      JOIN public.t_campaign c ON ((e.campaign_id = c.campaign_id)))
      JOIN public.t_instrument_name instname ON ((ds.instrument_id = instname.instrument_id)))
      JOIN public.t_dataset_state_name dsn ON ((ds.dataset_state_id = dsn.dataset_state_id)))
-     JOIN public.t_dataset_type_name dtn ON ((ds.dataset_type_id = dtn.dataset_type_id)));
+     JOIN public.t_dataset_type_name dtn ON ((ds.dataset_type_id = dtn.dataset_type_id)))
+     LEFT JOIN public.t_cached_dataset_links dl ON ((ds.dataset_id = dl.dataset_id)));
 
 
 ALTER VIEW public.v_dataset_qc_ions_list_report OWNER TO d3l243;
