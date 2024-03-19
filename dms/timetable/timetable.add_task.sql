278a545c714c05aba4ba84289a26773ece68340d
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-18 20:10:08 -0700

    Use parameter names instead of $1, $2, etc.

--- dms/timetable/timetable.add_task.sql
+++ dms/timetable/timetable.add_task.sql
@@ -15,7 +15,7 @@ CREATE OR REPLACE FUNCTION timetable.add_task(kind timetable.command_kind, comma
 **
 **  Arguments:
 **    kind          Command kind: 'SQL', 'PROGRAM', or 'BUILTIN'
-**    command       SQL query, procedure call, or Built-in command name
+**    command       SQL query, procedure call, or built-in command name
 **    parent_id     Parent task ID (not the chain ID)
 **    order_delta   Value to add to task_order for the given parent task when determining the task_order for the new task
 **    _infoOnly     When true, show the task that would be created
@@ -31,37 +31,45 @@ CREATE OR REPLACE FUNCTION timetable.add_task(kind timetable.command_kind, comma
 **
 **  Auth:   mem
 **  Date:   03/16/2024 mem - Initial release
+**          03/18/2024 mem - Use parameter names instead of $1, $2, etc.
 **
 *****************************************************/
 DECLARE
     _chainID int;
     _taskOrder int;
     _taskID int;
+    _taskKind timetable.command_kind;
+    _taskCommand text;
 BEGIN
     SELECT chain_id, task_order
     INTO _chainID, _taskOrder
     FROM timetable.task
-    WHERE task_id = $3;
+    WHERE task_id = parent_id;
 
     RAISE INFO '';
 
     If Not FOUND Then
-        RAISE WARNING 'Task_ID % not found in table timetable.task ', $3;
+        RAISE WARNING 'Task_ID % not found in table timetable.task', parent_id;
         RETURN null;
     End If;
 
     RAISE INFO '% task of type % to chain %, with order %, following task_id %',
                CASE WHEN _infoOnly THEN 'Would add' ELSE 'Adding' END,
-               $1, _chainID, _taskOrder + $4, $3;
+               kind, _chainID, _taskOrder + order_delta, parent_id;
 
-    RAISE INFO 'Command: %', $2;
+    RAISE INFO 'Command: %', command;
 
     If _infoOnly Then
         RETURN null;
     End If;
 
+    _taskKind    := kind;
+    _taskCommand := command;
+
     INSERT INTO timetable.task (chain_id, task_order, kind, command)
-    SELECT chain_id, task_order + $4, $1, $2 FROM timetable.task WHERE task_id = $3
+    SELECT _chainID, T.task_order + order_delta, _taskKind, _taskCommand
+    FROM timetable.task T
+    WHERE T.task_id = parent_id
     RETURNING task_id
     INTO _taskID;
 
