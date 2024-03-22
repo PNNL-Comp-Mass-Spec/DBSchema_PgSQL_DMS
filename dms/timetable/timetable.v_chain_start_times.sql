f9658edb6e4be08038a56db16b5ddc81dc5106f5
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-21 21:56:21 -0700

    Switch from Row_Number() to Lead()

--- dms/timetable/timetable.v_chain_start_times.sql
+++ dms/timetable/timetable.v_chain_start_times.sql
@@ -3,24 +3,20 @@
 --
 
 CREATE VIEW timetable.v_chain_start_times AS
- WITH rankq AS (
-         SELECT filterq.start_time,
+ SELECT leadq.chain,
+    leadq.start_time,
+    leadq.next_start_time,
+    ((leadq.next_start_time - leadq.start_time))::interval minute AS start_delta,
+    c.run_at,
+    i.interval_description
+   FROM ((( SELECT filterq.start_time,
             filterq.chain,
-            row_number() OVER (PARTITION BY filterq.chain ORDER BY filterq.start_time) AS start_order
+            lead(filterq.start_time, 1) OVER (PARTITION BY filterq.chain ORDER BY filterq.start_time) AS next_start_time
            FROM ( SELECT log.ts AS start_time,
                     ((log.message_data -> 'chain'::text))::integer AS chain
                    FROM timetable.log
-                  WHERE (log.message = 'Starting chain'::text)) filterq
-        )
- SELECT a.chain,
-    a.start_time,
-    b.start_time AS next_start_time,
-    ((b.start_time - a.start_time))::interval minute AS start_delta,
-    c.run_at,
-    i.interval_description
-   FROM (((rankq a
-     JOIN rankq b ON (((a.chain = b.chain) AND (b.start_order = (a.start_order + 1)))))
-     JOIN timetable.chain c ON ((a.chain = c.chain_id)))
+                  WHERE (log.message = 'Starting chain'::text)) filterq) leadq
+     JOIN timetable.chain c ON ((leadq.chain = c.chain_id)))
      LEFT JOIN timetable.t_cron_interval i ON (((c.run_at)::text = (i.cron_interval)::text)));
 
 
