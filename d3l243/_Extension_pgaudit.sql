139705e36fdd32b004436bff801b33b8c64bb4a4
alchemistmatt <matthew.monroe@pnnl.gov>
2024-03-30 16:40:55 -0700

    Add extensions

---
+++ d3l243/_Extension_pgaudit.sql
@@ -0,0 +1,12 @@
+--
+-- Name: pgaudit; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pgaudit WITH SCHEMA public;
+
+--
+-- Name: EXTENSION pgaudit; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION pgaudit IS 'provides auditing functionality';
+
