a56f33509a835b5eb97ac691be7f7eb1c1a257cd
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-07 18:10:19 -0700

    Add extensions

---
+++ d3l243/_Extension_fuzzystrmatch.sql
@@ -0,0 +1,12 @@
+--
+-- Name: fuzzystrmatch; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS fuzzystrmatch WITH SCHEMA public;
+
+--
+-- Name: EXTENSION fuzzystrmatch; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION fuzzystrmatch IS 'determine similarities and distance between strings';
+
---
+++ d3l243/_Extension_pg_trgm.sql
@@ -0,0 +1,12 @@
+--
+-- Name: pg_trgm; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
+
+--
+-- Name: EXTENSION pg_trgm; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION pg_trgm IS 'text similarity measurement and index searching based on trigrams';
+
