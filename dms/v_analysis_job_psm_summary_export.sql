b0ef57adb169df7f590d66f677d03940e2173e56
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-16 14:27:48 -0700

    Update PSM summary view to use t_cached_dataset_stats

--- dms/v_analysis_job_psm_summary_export.sql
+++ dms/v_analysis_job_psm_summary_export.sql
@@ -3,23 +3,15 @@
 --
 
 CREATE VIEW public.v_analysis_job_psm_summary_export AS
- SELECT j.dataset_id,
-    count(j.job) AS jobs,
-    COALESCE(max(psm.total_psms_fdr_filter), max(psm.total_psms)) AS max_total_psms,
-    COALESCE(max(psm.unique_peptides_fdr_filter), max(psm.unique_peptides)) AS max_unique_peptides,
-    COALESCE(max(psm.unique_proteins_fdr_filter), max(psm.unique_proteins)) AS max_unique_proteins,
-    max(psm.total_psms) AS max_total_psms_msgf,
-    max(psm.unique_peptides) AS max_unique_peptides_msgf,
-    max(psm.unique_proteins) AS max_unique_proteins_msgf,
-    max(psm.total_psms_fdr_filter) AS max_total_psms_fdr_filter,
-    max(psm.unique_peptides_fdr_filter) AS max_unique_peptides_fdr_filter,
-    max(psm.unique_proteins_fdr_filter) AS max_unique_proteins_fdr_filter
-   FROM (public.t_analysis_job j
-     JOIN public.t_analysis_job_psm_stats psm ON ((j.job = psm.job)))
-  WHERE (j.analysis_tool_id IN ( SELECT t_analysis_tool.analysis_tool_id
-           FROM public.t_analysis_tool
-          WHERE (t_analysis_tool.result_type OPERATOR(public.~~) '%peptide_hit'::public.citext)))
-  GROUP BY j.dataset_id;
+ SELECT dataset_id,
+    psm_job_count AS jobs,
+    max_total_psms,
+    max_unique_peptides,
+    max_unique_proteins,
+    max_total_psms_fdr_filter,
+    max_unique_peptides_fdr_filter,
+    max_unique_proteins_fdr_filter
+   FROM public.t_cached_dataset_stats cds;
 
 
 ALTER VIEW public.v_analysis_job_psm_summary_export OWNER TO d3l243;
