91838da3b9bf977089044d0da86af026808d8d8b
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-22 21:04:54 -0700

    Directly query t_experiments instead of using v_experiment_detail_report_ex

--- dms/v_ai_user_funded_datasets.sql
+++ dms/v_ai_user_funded_datasets.sql
@@ -3,15 +3,15 @@
 --
 
 CREATE VIEW public.v_ai_user_funded_datasets AS
- SELECT t_dataset.dataset,
-    t_instrument_name.instrument,
-    t_dataset.created AS date,
-    t.experiment,
+ SELECT ds.dataset,
+    instname.instrument,
+    ds.created AS date,
+    e.experiment,
     ai.value AS proposal_number
-   FROM (((public.v_experiment_detail_report_ex t
-     JOIN public.v_aux_info_value ai ON ((t.id = ai.target_id)))
-     JOIN public.t_dataset ON ((t.id = t_dataset.exp_id)))
-     JOIN public.t_instrument_name ON ((t_dataset.instrument_id = t_instrument_name.instrument_id)))
+   FROM (((public.t_experiments e
+     JOIN public.v_aux_info_value ai ON ((e.exp_id = ai.target_id)))
+     JOIN public.t_dataset ds ON ((e.exp_id = ds.exp_id)))
+     JOIN public.t_instrument_name instname ON ((ds.instrument_id = instname.instrument_id)))
   WHERE ((ai.target OPERATOR(public.=) 'Experiment'::public.citext) AND (ai.category OPERATOR(public.=) 'Accounting'::public.citext) AND (ai.subcategory OPERATOR(public.=) 'Funding'::public.citext) AND (ai.item OPERATOR(public.=) 'Proposal Number'::public.citext) AND (ai.value OPERATOR(public.<>) ''::public.citext));
 
 
