3ce9b95f65aa32329348985ffef7f3a9368294f6
alchemistmatt <matthew.monroe@pnnl.gov>
2024-05-23 17:36:46 -0700

    Query table directly instead of querying a view

--- dms/v_data_package_picklist.sql
+++ dms/v_data_package_picklist.sql
@@ -4,12 +4,18 @@
 
 CREATE VIEW public.v_data_package_picklist AS
  SELECT data_pkg_id AS id,
-    name,
-    (((((((id)::public.citext)::text || (': '::public.citext)::text))::public.citext)::text || (name)::text))::public.citext AS id_with_name
-   FROM dpkg.v_data_package_export;
+    package_name AS name,
+    (((data_pkg_id || (': '::public.citext)::text) || (package_name)::text))::public.citext AS id_with_name
+   FROM dpkg.t_data_package dp;
 
 
 ALTER VIEW public.v_data_package_picklist OWNER TO d3l243;
+
+--
+-- Name: VIEW v_data_package_picklist; Type: COMMENT; Schema: public; Owner: d3l243
+--
+
+COMMENT ON VIEW public.v_data_package_picklist IS 'Used by chooser "data_package_list"; see https://dmsdev.pnl.gov/config_db/edit_table/dms_chooser.db/chooser_definitions';
 
 --
 -- Name: TABLE v_data_package_picklist; Type: ACL; Schema: public; Owner: d3l243
