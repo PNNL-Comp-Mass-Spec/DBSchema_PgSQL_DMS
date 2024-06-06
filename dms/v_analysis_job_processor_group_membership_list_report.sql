0a8a4eec5f95c0cd7ea45f086ae554e025f69647
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-05 19:27:22 -0700

    Update view to show group_id after processor_name

--- dms/v_analysis_job_processor_group_membership_list_report.sql
+++ dms/v_analysis_job_processor_group_membership_list_report.sql
@@ -5,10 +5,10 @@
 CREATE VIEW public.v_analysis_job_processor_group_membership_list_report AS
  SELECT ajpgm.processor_id AS id,
     ajp.processor_name AS name,
+    ajpgm.group_id,
     ajpgm.membership_enabled,
     ajp.machine,
     ajp.notes,
-    ajpgm.group_id,
     (public.get_aj_processor_membership_in_groups_list(ajp.processor_id, 2))::public.citext AS group_membership
    FROM (public.t_analysis_job_processor_group_membership ajpgm
      JOIN public.t_analysis_job_processors ajp ON ((ajpgm.processor_id = ajp.processor_id)));
