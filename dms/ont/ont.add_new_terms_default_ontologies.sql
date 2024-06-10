435d434b5c0d6f847509cb9fba77b7675e33eca2
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-09 20:31:28 -0700

    When updating ontology terms, no longer call add_new_terms for NEWT; instead, use function add_new_newt_terms

--- dms/ont/ont.add_new_terms_default_ontologies.sql
+++ dms/ont/ont.add_new_terms_default_ontologies.sql
@@ -9,7 +9,20 @@ CREATE OR REPLACE FUNCTION ont.add_new_terms_default_ontologies(_infoonly boolea
 **
 **  Desc:
 **      Add new ontology terms to the ontology-specific tables for the default ontologies:
-**      CL, GO, MI (PSI_MI), MOD (PSI_Mod), PRIDE, NEWT, and DOID
+**      CL, GO, MI (PSI_MI), MOD (PSI_Mod), PRIDE, and DOID
+**
+**      Calls function add_new_terms, which pulls data from  t_ontology, t_term, and t_term_relationship (using v_term_lineage)
+**      and updates the ontology-specific table (t_cv_cl, t_cv_go, t_cv_mi, t_cv_mod, t_cv_pride, or t_cv_doid)
+**
+**      Note that BTO, ENVO, MS, and NEWT have dedicated functions for adding new terms
+**      - add_new_bto_terms
+**      - add_new_envo_terms
+**      - add_new_ms_terms
+**      - add_new_newt_terms
+**
+**  Arguments:
+**    _infoOnly       When true, preview updates
+**    _previewSql     When true, preview the SQL (but do not execute it)
 **
 **  Usage:
 **      SELECT * FROM ont.add_new_terms_default_ontologies(_infoOnly => false, _previewSql => false);
@@ -20,16 +33,25 @@ CREATE OR REPLACE FUNCTION ont.add_new_terms_default_ontologies(_infoonly boolea
 **          03/17/2014 mem - Added DOID (disease ontology)
 **          04/04/2022 mem - Ported to PostgreSQL
 **          10/04/2022 mem - Change _infoOnly from integer to boolean
+**          06/09/2024 mem - No longer call add_new_terms for NEWT; instead, use function add_new_newt_terms
 **
 *****************************************************/
 DECLARE
     _ontology citext;
 BEGIN
+
+    ---------------------------------------------------
+    -- Call add_new_terms for the ontologies of interest
+    -- This will pull data from t_ontology, t_term, and
+    -- t_term_relationship (using v_term_lineage) and update
+    -- the ontology-specific table (t_cv_cl, t_cv_go, etc.)
+    ---------------------------------------------------
+
+    -- Note that BTO, ENVO, MS, and NEWT have dedicated functions for adding new terms
+
     CREATE TEMP TABLE Tmp_OntologiesToUpdate (
         ontology citext
     );
-
-    -- Note that BTO, ENVO, and MS have dedicated functions for adding new terms
 
     INSERT INTO Tmp_OntologiesToUpdate (ontology)
     VALUES ('CL'),
@@ -37,7 +59,6 @@ BEGIN
            ('MI'),
            ('MOD'),
            ('PRIDE'),
-           ('NEWT'),
            ('DOID');
 
     FOR _ontology IN
@@ -56,7 +77,7 @@ BEGIN
 
     END LOOP;
 
-    Drop Table Tmp_OntologiesToUpdate;
+    DROP TABLE Tmp_OntologiesToUpdate;
 END
 $$;
 
