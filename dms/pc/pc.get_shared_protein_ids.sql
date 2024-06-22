84b1db544813276fd90592459c4a8bc608c5f703
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-21 20:27:31 -0700

    Use DISTINCT to assure that each protein ID only appears once
    Remove extra underscore from function arguments

--- dms/pc/pc.get_shared_protein_ids.sql
+++ dms/pc/pc.get_shared_protein_ids.sql
@@ -2,48 +2,81 @@
 -- Name: get_shared_protein_ids(integer, integer); Type: FUNCTION; Schema: pc; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION pc.get_shared_protein_ids(_collection_1 integer, _collection_2 integer) RETURNS TABLE(protein_id integer)
+CREATE OR REPLACE FUNCTION pc.get_shared_protein_ids(_collection1 integer, _collection2 integer) RETURNS TABLE(protein_id integer)
     LANGUAGE plpgsql
     AS $$
 /****************************************************
 **
 **  Desc:
-**      Show the shared Protein_IDs for two collections
+**      Show the shared protein IDs for two protein collections
+**
+**      If a protein collection has multiple instances of the same protein sequence (but different protein names)
+**      the returned table will only list the protein ID once
+**
+**  Arguments:
+**    _collection1  ID of the first protein collection
+**    _collection2  ID of the second protein collection
+**
+**  Example usage:
+**       --                      Protein collection stats
+**       -- Collection_ID  Collection_Name                     Proteins  Distinct_Sequences
+**       -- 3872           H_sapiens_UniProt_SPROT_2023-03-01  20407     20332
+**       -- 3909           H_sapiens_UniProt_SPROT_2023-09-01  20411     20337
+**
+**       SELECT PC.protein_collection_id,
+**              PC.collection_name,
+**              StatsQ.Proteins,
+**              StatsQ.Distinct_Sequences
+**       FROM (SELECT PCM.protein_collection_id,
+**                    COUNT(*) AS Proteins,
+**                    COUNT(DISTINCT PCM.protein_id) AS Distinct_Sequences
+**             FROM pc.v_protein_collection_members PCM
+**             WHERE PCM.protein_collection_id IN (3872, 3909)
+**             GROUP BY PCM.protein_collection_id
+**            ) StatsQ
+**            INNER JOIN pc.t_protein_collections PC
+**              ON StatsQ.protein_collection_id = PC.protein_collection_ID;
+**
+**       -- 20264 protein IDs in common
+**       SELECT COUNT(*)
+**       FROM pc.get_shared_protein_ids(3872, 3909);
 **
 **  Auth:   kja
 **  Date:   04/16/2004 kja - Initial version
 **          02/21/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
 **          05/02/2023 mem - Ported to PostgreSQL
+**          06/21/2024 mem - Remove extra underscore from function arguments
+**                         - Use DISTINCT to assure that each protein ID only appears once
 **
 *****************************************************/
 BEGIN
 
     RETURN QUERY
     WITH
-    Collection_1 (protein_ID, protein_collection_ID) AS
-      (SELECT PCM1.Protein_ID,
-              PCM1.Protein_Collection_ID
-       FROM pc.T_Protein_Collection_Members PCM1
-       WHERE PCM1.Protein_Collection_ID = _collection_1),
-    Collection_2 (protein_ID, protein_collection_ID) AS
-      (SELECT PCM2.Protein_ID,
-              PCM2.Protein_Collection_ID
-       FROM pc.T_Protein_Collection_Members PCM2
-       WHERE PCM2.Protein_Collection_ID = _collection_2)
-    SELECT collection_2.protein_ID
-    FROM Collection_1
-         INNER JOIN Collection_2
-           ON Collection_1.Protein_ID = Collection_2.protein_ID;
+    Collection1 (protein_id, protein_collection_id) AS
+      (SELECT PCM1.protein_id,
+              PCM1.protein_collection_id
+       FROM pc.t_protein_collection_members PCM1
+       WHERE PCM1.protein_collection_id = _collection1),
+    Collection2 (protein_id, protein_collection_id) AS
+      (SELECT PCM2.protein_id,
+              PCM2.protein_collection_id
+       FROM pc.t_protein_collection_members PCM2
+       WHERE PCM2.protein_collection_id = _collection2)
+    SELECT DISTINCT Collection2.protein_id
+    FROM Collection1
+         INNER JOIN Collection2
+           ON Collection1.protein_id = Collection2.protein_id;
 
 END
 $$;
 
 
-ALTER FUNCTION pc.get_shared_protein_ids(_collection_1 integer, _collection_2 integer) OWNER TO d3l243;
+ALTER FUNCTION pc.get_shared_protein_ids(_collection1 integer, _collection2 integer) OWNER TO d3l243;
 
 --
--- Name: FUNCTION get_shared_protein_ids(_collection_1 integer, _collection_2 integer); Type: COMMENT; Schema: pc; Owner: d3l243
+-- Name: FUNCTION get_shared_protein_ids(_collection1 integer, _collection2 integer); Type: COMMENT; Schema: pc; Owner: d3l243
 --
 
-COMMENT ON FUNCTION pc.get_shared_protein_ids(_collection_1 integer, _collection_2 integer) IS 'GetSharedProteinIDs';
+COMMENT ON FUNCTION pc.get_shared_protein_ids(_collection1 integer, _collection2 integer) IS 'GetSharedProteinIDs';
 
--- dms/pc/pc.get_unique_protein_ids.sql
+++ dms/pc/pc.get_unique_protein_ids.sql
@@ -2,49 +2,61 @@
 -- Name: get_unique_protein_ids(integer, integer); Type: FUNCTION; Schema: pc; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION pc.get_unique_protein_ids(_collection_1 integer, _collection_2 integer) RETURNS TABLE(protein_id integer)
+CREATE OR REPLACE FUNCTION pc.get_unique_protein_ids(_collection1 integer, _collection2 integer) RETURNS TABLE(protein_id integer)
     LANGUAGE plpgsql
     AS $$
 /****************************************************
 **
 **  Desc:
-**      Show the Protein_IDs present in collection_1 that are not in collection_2
+**      Show the protein IDs present in collection 1 that are not in collection 2
+**
+**  Arguments:
+**    _collection1  ID of the first protein collection
+**    _collection2  ID of the second protein collection
+**
+**  Example Usage:
+**      -- 68 proteins are in collection 1 but not in collection 2
+**      SELECT COUNT(*)
+**      FROM pc.get_unique_protein_ids(3872, 3909);
 **
 **  Auth:   kja
 **  Date:   04/16/2004 kja - Initial version
 **          02/21/2023 bcg - Rename procedure and parameters to a case-insensitive match to postgres
 **          05/02/2023 mem - Ported to PostgreSQL
+**          06/21/2024 mem - Remove extra underscore from function arguments
+**                         - Use DISTINCT to assure that each protein ID only appears once
+**                         - Fix bug that showed number of proteins in collection 2, but not in collection 1
 **
 *****************************************************/
 BEGIN
 
     RETURN QUERY
     WITH
-    Collection_1 (protein_ID, protein_collection_ID) AS
-      (SELECT PCM1.Protein_ID,
-              PCM1.Protein_Collection_ID
-       FROM pc.T_Protein_Collection_Members PCM1
-       WHERE PCM1.Protein_Collection_ID = _collection_1),
-    Collection_2 (protein_ID, protein_collection_ID) AS
-      (SELECT PCM2.Protein_ID,
-              PCM2.Protein_Collection_ID
+    Collection1 (protein_id, protein_collection_id) AS
+      (SELECT PCM1.protein_id,
+              PCM1.protein_collection_id
+       FROM pc.t_protein_collection_members PCM1
+       WHERE PCM1.protein_collection_id = _collection1),
+    Collection2 (protein_id, protein_collection_id) AS
+      (SELECT PCM2.protein_id,
+              PCM2.protein_collection_id
        FROM pc.T_Protein_Collection_Members PCM2
-       WHERE PCM2.Protein_Collection_ID = _collection_2)
-    SELECT collection_2.protein_ID
-    FROM Collection_1
-         RIGHT OUTER JOIN Collection_2
-           ON Collection_1.Protein_ID = Collection_2.protein_ID
-    WHERE Collection_1.Protein_ID IS NULL;
+       WHERE PCM2.protein_collection_id = _collection2)
+    SELECT DISTINCT Collection1.protein_id
+    FROM Collection1
+         LEFT OUTER JOIN Collection2
+           ON Collection1.protein_id = Collection2.protein_id
+    WHERE Collection2.protein_id IS NULL;
 
 END
 $$;
 
 
-ALTER FUNCTION pc.get_unique_protein_ids(_collection_1 integer, _collection_2 integer) OWNER TO d3l243;
+ALTER FUNCTION pc.get_unique_protein_ids(_collection1 integer, _collection2 integer) OWNER TO d3l243;
 
 --
--- Name: FUNCTION get_unique_protein_ids(_collection_1 integer, _collection_2 integer); Type: COMMENT; Schema: pc; Owner: d3l243
+-- Name: FUNCTION get_unique_protein_ids(_collection1 integer, _collection2 integer); Type: COMMENT; Schema: pc; Owner: d3l243
 --
 
-COMMENT ON FUNCTION pc.get_unique_protein_ids(_collection_1 integer, _collection_2 integer) IS 'GetUniqueProteinIDs';
+COMMENT ON FUNCTION pc.get_unique_protein_ids(_collection1 integer, _collection2 integer) IS 'GetUniqueProteinIDs';
 
