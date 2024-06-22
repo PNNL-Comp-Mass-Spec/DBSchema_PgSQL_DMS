5e9483662d93c9e4703f4d6975d8d29facc223dd
alchemistmatt <matthew.monroe@pnnl.gov>
2024-06-21 20:27:07 -0700

    Add parameter _directorySeparator

--- dms/pc/pc.get_file_name_from_path.sql
+++ dms/pc/pc.get_file_name_from_path.sql
@@ -1,19 +1,25 @@
 --
--- Name: get_file_name_from_path(text); Type: FUNCTION; Schema: pc; Owner: d3l243
+-- Name: get_file_name_from_path(text, text); Type: FUNCTION; Schema: pc; Owner: d3l243
 --
 
-CREATE OR REPLACE FUNCTION pc.get_file_name_from_path(_filepath text) RETURNS text
+CREATE OR REPLACE FUNCTION pc.get_file_name_from_path(_filepath text, _directoryseparator text DEFAULT '\'::text) RETURNS text
     LANGUAGE plpgsql
     AS $$
 /****************************************************
 **
 **  Desc:
-**      Look for the final \ in _filePath, then returns the filename after the slash
-**      If no slash in _filePath, or if no text after the slash, returns an empty string
+**      Look for the final backslash (or forward slash) in _filePath, then return the filename after the slash
+**
+**      If no slash in _filePath, or if no text after the directory separator, return an empty string
+**
+**  Arguments:
+**    _filePath             File path, e.g. C:\Temp\Data.txt
+**    _directorySeparator   Directory separator (defaults to a backslash)
 **
 **  Auth:   mem
 **  Date:   10/09/2006
 **          06/27/2022 mem - Ported to PostgreSQL
+**          06/21/2024 mem - Add parameter _directorySeparator
 **
 *****************************************************/
 DECLARE
@@ -23,12 +29,18 @@ DECLARE
 BEGIN
     _fileName := '';
 
-    _filePath := Trim(Coalesce(_filePath, ''));
+    _filePath           := Trim(Coalesce(_filePath, ''));
+    _directorySeparator := Trim(Coalesce(_directorySeparator, '\'));
+
+    If Not _directorySeparator IN ('\', '/') Then
+        RAISE WARNING 'Invalid directory separator "%"; will instead use "\"', _directorySeparator;
+        _directorySeparator := '\';
+    End If;
 
     _pathLength := char_length(_filePath);
 
     If _pathLength > 0 Then
-        _slashLoc := Position('\' In reverse(_filePath));
+        _slashLoc := Position(_directorySeparator In reverse(_filePath));
         If _slashLoc > 0 Then
             _slashLoc := _pathLength - _slashLoc + 1;
             If _slashLoc < _pathLength Then
@@ -42,11 +54,11 @@ END
 $$;
 
 
-ALTER FUNCTION pc.get_file_name_from_path(_filepath text) OWNER TO d3l243;
+ALTER FUNCTION pc.get_file_name_from_path(_filepath text, _directoryseparator text) OWNER TO d3l243;
 
 --
--- Name: FUNCTION get_file_name_from_path(_filepath text); Type: COMMENT; Schema: pc; Owner: d3l243
+-- Name: FUNCTION get_file_name_from_path(_filepath text, _directoryseparator text); Type: COMMENT; Schema: pc; Owner: d3l243
 --
 
-COMMENT ON FUNCTION pc.get_file_name_from_path(_filepath text) IS 'GetFileNameFromPath';
+COMMENT ON FUNCTION pc.get_file_name_from_path(_filepath text, _directoryseparator text) IS 'GetFileNameFromPath';
 
