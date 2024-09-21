10cc2d4fcad59e2a05e42c354208db6967bd0b5b
alchemistmatt <matthew.monroe@pnnl.gov>
2024-09-20 18:02:33 -0700

    Add mc and pc schema tables to v_log_entries

--- dms/v_log_entries.sql
+++ dms/v_log_entries.sql
@@ -13,31 +13,49 @@ CREATE VIEW public.v_log_entries AS
    FROM public.t_log_entries
 UNION
  SELECT 'cap'::public.citext AS schema,
-    t_log_entries.entry_id,
-    t_log_entries.posted_by,
-    t_log_entries.entered,
-    t_log_entries.type,
-    t_log_entries.message,
-    t_log_entries.entered_by
-   FROM cap.t_log_entries
+    ctl.entry_id,
+    ctl.posted_by,
+    ctl.entered,
+    ctl.type,
+    ctl.message,
+    ctl.entered_by
+   FROM cap.t_log_entries ctl
 UNION
  SELECT 'sw'::public.citext AS schema,
-    t_log_entries.entry_id,
-    t_log_entries.posted_by,
-    t_log_entries.entered,
-    t_log_entries.type,
-    t_log_entries.message,
-    t_log_entries.entered_by
-   FROM sw.t_log_entries
+    swl.entry_id,
+    swl.posted_by,
+    swl.entered,
+    swl.type,
+    swl.message,
+    swl.entered_by
+   FROM sw.t_log_entries swl
 UNION
  SELECT 'dpkg'::public.citext AS schema,
-    t_log_entries.entry_id,
-    t_log_entries.posted_by,
-    t_log_entries.entered,
-    t_log_entries.type,
-    t_log_entries.message,
-    t_log_entries.entered_by
-   FROM dpkg.t_log_entries;
+    dpl.entry_id,
+    dpl.posted_by,
+    dpl.entered,
+    dpl.type,
+    dpl.message,
+    dpl.entered_by
+   FROM dpkg.t_log_entries dpl
+UNION
+ SELECT 'mc'::public.citext AS schema,
+    mcl.entry_id,
+    mcl.posted_by,
+    mcl.entered,
+    mcl.type,
+    mcl.message,
+    mcl.entered_by
+   FROM mc.t_log_entries mcl
+UNION
+ SELECT 'pc'::public.citext AS schema,
+    pcl.entry_id,
+    pcl.posted_by,
+    pcl.entered,
+    pcl.type,
+    pcl.message,
+    pcl.entered_by
+   FROM pc.t_log_entries pcl;
 
 
 ALTER VIEW public.v_log_entries OWNER TO d3l243;
