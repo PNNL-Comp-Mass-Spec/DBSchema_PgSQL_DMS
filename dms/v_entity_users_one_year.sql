9fb3c9d568258a8cc37b7bc3efa491e3b0efcf05
alchemistmatt <matthew.monroe@pnnl.gov>
2025-01-23 18:23:51 -0800

    Change column data type to citext

--- dms/v_entity_users_one_year.sql
+++ dms/v_entity_users_one_year.sql
@@ -3,14 +3,14 @@
 --
 
 CREATE VIEW public.v_entity_users_one_year AS
- SELECT 'Analysis job owner'::text AS entity,
+ SELECT 'Analysis job owner'::public.citext AS entity,
     t_analysis_job.owner_username AS username,
     count(*) AS items
    FROM public.t_analysis_job
   WHERE ((t_analysis_job.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_analysis_job.created <= CURRENT_TIMESTAMP))
   GROUP BY t_analysis_job.owner_username
 UNION
- SELECT 'Analysis job request user'::text AS entity,
+ SELECT 'Analysis job request user'::public.citext AS entity,
     u.username,
     count(*) AS items
    FROM (public.t_analysis_job_request ajr
@@ -18,70 +18,70 @@ UNION
   WHERE ((ajr.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (ajr.created <= CURRENT_TIMESTAMP))
   GROUP BY u.username
 UNION
- SELECT 'Biomaterial contact'::text AS entity,
+ SELECT 'Biomaterial contact'::public.citext AS entity,
     t_biomaterial.contact_username AS username,
     count(*) AS items
    FROM public.t_biomaterial
   WHERE ((t_biomaterial.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_biomaterial.created <= CURRENT_TIMESTAMP))
   GROUP BY t_biomaterial.contact_username
 UNION
- SELECT 'Dataset operator'::text AS entity,
+ SELECT 'Dataset operator'::public.citext AS entity,
     t_dataset.operator_username AS username,
     count(*) AS items
    FROM public.t_dataset
   WHERE ((t_dataset.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_dataset.created <= CURRENT_TIMESTAMP))
   GROUP BY t_dataset.operator_username
 UNION
- SELECT 'Experiment group researcher'::text AS entity,
+ SELECT 'Experiment group researcher'::public.citext AS entity,
     t_experiment_groups.researcher_username AS username,
     count(*) AS items
    FROM public.t_experiment_groups
   WHERE ((t_experiment_groups.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_experiment_groups.created <= CURRENT_TIMESTAMP))
   GROUP BY t_experiment_groups.researcher_username
 UNION
- SELECT 'Experiment researcher'::text AS entity,
+ SELECT 'Experiment researcher'::public.citext AS entity,
     t_experiments.researcher_username AS username,
     count(*) AS items
    FROM public.t_experiments
   WHERE ((t_experiments.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_experiments.created <= CURRENT_TIMESTAMP))
   GROUP BY t_experiments.researcher_username
 UNION
- SELECT 'Cart config entered'::text AS entity,
+ SELECT 'Cart config entered'::public.citext AS entity,
     t_lc_cart_configuration.entered_by AS username,
     count(*) AS items
    FROM public.t_lc_cart_configuration
   WHERE ((t_lc_cart_configuration.entered >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_lc_cart_configuration.entered <= CURRENT_TIMESTAMP))
   GROUP BY t_lc_cart_configuration.entered_by
 UNION
- SELECT 'Cart config updated'::text AS entity,
+ SELECT 'Cart config updated'::public.citext AS entity,
     t_lc_cart_configuration.updated_by AS username,
     count(*) AS items
    FROM public.t_lc_cart_configuration
   WHERE ((t_lc_cart_configuration.entered >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_lc_cart_configuration.entered <= CURRENT_TIMESTAMP))
   GROUP BY t_lc_cart_configuration.updated_by
 UNION
- SELECT 'LC column operator'::text AS entity,
+ SELECT 'LC column operator'::public.citext AS entity,
     t_lc_column.operator_username AS username,
     count(*) AS items
    FROM public.t_lc_column
   WHERE ((t_lc_column.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_lc_column.created <= CURRENT_TIMESTAMP))
   GROUP BY t_lc_column.operator_username
 UNION
- SELECT 'Reference compount contact'::text AS entity,
+ SELECT 'Reference compount contact'::public.citext AS entity,
     t_reference_compound.contact_username AS username,
     count(*) AS items
    FROM public.t_reference_compound
   WHERE ((t_reference_compound.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_reference_compound.created <= CURRENT_TIMESTAMP))
   GROUP BY t_reference_compound.contact_username
 UNION
- SELECT 'Requested run requester'::text AS entity,
+ SELECT 'Requested run requester'::public.citext AS entity,
     t_requested_run.requester_username AS username,
     count(*) AS items
    FROM public.t_requested_run
   WHERE ((t_requested_run.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_requested_run.created <= CURRENT_TIMESTAMP))
   GROUP BY t_requested_run.requester_username
 UNION
- SELECT 'Requested run updater'::text AS entity,
+ SELECT 'Requested run updater'::public.citext AS entity,
     (public.replace(t_requested_run.updated_by, 'PNL\'::public.citext, ''::public.citext))::public.citext AS username,
     count(*) AS items
    FROM public.t_requested_run
@@ -91,7 +91,7 @@ UNION
  SELECT countq.entity,
     (replace(public.replace(countq.entered_by, ' (via dmswebuser)'::public.citext, ''::public.citext), 'PNL\'::text, ''::text))::public.citext AS username,
     sum(countq.items) AS items
-   FROM ( SELECT 'Analysis job state change in sw.t_jobs'::text AS entity,
+   FROM ( SELECT 'Analysis job state change in sw.t_jobs'::public.citext AS entity,
             t_job_events.entered_by,
             count(*) AS items
            FROM sw.t_job_events
@@ -99,42 +99,42 @@ UNION
           GROUP BY t_job_events.entered_by) countq
   GROUP BY countq.entity, (replace(public.replace(countq.entered_by, ' (via dmswebuser)'::public.citext, ''::public.citext), 'PNL\'::text, ''::text))::public.citext
 UNION
- SELECT 'Analysis job owner in sw.t_jobs'::text AS entity,
+ SELECT 'Analysis job owner in sw.t_jobs'::public.citext AS entity,
     t_jobs.owner_username AS username,
     count(*) AS items
    FROM sw.t_jobs
   WHERE ((t_jobs.imported >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_jobs.imported <= CURRENT_TIMESTAMP))
   GROUP BY t_jobs.owner_username
 UNION
- SELECT 'Procedure usage in sw schema'::text AS entity,
+ SELECT 'Procedure usage in sw schema'::public.citext AS entity,
     t_sp_usage.calling_user AS username,
     count(*) AS items
    FROM sw.t_sp_usage
   WHERE ((t_sp_usage.entered >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_sp_usage.entered <= CURRENT_TIMESTAMP))
   GROUP BY t_sp_usage.calling_user
 UNION
- SELECT 'Capture task state change in cap.t_tasks'::text AS entity,
+ SELECT 'Capture task state change in cap.t_tasks'::public.citext AS entity,
     (public.replace(t_task_events.entered_by, 'PNL\'::public.citext, ''::public.citext))::public.citext AS username,
     count(*) AS items
    FROM cap.t_task_events
   WHERE ((t_task_events.entered >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_task_events.entered <= CURRENT_TIMESTAMP))
   GROUP BY (public.replace(t_task_events.entered_by, 'PNL\'::public.citext, ''::public.citext))::public.citext
 UNION
- SELECT 'Data package owner'::text AS entity,
+ SELECT 'Data package owner'::public.citext AS entity,
     t_data_package.owner_username AS username,
     count(*) AS items
    FROM dpkg.t_data_package
   WHERE ((t_data_package.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_data_package.created <= CURRENT_TIMESTAMP))
   GROUP BY t_data_package.owner_username
 UNION
- SELECT 'Data package requester'::text AS entity,
+ SELECT 'Data package requester'::public.citext AS entity,
     t_data_package.requester AS username,
     count(*) AS items
    FROM dpkg.t_data_package
   WHERE ((t_data_package.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (t_data_package.created <= CURRENT_TIMESTAMP))
   GROUP BY t_data_package.requester
 UNION
- SELECT replaceq.entity,
+ SELECT (replaceq.entity)::public.citext AS entity,
         CASE
             WHEN (replaceq.semicolon_position > 1) THEN ("left"((replaceq.entered_by)::text, (replaceq.semicolon_position - 1)))::public.citext
             ELSE replaceq.entered_by
@@ -169,7 +169,7 @@ UNION
             ELSE replaceq.entered_by
         END
 UNION
- SELECT 'Requested run batch user'::text AS entity,
+ SELECT 'Requested run batch user'::public.citext AS entity,
     u.username,
     count(*) AS items
    FROM (public.t_requested_run_batches rrb
@@ -177,7 +177,7 @@ UNION
   WHERE ((rrb.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (rrb.created <= CURRENT_TIMESTAMP))
   GROUP BY u.username
 UNION
- SELECT 'Requested run batch group user'::text AS entity,
+ SELECT 'Requested run batch group user'::public.citext AS entity,
     u.username,
     count(*) AS items
    FROM (public.t_requested_run_batch_group rrbg
@@ -185,7 +185,7 @@ UNION
   WHERE ((rrbg.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (rrbg.created <= CURRENT_TIMESTAMP))
   GROUP BY u.username
 UNION
- SELECT 'Sample prep request requester'::text AS entity,
+ SELECT 'Sample prep request requester'::public.citext AS entity,
     t_sample_prep_request.requester_username AS username,
     count(*) AS items
    FROM public.t_sample_prep_request
@@ -204,7 +204,7 @@ UNION
            FROM ( SELECT countq.entity,
                     TRIM(BOTH FROM public.regexp_split_to_table(countq.assigned_personnel, ';'::public.citext)) AS assigned_personnel,
                     countq.items
-                   FROM ( SELECT 'Sample prep request personnel'::text AS entity,
+                   FROM ( SELECT 'Sample prep request personnel'::public.citext AS entity,
                             t_sample_prep_request.assigned_personnel,
                             count(*) AS items
                            FROM public.t_sample_prep_request
@@ -212,7 +212,7 @@ UNION
                           GROUP BY t_sample_prep_request.assigned_personnel) countq) splitq) matchq
   WHERE (NOT (matchq.assigned_personnel IS NULL))
 UNION
- SELECT 'Sample submission receiver'::text AS entity,
+ SELECT 'Sample submission receiver'::public.citext AS entity,
     u.username,
     count(*) AS items
    FROM (public.t_sample_submission ss
@@ -220,7 +220,7 @@ UNION
   WHERE ((ss.created >= (CURRENT_TIMESTAMP - '1 year'::interval)) AND (ss.created <= CURRENT_TIMESTAMP))
   GROUP BY u.username
 UNION
- SELECT 'New user'::text AS entity,
+ SELECT 'New user'::public.citext AS entity,
     t_users.username,
     count(*) AS items
    FROM public.t_users
