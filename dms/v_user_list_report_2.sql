8773345e66402de9e0835b91fbc5430e3c39053c
alchemistmatt <matthew.monroe@pnnl.gov>
2025-03-05 15:51:57 -0800

    Add column to user list report view

--- dms/v_user_list_report_2.sql
+++ dms/v_user_list_report_2.sql
@@ -11,6 +11,7 @@ CREATE VIEW public.v_user_list_report_2 AS
     (public.get_user_operations_list(u.user_id))::public.citext AS operations_list,
     u.comment,
     u.created AS created_dms,
+    u.update,
     eu.person_id AS eus_id,
     eu.valid AS valid_eus_id,
     ess.eus_site_status AS eus_status,
@@ -26,7 +27,7 @@ ALTER VIEW public.v_user_list_report_2 OWNER TO d3l243;
 -- Name: VIEW v_user_list_report_2; Type: COMMENT; Schema: public; Owner: d3l243
 --
 
-COMMENT ON VIEW public.v_user_list_report_2 IS 'Note that a few DMS Users have multiple EUS Person_ID values. That leads to duplicate rows in this report, but it doesn"t hurt anything (and is actually informative)';
+COMMENT ON VIEW public.v_user_list_report_2 IS 'Note that some DMS Users have multiple EUS Person_ID values; that leads to duplicate rows in this report, but it doesn"t hurt anything (and is actually informative)';
 
 --
 -- Name: TABLE v_user_list_report_2; Type: ACL; Schema: public; Owner: d3l243
