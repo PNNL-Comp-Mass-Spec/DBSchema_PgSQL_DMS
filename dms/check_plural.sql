86b9477466dd387d0a035cd5fc2fdf9cc9ef3f06
alchemistmatt <matthew.monroe@pnnl.gov>
2025-06-25 18:48:58 -0700

    Add overload to check_plural()

--- dms/check_plural.sql
+++ dms/check_plural.sql
@@ -1,6 +1,7 @@
 --
 -- Name: check_plural(integer, text, text); Type: FUNCTION; Schema: public; Owner: d3l243
 --
+-- Overload 1
 
 CREATE OR REPLACE FUNCTION public.check_plural(_count integer DEFAULT 0, _textifoneitem text DEFAULT 'item'::text, _textifzeroormultiple text DEFAULT 'items'::text) RETURNS text
     LANGUAGE plpgsql
@@ -38,4 +39,38 @@ ALTER FUNCTION public.check_plural(_count integer, _textifoneitem text, _textifz
 --
 
 COMMENT ON FUNCTION public.check_plural(_count integer, _textifoneitem text, _textifzeroormultiple text) IS 'CheckPlural';
+
+--
+-- Name: check_plural(text, text, text); Type: FUNCTION; Schema: public; Owner: d3l243
+--
+-- Overload 2
+
+CREATE OR REPLACE FUNCTION public.check_plural(_itemlist text DEFAULT ''::text, _textifoneitem text DEFAULT 'item'::text, _textifzeroormultiple text DEFAULT 'items'::text) RETURNS text
+    LANGUAGE plpgsql
+    AS $$
+/****************************************************
+**
+**  Desc:
+**      Return _textIfOneItem if _count is 1; otherwise, returns _textIfZeroOrMultiple
+**
+**  Arguments:
+**    _itemList                 Comma separated list of items
+**    _textIfOneItem            Text to return if _itemList does not have a comma
+**    _textIfZeroOrMultiple     Text to return if _itemList has a comma
+**
+**  Auth:   mem
+**  Date:   06/25/2025 mem - Overloaded version function check_plural() that accepts an integer
+**
+*****************************************************/
+BEGIN
+    If Coalesce(_itemList, '') LIKE '%,%' OR Coalesce(_itemList, '') = '' Then
+        RETURN _textIfZeroOrMultiple;
+    Else
+        RETURN _textIfOneItem;
+    End If;
+END
+$$;
+
+
+ALTER FUNCTION public.check_plural(_itemlist text, _textifoneitem text, _textifzeroormultiple text) OWNER TO d3l243;
 
