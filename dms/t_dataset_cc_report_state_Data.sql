f5cb6594dba4e20866d1b3385c67f5bebf2dd972
alchemistmatt <matthew.monroe@pnnl.gov>
2025-08-20 20:30:38 -0700

    Update dataset service center report state descriptions

--- dms/t_dataset_cc_report_state_Data.sql
+++ dms/t_dataset_cc_report_state_Data.sql
@@ -23,11 +23,11 @@ SET row_security = off;
 
 COPY public.t_dataset_cc_report_state (cc_report_state_id, cc_report_state, description) FROM stdin;
 0	Undefined	
-1	Do not submit to cost center	
-2	Need to submit to cost center	
-3	Submitted to cost center	
-4	Need to refund to cost center	
-5	Refunded to cost center	
+1	Do not submit to service center	
+2	Need to submit to service center	
+3	Submitted to service center	
+4	Need to refund to service center	
+5	Refunded to service center	
 \.
 
 
