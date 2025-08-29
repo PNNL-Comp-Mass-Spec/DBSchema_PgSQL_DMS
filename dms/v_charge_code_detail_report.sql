7be8e0ba63e9e94c40f8fc8657483ff763cd6674
alchemistmatt <matthew.monroe@pnnl.gov>
2025-08-28 20:11:27 -0700

    Add/update columns in charge code view

--- dms/v_charge_code_detail_report.sql
+++ dms/v_charge_code_detail_report.sql
@@ -4,7 +4,7 @@
 
 CREATE VIEW public.v_charge_code_detail_report AS
  SELECT cc.charge_code,
-    COALESCE(cca.activation_state_name, 'Invalid'::public.citext) AS state,
+    COALESCE(cca.activation_state_name, 'Invalid'::public.citext) AS charge_code_activation_state,
     cc.wbs_title AS wbs,
     cc.charge_code_title AS title,
     cc.sub_account AS sub_account_id,
@@ -22,10 +22,12 @@ CREATE VIEW public.v_charge_code_detail_report AS
     dmsuser.username AS owner_username,
     dmsuser.name AS owner_name,
     cc.auto_defined,
-    cc.charge_code_state,
+    cc.charge_code_state AS charge_code_state_id,
+    ccs.charge_code_state_name,
     cc.last_affected,
     cca.activation_state AS wp_activation_state
-   FROM ((public.t_charge_code cc
+   FROM (((public.t_charge_code cc
+     JOIN public.t_charge_code_state ccs ON ((cc.charge_code_state = ccs.charge_code_state)))
      JOIN public.t_charge_code_activation_state cca ON ((cc.activation_state = cca.activation_state)))
      LEFT JOIN public.v_charge_code_owner_dms_user_map dmsuser ON ((cc.charge_code OPERATOR(public.=) dmsuser.charge_code)));
 
