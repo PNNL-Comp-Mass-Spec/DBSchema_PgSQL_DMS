335c7bf501d3049dce78e8915909b299164af28e
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-11 16:40:43 -0700

    Remove ticket_number from list report since ticket_number is now entry_id

--- dms/v_service_center_use_list_report.sql
+++ dms/v_service_center_use_list_report.sql
@@ -14,8 +14,7 @@ CREATE VIEW public.v_service_center_use_list_report AS
     u.transaction_date,
     u.transaction_units,
     u.is_held,
-    u.comment,
-    u.ticket_number
+    u.comment
    FROM ((((svc.t_service_use u
      JOIN svc.t_service_use_report rep ON ((rep.report_id = u.report_id)))
      JOIN svc.t_service_type t ON ((t.service_type_id = u.service_type_id)))
