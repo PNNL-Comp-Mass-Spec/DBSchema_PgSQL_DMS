10c9a7eaed9228f5ac89fb9761f20249d551198a
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-17 20:06:03 -0700

    Rename columns in service center use export view

--- dms/v_service_center_use_export.sql
+++ dms/v_service_center_use_export.sql
@@ -3,21 +3,25 @@
 --
 
 CREATE VIEW public.v_service_center_use_export AS
- SELECT u.ticket_number AS "TicketNumber",
-    cc.sub_account AS "Subaccount",
-    u.service_type_id AS "TOS",
-    rep.requestor_employee_id AS "RequestorEmployeeId",
-    u.charge_code AS "ChargeCode",
-    u.transaction_date AS "TransactionDate",
-    u.transaction_units AS "TransactionUnits",
-    u.is_held AS "IsHeld",
-    u.comment AS "Comment",
+ SELECT u.ticket_number,
+    cc.sub_account,
+    u.service_type_id,
+    rep.requestor_employee_id,
+    u.charge_code,
+    u.transaction_date,
+    u.transaction_units,
+    u.is_held,
+    u.comment,
+    inst.instrument,
+    u.dataset_id,
     u.report_id
-   FROM ((((svc.t_service_use u
+   FROM ((((((svc.t_service_use u
      JOIN svc.t_service_use_report rep ON ((rep.report_id = u.report_id)))
      JOIN svc.t_service_type t ON ((t.service_type_id = u.service_type_id)))
      LEFT JOIN public.t_charge_code cc ON ((cc.charge_code OPERATOR(public.=) u.charge_code)))
      LEFT JOIN public.t_cached_dataset_stats cds ON ((u.dataset_id = cds.dataset_id)))
+     LEFT JOIN public.t_dataset ds ON ((u.dataset_id = ds.dataset_id)))
+     LEFT JOIN public.t_instrument_name inst ON ((ds.instrument_id = inst.instrument_id)))
   WHERE (NOT (u.service_type_id = ANY (ARRAY[0, 1, 25])));
 
 
