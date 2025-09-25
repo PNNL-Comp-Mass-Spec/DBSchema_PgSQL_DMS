fb16790afb53714d6e807c008f57d31150bf5ae0
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-24 19:30:01 -0700

    Add mode 'serviceCenterRefund' to procedure

--- dms/update_service_use_entries.sql
+++ dms/update_service_use_entries.sql
@@ -11,13 +11,19 @@ CREATE OR REPLACE PROCEDURE public.update_service_use_entries(IN _mode text, IN 
 **      Update the dataset rating of the datasets associated with the specified service use entries
 **      Service use entries must be associated with an active service use report
 **
+**      If _mode is 'serviceCenterRefund' and _newValue is 'true', change the service center report state to 'Need to refund to service center' for the specified datasets
+**      The service use entry IDs must be associated with a completed service use report, and the datasets must have a service center report state of 'Submitted to service center' or 'Need to refund to service center'
+**
+**      If _mode is 'serviceCenterRefund' and _newValue is 'false', change the service center report state to 'Submitted to service center' if the report state is 'Need to refund to service center'
+**      The service use entry IDs must be associated with a completed service use report, and the datasets must have a service center report state of 'Submitted to service center' or 'Need to refund to service center'
+**
 **      This procedure is called via a POST to service_use/operation/ , originating from https://dms2.pnl.gov/service_use_admin/report
 **         - See file service_use_admin_cmds.php at https://github.com/PNNL-Comp-Mass-Spec/DMS-Website/blob/master/app/Views/cmd/service_use_admin_cmds.php
 **           and file lcmd.js at https://github.com/PNNL-Comp-Mass-Spec/DMS-Website/blob/89871a5bbbde297a5878194787c418e7a42cd9ad/public/javascript/lcmd.js#L226
 **
 **  Arguments:
-**    _mode             Mode: 'datasetRating'
-**    _newValue         When mode is 'datasetRating', this is the dataset rating name
+**    _mode             Mode: 'datasetRating', 'serviceCenterRefund'
+**    _newValue         When mode is 'datasetRating', this is the dataset rating name; when mode is 'serviceCenterRefund', this is either 'true' or 'false'
 **    _entryIdList      Comma-separated list of service use entry IDs
 **    _infoOnly         When true, preview updates
 **    _message          Status message
@@ -27,6 +33,7 @@ CREATE OR REPLACE PROCEDURE public.update_service_use_entries(IN _mode text, IN 
 **  Auth:   mem
 **  Date:   08/18/2025 mem - Initial version
 **          08/20/2025 mem - Reference schema svc instead of cc
+**          09/23/2025 mem - Add mode 'serviceCenterRefund'
 **
 *****************************************************/
 DECLARE
@@ -38,6 +45,8 @@ DECLARE
     _logErrors boolean := false;
 
     _lockedServiceUseEntries int;
+    _unlockedServiceUseEntries int;
+    _refundIneligibleUseEntries int;
     _updateCount int;
     _entryCount int := 0;
     _valueList text;
@@ -45,6 +54,8 @@ DECLARE
 
     _newDatasetRating citext := '';
     _newDatasetRatingID int := 0;
+    _newDatasetServiceCenterReportState citext := '';
+    _newDatasetServiceCenterReportStateID int := 0;
     _datasetID int;
 
     _formatSpecifier text;
@@ -106,10 +117,15 @@ BEGIN
         _newValue     := Trim(Coalesce(_newValue, ''));
         _entryIdList  := Trim(Coalesce(_entryIdList, ''));
         _infoOnly     := Coalesce(_infoOnly, false);
+        _callingUser  := Trim(Coalesce(_callingUser, ''));
 
-        If Not _mode::citext IN ('datasetRating') Then
+        If Not _mode::citext IN ('datasetRating', 'serviceCenterRefund') Then
             _returnCode := 'U5201';
             RAISE EXCEPTION 'Unsupported mode: %', _mode;
+        End If;
+
+        If _callingUser = '' Then
+            _callingUser := SESSION_USER;
         End If;
 
         ---------------------------------------------------
@@ -134,24 +150,67 @@ BEGIN
         End If;
 
         ---------------------------------------------------
-        -- Look for service use entries associated with service use reports that are not New or Active
+        -- If mode is 'datasetRating', look for service use entries associated with service use reports that are not New or Active
         ---------------------------------------------------
 
-        SELECT COUNT(U.entry_id)
-        INTO _lockedServiceUseEntries
-        FROM Tmp_ServiceUseIDs src
-             INNER JOIN svc.t_service_use U
-               ON U.entry_id = src.entry_id
-             INNER JOIN svc.t_service_use_report R
-               ON R.report_id = U.report_id
-        WHERE Not R.report_state_id IN (1, 2);
+        If _mode::citext = 'datasetRating' Then
+            SELECT COUNT(U.entry_id)
+            INTO _lockedServiceUseEntries
+            FROM Tmp_ServiceUseIDs src
+                 INNER JOIN svc.t_service_use U
+                   ON U.entry_id = src.entry_id
+                 INNER JOIN svc.t_service_use_report R
+                   ON R.report_id = U.report_id
+            WHERE NOT R.report_state_id IN (1, 2);
 
-        If _lockedServiceUseEntries > 0 Then
-            _returnCode := 'U5203';
-            _message := format('%s service use %s associated with a service use report that is not in state New or Active; aborting the update',
-                               _lockedServiceUseEntries, public.check_plural(_lockedServiceUseEntries, 'entry is', 'entries are'));
-            RAISE WARNING '%', _message;
-            RAISE EXCEPTION '%', _message;
+            If _lockedServiceUseEntries > 0 Then
+                _returnCode := 'U5203';
+                _message := format('%s service use %s associated with a service use report that is not in state New or Active; aborting the update',
+                                   _lockedServiceUseEntries, public.check_plural(_lockedServiceUseEntries, 'entry is', 'entries are'));
+                RAISE WARNING '%', _message;
+                RAISE EXCEPTION '%', _message;
+            End If;
+        End If;
+
+        ---------------------------------------------------
+        -- If mode is 'serviceCenterRefund', look for service use entries associated with service use reports that are not Complete
+        -- Also look for service use entries associated with dataasets that do not have the correct service center report state
+        ---------------------------------------------------
+
+        If _mode::citext = 'serviceCenterRefund' Then
+            SELECT COUNT(U.entry_id)
+            INTO _unlockedServiceUseEntries
+            FROM Tmp_ServiceUseIDs src
+                 INNER JOIN svc.t_service_use U
+                   ON U.entry_id = src.entry_id
+                 INNER JOIN svc.t_service_use_report R
+                   ON R.report_id = U.report_id
+            WHERE NOT R.report_state_id IN (3);
+
+            If _unlockedServiceUseEntries > 0 Then
+                _returnCode := 'U5204';
+                _message := format('%s service use %s associated with a service use report that is not in state Complete; aborting the update',
+                                   _unlockedServiceUseEntries, public.check_plural(_unlockedServiceUseEntries, 'entry is', 'entries are'));
+                RAISE WARNING '%', _message;
+                RAISE EXCEPTION '%', _message;
+            End If;
+
+            SELECT COUNT(U.entry_id)
+            INTO _refundIneligibleUseEntries
+            FROM Tmp_ServiceUseIDs src
+                 INNER JOIN svc.t_service_use U
+                   ON U.entry_id = src.entry_id
+                 INNER JOIN t_dataset DS
+                   ON DS.dataset_id = U.dataset_id
+            WHERE NOT DS.svc_center_report_state_id IN (3, 4);      -- 3: Submitted to service center, 4: Need to refund to service center
+
+            If _refundIneligibleUseEntries > 0 Then
+                _returnCode := 'U5205';
+                _message := format('%s service use %s associated with a dataset that does not have the correct service center report state ("submitted to service center" or "need to refund to service center"); aborting the update',
+                                   _refundIneligibleUseEntries, public.check_plural(_refundIneligibleUseEntries, 'entry is', 'entries are'));
+                RAISE WARNING '%', _message;
+                RAISE EXCEPTION '%', _message;
+            End If;
         End If;
 
         -- Initial validation checks are complete; now enable _logErrors
@@ -178,13 +237,33 @@ BEGIN
 
             If Not FOUND Then
                 _logErrors := false;
-                _returnCode := 'U5203';
+                _returnCode := 'U5206';
                 RAISE EXCEPTION 'Invalid dataset rating: "%" does not exist', _newValue;
             End If;
         End If;
 
+        If _mode::citext = 'serviceCenterRefund' Then
+            ---------------------------------------------------
+            -- Validate that _newValue is 'true' or 'false'
+            ---------------------------------------------------
+
+            If Not _newValue::citext IN ('true', 'false') Then
+                _logErrors := false;
+                _returnCode := 'U5207';
+                RAISE EXCEPTION 'Invalid value for _newValue when _mode is serviceCenterRefund: "%" (should be true or false)', _newValue;
+            End If;
+
+            If _newValue::citext = 'true' Then
+                _newDatasetServiceCenterReportStateID := 4;
+                _newDatasetServiceCenterReportState := 'Need to refund to service center';
+            Else
+                _newDatasetServiceCenterReportStateID := 3;
+                _newDatasetServiceCenterReportState := 'Submitted to service center';
+            End If;
+        End If;
+
         ----------------------------------------------------------
-        -- Generate a log message that describes the service use entries that will be updated
+        -- Generate a list of service use entry IDs that will be used in the log message that describes updates
         ----------------------------------------------------------
 
         -- Create and populate the temp table used by procedure condense_integer_list_to_ranges
@@ -209,7 +288,6 @@ BEGIN
         ---------------------------------------------------
 
         If _mode::citext = 'datasetRating' Then
-
             If _infoOnly Then
                 _logMessage := 'Will change';
             Else
@@ -323,6 +401,103 @@ BEGIN
             LOOP
                 CALL update_dataset_service_type_if_required (_datasetID, _infoOnly => _infoOnly);
             END LOOP;
+        End If;
+
+        If _mode::citext = 'serviceCenterRefund' Then
+            If _infoOnly Then
+                _logMessage := 'Will change';
+            Else
+                _logMessage := 'Changed';
+            End If;
+
+            _logMessage := format('%s the dataset service center report state to "%s" for datasets associated with %s service use %s; user %s; IDs %s',
+                                  _logMessage,
+                                  _newDatasetServiceCenterReportState,
+                                  _entryCount,
+                                  public.check_plural(_entryCount, 'entry', 'entries'),
+                                  _callingUser,
+                                  Coalesce(_valueList, '??'));
+
+            If _infoOnly Then
+                ----------------------------------------------------------
+                -- Preview what would be updated
+                ----------------------------------------------------------
+
+                RAISE INFO '';
+                RAISE INFO '%', _logMessage;
+                RAISE INFO '';
+
+                _formatSpecifier := '%-10s %-10s %-32s %-32s %-19s %-19s';
+
+                _infoHead := format(_formatSpecifier,
+                                    'Entry_ID',
+                                    'Dataset_ID',
+                                    'Old_Service_Center_Report_State',
+                                    'New_Service_Center_Report_State',
+                                    'Old_Report_State_ID',
+                                    'New_Report_State_ID'
+                                   );
+
+                _infoHeadSeparator := format(_formatSpecifier,
+                                             '----------',
+                                             '----------',
+                                             '--------------------------------',
+                                             '--------------------------------',
+                                             '-------------------',
+                                             '-------------------'
+                                            );
+
+                RAISE INFO '%', _infoHead;
+                RAISE INFO '%', _infoHeadSeparator;
+
+                FOR _previewData IN
+                  SELECT U.Entry_ID,
+                           U.Dataset_ID,
+                           RepState.svc_center_report_state      AS Old_Service_Center_Report_State,
+                           _newDatasetServiceCenterReportState   AS New_Service_Center_Report_State,
+                           DS.svc_center_report_state_id         AS Old_Report_State_ID,
+                           _newDatasetServiceCenterReportStateID AS New_Report_State_ID
+                    FROM Tmp_ServiceUseIDs src
+                         INNER JOIN svc.t_service_use U
+                           ON U.entry_id = src.entry_id
+                         INNER JOIN svc.t_service_use_report R
+                           ON R.report_id = U.report_id
+                         INNER JOIN t_dataset DS
+                           ON DS.dataset_id = U.dataset_id
+                         INNER JOIN t_dataset_svc_center_report_state RepState
+                           ON RepState.svc_center_report_state_id = DS.svc_center_report_state_id
+                    WHERE R.report_state_id IN (3)
+                    ORDER BY Entry_ID
+                LOOP
+                    _infoData := format(_formatSpecifier,
+                                        _previewData.Entry_ID,
+                                        _previewData.Dataset_ID,
+                                        _previewData.Old_Service_Center_Report_State,
+                                        _previewData.New_Service_Center_Report_State,
+                                        _previewData.Old_Report_State_ID,
+                                        _previewData.New_Report_State_ID
+                                       );
+
+                    RAISE INFO '%', _infoData;
+                END LOOP;
+
+                _message := _logMessage;
+            Else
+                UPDATE t_dataset DS
+                SET svc_center_report_state_id = _newDatasetServiceCenterReportStateID
+                FROM Tmp_ServiceUseIDs src
+                     INNER JOIN svc.t_service_use U
+                       ON U.entry_id = src.entry_id
+                     INNER JOIN svc.t_service_use_report R
+                       ON R.report_id = U.report_id
+                WHERE R.report_state_id IN (3) AND
+                      DS.dataset_id = U.dataset_id;
+                --
+                GET DIAGNOSTICS _updateCount = ROW_COUNT;
+
+                _message := format('Changed the dataset service center report state to "%s" for %s service use %s',
+                                   _newDatasetServiceCenterReportState, _updateCount, public.check_plural(_updateCount, 'entry', 'entries'));
+            End If;
         End If;
 
     EXCEPTION
