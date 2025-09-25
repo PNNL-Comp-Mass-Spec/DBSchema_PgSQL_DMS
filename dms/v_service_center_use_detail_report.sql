0e3d1e5459641f73c58aebd8a162383d89fd28e7
alchemistmatt <matthew.monroe@pnnl.gov>
2025-09-24 19:30:18 -0700

    Update table alias name in views

--- dms/v_service_center_use_detail_report.sql
+++ dms/v_service_center_use_detail_report.sql
@@ -23,7 +23,7 @@ CREATE VIEW public.v_service_center_use_detail_report AS
         CASE
             WHEN ((rep.report_state_id = ANY (ARRAY[1, 2])) AND (ds.svc_center_report_state_id = 3)) THEN 'Submitting to service center'::public.citext
             WHEN ((rep.report_state_id = ANY (ARRAY[1, 2])) AND (ds.svc_center_report_state_id = 5)) THEN 'Refunding to service center'::public.citext
-            ELSE scrs.svc_center_report_state
+            ELSE repstate.svc_center_report_state
         END AS svc_center_report_state
    FROM ((((((((svc.t_service_use u
      JOIN svc.t_service_use_report rep ON ((rep.report_id = u.report_id)))
@@ -31,7 +31,7 @@ CREATE VIEW public.v_service_center_use_detail_report AS
      LEFT JOIN public.t_charge_code cc ON ((cc.charge_code OPERATOR(public.=) u.charge_code)))
      LEFT JOIN public.t_dataset ds ON ((u.dataset_id = ds.dataset_id)))
      LEFT JOIN public.t_dataset_rating_name dsrating ON ((dsrating.dataset_rating_id = ds.dataset_rating_id)))
-     LEFT JOIN public.t_dataset_svc_center_report_state scrs ON ((ds.svc_center_report_state_id = scrs.svc_center_report_state_id)))
+     LEFT JOIN public.t_dataset_svc_center_report_state repstate ON ((ds.svc_center_report_state_id = repstate.svc_center_report_state_id)))
      LEFT JOIN public.t_requested_run rr ON ((ds.dataset_id = rr.dataset_id)))
      LEFT JOIN public.t_cached_dataset_stats cds ON ((u.dataset_id = cds.dataset_id)));
 
