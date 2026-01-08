c974af22a4675e9978db29982ca1cb21555462cd
alchemistmatt <matthew.monroe@pnnl.gov>
2026-01-07 21:11:41 -0800

    Use cached children column instead of function get_taxid_child_count()

--- dms/ont/ont.v_helper_ncbi_taxonomy_report.sql
+++ dms/ont/ont.v_helper_ncbi_taxonomy_report.sql
@@ -7,7 +7,7 @@ CREATE VIEW ont.v_helper_ncbi_taxonomy_report AS
     nodenames.name,
     nodes.rank,
     nodes.parent_tax_id,
-    ont.get_taxid_child_count(nodes.tax_id) AS children,
+    cached_info.children,
     parentnodename.name AS parent_name,
     division.division_name AS division
    FROM (((((ont.t_ncbi_taxonomy_names nodenames
@@ -15,7 +15,7 @@ CREATE VIEW ont.v_helper_ncbi_taxonomy_report AS
      JOIN ont.t_ncbi_taxonomy_division division ON ((nodes.division_id = division.division_id)))
      JOIN ont.t_ncbi_taxonomy_nodes parentnode ON ((nodes.parent_tax_id = parentnode.tax_id)))
      JOIN ont.t_ncbi_taxonomy_names parentnodename ON (((parentnode.tax_id = parentnodename.tax_id) AND (parentnodename.name_class OPERATOR(public.=) 'scientific name'::public.citext))))
-     LEFT JOIN ont.t_ncbi_taxonomy_cached synonymstats ON ((nodes.tax_id = synonymstats.tax_id)))
+     LEFT JOIN ont.t_ncbi_taxonomy_cached cached_info ON ((nodes.tax_id = cached_info.tax_id)))
   WHERE ((nodenames.name_class OPERATOR(public.=) 'scientific name'::public.citext) AND (NOT (nodes.rank OPERATOR(public.=) ANY (ARRAY['class'::public.citext, 'infraclass'::public.citext, 'infraorder'::public.citext, 'kingdom'::public.citext, 'order'::public.citext, 'parvorder'::public.citext, 'phylum'::public.citext, 'subclass'::public.citext, 'subkingdom'::public.citext, 'suborder'::public.citext, 'subphylum'::public.citext, 'subtribe'::public.citext, 'superclass'::public.citext, 'superfamily'::public.citext, 'superkingdom'::public.citext, 'superorder'::public.citext, 'superphylum'::public.citext, 'tribe'::public.citext]))));
 
 
--- dms/ont/ont.v_ncbi_taxonomy_list_report.sql
+++ dms/ont/ont.v_ncbi_taxonomy_list_report.sql
@@ -7,13 +7,13 @@ CREATE VIEW ont.v_ncbi_taxonomy_list_report AS
     nodenames.name,
     nodes.rank,
     nodes.parent_tax_id,
-    ont.get_taxid_child_count(nodes.tax_id) AS children,
+    cached_info.children,
     parentnodename.name AS parent_name,
     nodes.embl_code,
     division.division_name AS division,
     gencode.genetic_code_name,
     gencodemit.genetic_code_name AS mito_gen_code_name,
-    synonymstats.synonyms,
+    cached_info.synonyms,
     nodes.genbank_hidden
    FROM (((((((ont.t_ncbi_taxonomy_names nodenames
      JOIN ont.t_ncbi_taxonomy_nodes nodes ON ((nodenames.tax_id = nodes.tax_id)))
@@ -22,7 +22,7 @@ CREATE VIEW ont.v_ncbi_taxonomy_list_report AS
      JOIN ont.t_ncbi_taxonomy_nodes parentnode ON ((nodes.parent_tax_id = parentnode.tax_id)))
      JOIN ont.t_ncbi_taxonomy_names parentnodename ON (((parentnode.tax_id = parentnodename.tax_id) AND (parentnodename.name_class OPERATOR(public.=) 'scientific name'::public.citext))))
      JOIN ont.t_ncbi_taxonomy_gen_code gencodemit ON ((nodes.mito_genetic_code_id = gencodemit.genetic_code_id)))
-     LEFT JOIN ont.t_ncbi_taxonomy_cached synonymstats ON ((nodes.tax_id = synonymstats.tax_id)))
+     LEFT JOIN ont.t_ncbi_taxonomy_cached cached_info ON ((nodes.tax_id = cached_info.tax_id)))
   WHERE (nodenames.name_class OPERATOR(public.=) 'scientific name'::public.citext);
 
 
