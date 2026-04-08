3b79a8bcec0a24752583ab707ed5cf2a28fac2d5
alchemistmatt <matthew.monroe@pnnl.gov>
2026-04-07 17:37:47 -0700

    Add Unimod ID to list report

--- dms/v_mass_correction_factors.sql
+++ dms/v_mass_correction_factors.sql
@@ -3,17 +3,19 @@
 --
 
 CREATE VIEW public.v_mass_correction_factors AS
- SELECT mass_correction_id,
-    mass_correction_tag,
-    description,
-    monoisotopic_mass,
-    average_mass,
-    COALESCE(empirical_formula, ''::public.citext) AS empirical_formula,
-    affected_atom,
-    original_source,
-    original_source_name,
-    alternative_name
-   FROM public.t_mass_correction_factors;
+ SELECT mcf.mass_correction_id,
+    mcf.mass_correction_tag,
+    mcf.description,
+    mcf.monoisotopic_mass,
+    mcf.average_mass,
+    COALESCE(mcf.empirical_formula, ''::public.citext) AS empirical_formula,
+    mcf.affected_atom,
+    mcf.original_source,
+    mcf.original_source_name,
+    m.unimod_id,
+    mcf.alternative_name
+   FROM (public.t_mass_correction_factors mcf
+     LEFT JOIN ont.t_unimod_mods m ON (((mcf.original_source_name OPERATOR(public.=) m.name) AND (mcf.original_source OPERATOR(public.=) 'UniMod'::public.citext))));
 
 
 ALTER VIEW public.v_mass_correction_factors OWNER TO d3l243;
--- dms/v_param_file_mass_mods_list_report.sql
+++ dms/v_param_file_mass_mods_list_report.sql
@@ -16,16 +16,18 @@ CREATE VIEW public.v_param_file_mass_mods_list_report AS
     COALESCE(mcf.empirical_formula, ''::public.citext) AS empirical_formula,
     mcf.original_source,
     mcf.original_source_name,
+    m.unimod_id,
     pf.param_file_name,
     pf.param_file_description,
     tool.analysis_tool AS primary_tool
-   FROM ((((((public.t_param_file_mass_mods pfmm
+   FROM (((((((public.t_param_file_mass_mods pfmm
      JOIN public.t_residues r ON ((pfmm.residue_id = r.residue_id)))
      JOIN public.t_mass_correction_factors mcf ON ((pfmm.mass_correction_id = mcf.mass_correction_id)))
      JOIN public.t_seq_local_symbols_list sls ON ((pfmm.local_symbol_id = sls.local_symbol_id)))
      JOIN public.t_param_files pf ON ((pfmm.param_file_id = pf.param_file_id)))
      JOIN public.t_param_file_types pft ON ((pf.param_file_type_id = pft.param_file_type_id)))
-     JOIN public.t_analysis_tool tool ON ((pft.primary_tool_id = tool.analysis_tool_id)));
+     JOIN public.t_analysis_tool tool ON ((pft.primary_tool_id = tool.analysis_tool_id)))
+     LEFT JOIN ont.t_unimod_mods m ON (((mcf.original_source_name OPERATOR(public.=) m.name) AND (mcf.original_source OPERATOR(public.=) 'UniMod'::public.citext))));
 
 
 ALTER VIEW public.v_param_file_mass_mods_list_report OWNER TO d3l243;
