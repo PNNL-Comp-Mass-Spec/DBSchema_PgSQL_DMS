871f53befe5d8e912b85aa0c23a5e12c5c1946fd
alchemistmatt <matthew.monroe@pnnl.gov>
2026-06-09 18:19:32 -0700

    Add simplified service cost rate view

---
+++ dms/v_service_cost_rate_list_report.sql
@@ -0,0 +1,27 @@
+--
+-- Name: v_service_cost_rate_list_report; Type: VIEW; Schema: public; Owner: d3l243
+--
+
+CREATE VIEW public.v_service_cost_rate_list_report AS
+ SELECT cr.cost_group_id,
+    cg.description,
+    t.service_type,
+    cr.service_type_id,
+    cr.doe_burdened_rate_per_run AS doe_rate,
+    cr.hhs_burdened_rate_per_run AS hhs_rate,
+    cr.ldrd_burdened_rate_per_run AS ldrd_rate
+   FROM ((svc.t_service_cost_rate cr
+     JOIN svc.t_service_type t ON ((t.service_type_id = cr.service_type_id)))
+     JOIN svc.t_service_cost_group cg ON ((cg.cost_group_id = cr.cost_group_id)))
+  WHERE (cg.service_cost_state_id = 2);
+
+
+ALTER VIEW public.v_service_cost_rate_list_report OWNER TO d3l243;
+
+--
+-- Name: TABLE v_service_cost_rate_list_report; Type: ACL; Schema: public; Owner: d3l243
+--
+
+GRANT SELECT ON TABLE public.v_service_cost_rate_list_report TO readaccess;
+GRANT SELECT ON TABLE public.v_service_cost_rate_list_report TO writeaccess;
+
